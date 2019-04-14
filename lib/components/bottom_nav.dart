679cedcddfa52069f6553758448b65e8a8135910
Eric Yang <eric@junto.love>
2019-04-14 15:16:34 -0400

    fix bottom nav alignment

--- lib/components/bottom_nav.dart
+++ lib/components/bottom_nav.dart
@@ -10,74 +10,67 @@ class BottomNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return BottomNavigationBar(items: [
-      BottomNavigationBarItem(
-        icon: IconButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => JuntoCollective()),
-              );
-            },
-            icon: Icon(CustomIcons.home),
-            color: Colors.black),
-        title: Text(
-          '',
-          style: TextStyle(color: Colors.black),
+    return BottomNavigationBar(
+      // type fixes alignment of BottomNavigationBarItem if x > 3
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          icon: IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => JuntoSpheres()),
+                );
+              },
+              icon: Icon(CustomIcons.home),
+              color: Colors.black),
+              title: SizedBox.shrink()
+
         ),
-      ),
-      BottomNavigationBarItem(
-        icon: IconButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => JuntoSpheres()),
-              );
-            },
-            icon: Icon(CustomIcons.home),
-            color: Colors.black),
-        title: Text(
-          '',
-          style: TextStyle(color: Colors.black),
+        BottomNavigationBarItem(
+          icon: IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => JuntoSpheres()),
+                );
+              },
+              icon: Icon(CustomIcons.home),
+              color: Colors.black),
+              title: Container(height: 0, width: 0)
+
         ),
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(CustomIcons.home, color: Colors.black),
-        title: Text(
-          '',
-          style: TextStyle(color: Colors.black),
+        BottomNavigationBarItem(
+          icon: Icon(CustomIcons.home, color: Colors.black),
+              title: Container(height: 0, width: 0)
+
         ),
-      ),
-      BottomNavigationBarItem(
-        icon: IconButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => JuntoPack()),
-              );
-            },
-            icon: Icon(CustomIcons.home),
-            color: Colors.black),
-        title: Text(
-          '',
-          style: TextStyle(color: Colors.black),
+        BottomNavigationBarItem(
+          icon: IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => JuntoPack()),
+                );
+              },
+              icon: Icon(CustomIcons.home),
+              color: Colors.black),
+              title: Container(height: 0, width: 0)
+
         ),
-      ),
-      BottomNavigationBarItem(
-        icon: IconButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => JuntoDen()),
-              );
-            },
-            icon: Icon(CustomIcons.home),
-            color: Colors.black),
-        title: Text(
-          '',
-          style: TextStyle(color: Colors.black),
+        BottomNavigationBarItem(
+          icon: IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => JuntoDen()),
+                );
+              },
+              icon: Icon(CustomIcons.home),
+              color: Colors.black),
+              title: Container(height: 0, width: 0)
+
         ),
-      ),
     ]);
   }
 }
