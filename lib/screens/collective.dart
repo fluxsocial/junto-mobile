189a8bb336de3965b7da16493e75e126544ece60
Eric Yang <eric@junto.love>
2019-04-18 15:32:03 -0400

    revert back to commit 27

--- lib/screens/collective.dart
+++ lib/screens/collective.dart
@@ -7,7 +7,7 @@ import './../components/bottom_nav/bottom_nav.dart';
 import './../components/expression_preview/expression_preview.dart';
 import './../components/filter_channel_collective/filter_channel_collective.dart';
 
-import './../models/expression.dart';
+import './../models/expressions.dart';
 
 class JuntoCollective extends StatefulWidget {
   @override
@@ -22,54 +22,58 @@ class _JuntoCollectiveState extends State {
   initState() {
     super.initState();
 
-    expressions = Expression.fetchExpressions();
+    expressions = Expressions.fetchExpressions();
+  }
+
+  Widget _collectiveExpressions(BuildContext context, int index) {
+    return ExpressionPreview(expressions, index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:
-            // PreferredSize(
-            //   preferredSize: Size.fromHeight(45.0),
-            //   child:
-            AppBar(
-                backgroundColor: JuntoTheme.juntoWhite,
-                // backgroundColor: Colors.blue,
-                brightness: Brightness.light,
-                elevation: 0,
-                title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: <Widget>[
-                          Image.asset(
-                              'assets/images/junto-mobile__logo--collective.png',
-                              height: 20.0,
-                              width: 20.0),
-                          Container(
-                            margin: EdgeInsets.only(left: 10.0),
-                            child: Text(
-                              'JUNTO',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: JuntoTheme.juntoSleek,
-                                  fontWeight: FontWeight.w700,
-                                  letterSpacing: 1.3),
-                            ),
+        appBar: 
+        // PreferredSize(
+        //   preferredSize: Size.fromHeight(45.0),
+        //   child: 
+          AppBar(
+              backgroundColor: JuntoTheme.juntoWhite,
+              // backgroundColor: Colors.blue,
+              brightness: Brightness.light,
+              elevation: 0,
+              title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: <Widget>[
+                        Image.asset(
+                            'assets/images/junto-mobile__logo--collective.png',
+                            height: 20.0,
+                            width: 20.0),
+                        Container(
+                          margin: EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            'JUNTO',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: JuntoTheme.juntoSleek,
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 1.3),
                           ),
-                        ],
-                      ),
-                      IconButton(
-                        alignment: Alignment.centerRight,
-                        icon: Icon(CustomIcons.moon),
-                        iconSize: 20.0,
-                        color: JuntoTheme.juntoSleek,
-                        tooltip: 'open notifcations',
-                        onPressed: () {
-                          // ...
-                        },
-                      ),
-                    ])),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      alignment: Alignment.centerRight,
+                      icon: Icon(CustomIcons.moon),
+                      iconSize: 20.0,
+                      color: JuntoTheme.juntoSleek,
+                      tooltip: 'open notifcations',
+                      onPressed: () {
+                        // ...
+                      },
+                    ),
+                  ])),
         // ),
 
         body: Container(
@@ -116,8 +120,7 @@ class _JuntoCollectiveState extends State {
 
               Expanded(
                 child: ListView.builder(
-                    itemBuilder: (context, ndx) =>
-                        ExpressionPreview(expressions[ndx]),
+                    itemBuilder: _collectiveExpressions,
                     itemCount: expressions.length),
               ),
             ],
