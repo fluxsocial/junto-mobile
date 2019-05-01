f7eb84a19b16bec6f5131bc291f6bee22bac7393
Eric Yang <eric@junto.love>
2019-05-01 12:13:14 -0400

    Build custom collective_perspectives widget

--- lib/screens/collective/collective.dart
+++ lib/screens/collective/collective.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../components/appbar/appbar.dart';
 import './../../components/bottom_nav/bottom_nav.dart';
+import './collective_perspectives/collective_perspectives.dart';
 import './../../components/expression_preview/expression_preview.dart';
 import '../../scoped_models/scoped_expressions.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -24,34 +25,7 @@ class JuntoCollective extends StatelessWidget {
             children: <Widget>[
               
               // perspectives
-              Container(
-                height: 75.0,
-                color: JuntoPalette.juntoWhite,
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                foregroundDecoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(width: .75, color: Color(0xffeeeeee)),
-                  ),
-                ),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'COLLECTIVE',
-                        style: TextStyle(fontWeight: FontWeight.w700),
-                      ),
-
-                      IconButton(
-                        padding: EdgeInsets.all(0.0),
-                        alignment: Alignment.centerRight,
-                        icon: Icon(Icons.arrow_right),
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(
-                              context, '/perspectives');
-                        },
-                      )
-                    ]),
-              ),
+              CollectivePerspectives(),
 
               // expressions
               ScopedModelDescendant<ScopedExpressions>(
