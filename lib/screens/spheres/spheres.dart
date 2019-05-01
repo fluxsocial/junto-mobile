08232068b8fea25470e0212dc5bc7a04036ce45a
Eric Yang <eric@junto.love>
2019-05-01 12:38:34 -0400

    Create custom spheres_create widget

--- lib/screens/spheres/spheres.dart
+++ lib/screens/spheres/spheres.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import '../../components/appbar/appbar.dart';
 import './../../components/bottom_nav/bottom_nav.dart';
 import '../../models/sphere.dart';
+import './spheres__create/spheres__create.dart';
 import './sphere_preview.dart';
 import './../../typography/palette.dart';
 
@@ -37,22 +38,7 @@ class _JuntoSpheresState extends State {
             ListView(
               children: <Widget>[
                 // Create sphere
-                Container(
-                  height: 75,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    border: Border(bottom: BorderSide(color: Color(0xffeeeeee), width: 1))
-                    ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Create a Sphere', style: TextStyle(color: JuntoPalette.juntoGrey, fontSize: 14, fontWeight: FontWeight.w700)),
-                      Container(
-                        child: Icon(Icons.add_circle_outline, size: 17, color: JuntoPalette.juntoGrey)
-                      )
-                    ])
-                ),
+                SpheresCreate(),
 
                 // List of spheres member belongs to
                   ListView(
---
+++ lib/screens/spheres/spheres__create/spheres__create.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../typography/palette.dart';
+
+class SpheresCreate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return 
+      Container(
+        height: 75,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Color(0xffeeeeee), width: 1))
+          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Create a Sphere', style: TextStyle(color: JuntoPalette.juntoGrey, fontSize: 14, fontWeight: FontWeight.w700)),
+            Container(
+              child: Icon(Icons.add_circle_outline, size: 17, color: JuntoPalette.juntoGrey)
+            )
+          ])
+      ); 
+  }
+}
