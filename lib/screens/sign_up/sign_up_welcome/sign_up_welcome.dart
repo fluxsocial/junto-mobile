1f83ed5ae44f90f1c23c9d001881258a57d171f9
Eric Yang <eric@junto.love>
2019-05-08 17:19:30 -0400

    Remove Stack, and refactor Column widget and margin

--- lib/screens/sign_up/sign_up_welcome/sign_up_welcome.dart
+++ lib/screens/sign_up/sign_up_welcome/sign_up_welcome.dart
@@ -37,106 +37,94 @@ class SignUpWelcomeState extends State<SignUpWelcome> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Stack(
-        children: [
-
+      body: 
           Container(
             color: Colors.white,
             width: MediaQuery.of(context).size.width,
             child: 
               Container(
-                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * .20),
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * .15,
+                  bottom: MediaQuery.of(context).size.height * .15),
                 child:
-                  Column(children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 40),
-                      child: Image.asset('assets/images/junto-mobile__outlinelogo--gradient.png',
-                      height: 69)
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                    Container(
-                      width: MediaQuery.of(context).size.width * .5,
-                      margin: EdgeInsets.only(bottom: 40),
-                      decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(
-                          color: Color(0xffeeeeee),
-                        ))
-                      )
-                    ),    
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[  
+                          Container(
+                            margin: EdgeInsets.only(bottom: 40),
+                            child: Image.asset('assets/images/junto-mobile__outlinelogo--gradient.png',
+                            height: 69)
+                          ),
 
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .10),
-                      margin: EdgeInsets.only(bottom: 40),
-                      child: Text('Hey ' + widget.firstName + 
-                      '! We are stoked to have you here.'
-                      ,style: TextStyle(color: JuntoPalette.juntoGrey, fontWeight: FontWeight.w700, fontSize: 22
-                      ), textAlign: TextAlign.center,                                  
-                      ),
-                    ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * .5,
+                            margin: EdgeInsets.only(bottom: 40),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(
+                                color: Color(0xffeeeeee),
+                              ))
+                            )
+                          ),    
 
-                    Container(
-                      width: MediaQuery.of(context).size.width * .5,
-                      margin: EdgeInsets.only(bottom: 40),
-                      decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(
-                          color: Color(0xffeeeeee),
-                        ))
-                      )
-                    ),                 
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .10),
+                            margin: EdgeInsets.only(bottom: 40),
+                            child: Text('Hey ' + widget.firstName + 
+                            '! We are stoked to have you here.'
+                            ,style: TextStyle(color: JuntoPalette.juntoGrey, fontWeight: FontWeight.w700, fontSize: 22
+                            ), textAlign: TextAlign.center,                                  
+                            ),
+                          ),
 
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .10),
-                      child: Text('Junto is a community of individuals working together to inspire authenticity and meaningful collaboration.'                        
-                      , style: TextStyle(color: JuntoPalette.juntoGrey, fontSize: 17,), 
-                      textAlign: TextAlign.center,),
-                    ),          
+                          Container(
+                            width: MediaQuery.of(context).size.width * .5,
+                            margin: EdgeInsets.only(bottom: 40),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(
+                                color: Color(0xffeeeeee),
+                              ))
+                            )
+                          ),                 
 
-                                  
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .10),
+                            child: Text('Junto is a community of individuals working together to inspire authenticity and meaningful collaboration.'                        
+                            , style: TextStyle(color: JuntoPalette.juntoGrey, fontSize: 17,), 
+                            textAlign: TextAlign.center,),
+                          ),   
+                      ],),
+       
+                        Container(
+                          width: 200,
+                          child:     
+                            RaisedButton(                                        
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => JuntoCollective()
+                                ));
+                              },  
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 20
+                              ),            
 
+                              color: Color(0xff4968BF),
+                              child: Text('LET\'S GO!', 
+                                style: TextStyle(
+                                  // color: JuntoPalette.juntoBlue, 
+                                  color: Colors.white, 
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 14)),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100))
+                            )                                           
+                        )                                  
                   ],)
               )
-          ),
-
-          Positioned(
-            left: 0,
-            bottom: MediaQuery.of(context).size.height * .2,
-            child: 
-              Container(            
-                width: MediaQuery.of(context).size.width,    
-                child: 
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 200,
-                        child:     
-                          RaisedButton(                                        
-                            onPressed: () {
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => JuntoCollective()
-                              ));
-                            },  
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 20
-                            ),            
-
-                            color: Color(0xff4968BF),
-                            child: Text('LET\'S GO!', 
-                              style: TextStyle(
-                                // color: JuntoPalette.juntoBlue, 
-                                color: Colors.white, 
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14)),
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100))
-                          )                                           
-                      )
-                  ],)
-              ),               
-          )
-                          
-        ])
-    
+          ),                            
     );      
   }
 }
