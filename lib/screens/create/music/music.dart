60464a1573eb108a7eb7ddfa4263232feb26fe62
Eric Yang <eric@junto.love>
2019-06-12 17:21:50 -0400

    Integrate WebView package

--- lib/screens/create/music/music.dart
+++ lib/screens/create/music/music.dart
@@ -1,16 +1,23 @@
 
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class CreateMusic extends StatelessWidget {
+  WebViewController _controller;
+
   @override
   Widget build(BuildContext context) {
 
-    return Expanded(
-      child: ListView(
-        padding: EdgeInsets.all(0),
-        children: <Widget>[
-        Container(child: Center(child: Text('MUSIC')))
-      ],)
-    );
+    return 
+      Container();
+      // Container(
+      //   height: 500,
+      //   child: WebView(
+      //         initialUrl: 'https://junto.foundation',
+      //         onWebViewCreated: (WebViewController webViewController) {
+      //           _controller = webViewController;
+      //         },
+      //         javascriptMode: JavascriptMode.unrestricted
+      // ));
   }
 }
