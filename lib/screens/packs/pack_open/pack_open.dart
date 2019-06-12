7c2f0897380ae6b22d201de1214e00b175353224
Eric Yang <eric@junto.love>
2019-06-12 12:42:13 -0400

    Create separate widget for pack_open_appbar

--- lib/screens/packs/pack_open/pack_open.dart
+++ lib/screens/packs/pack_open/pack_open.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-import '../../../typography/palette.dart';
-import '../../../custom_icons.dart';
+import './pack_open_appbar/pack_open_appbar.dart';
 import '../../../scoped_models/scoped_user.dart';
 import '../../../components/expression_preview/expression_preview.dart';
 
@@ -31,73 +30,10 @@ class PackOpenState extends State<PackOpen> {
       backgroundColor: Colors.white,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(45.0),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          brightness: Brightness.light,
-          iconTheme: IconThemeData(color: JuntoPalette.juntoSleek),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleSpacing: 0,
-          title: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: Icon(CustomIcons.back_arrow_left,
-                      color: JuntoPalette.juntoSleek, size: 24),
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Text(widget.packTitle,
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black)),
-                    ),
-                    ClipOval(
-                      child: Image.asset(
-                        widget.packImage,
-                        height: 27.0,
-                        width: 27.0,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ],
-                ),
-
-                // Icon(CustomIcons.more,
-                //     color: JuntoPalette.juntoSleek, size: 24)
-              ],
-            ),
-          ),
-          bottom: PreferredSize(
-              preferredSize: Size.fromHeight(1),
-              child: Container(
-                height: 1,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      stops: [
-                        0.1,
-                        0.9
-                      ],
-                      // colors: [Color(0xffeeeeee), Color(0xffeeeeee)]
-                      colors: [
-                        JuntoPalette.juntoPurple,
-                        JuntoPalette.juntoPurpleLight
-                      ]),
-                ),
-              )),
-        ),
+        child: PackOpenAppbar(widget.packTitle, widget.packUser, widget.packImage)
       ),
-      body: ScopedModelDescendant<ScopedUser>(
+      
+            body: ScopedModelDescendant<ScopedUser>(
         builder: (context, child, model) => ListView(
               // shrinkWrap: true,
               // physics: ClampingScrollPhysics(),
---
+++ lib/screens/packs/pack_open/pack_open_appbar/pack_open_appbar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../../../../typography/palette.dart';
+import '../../../../custom_icons.dart';
+
+class PackOpenAppbar extends StatelessWidget {
+  final packTitle;
+  final packUser;
+  final packImage;
+
+  PackOpenAppbar(this.packTitle, this.packUser, this.packImage);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      brightness: Brightness.light,
+      iconTheme: IconThemeData(color: JuntoPalette.juntoSleek),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      titleSpacing: 0,
+      title: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Icon(CustomIcons.back_arrow_left,
+                  color: JuntoPalette.juntoSleek, size: 24),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: Text(packTitle,
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black)),
+                ),
+                ClipOval(
+                  child: Image.asset(
+                    packImage,
+                    height: 27.0,
+                    width: 27.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Container(
+            height: 1,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  stops: [
+                    0.1,
+                    0.9
+                  ],
+                  colors: [
+                    JuntoPalette.juntoPurple,
+                    JuntoPalette.juntoPurpleLight
+                  ]),
+            ),
+          )),
+    );
+  }
+}
