93046d4cbd793f89f63a67c08595c342a7b4a275
Eric Yang <eric@junto.love>
2019-06-12 13:55:43 -0400

    Create separate widget for PackOpenFAB

--- lib/screens/packs/pack_open/pack_open.dart
+++ lib/screens/packs/pack_open/pack_open.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter/rendering.dart';
 
+import './pack_open_fab/pack_open_fab.dart';
 import './pack_open_appbar/pack_open_appbar.dart';
 import '../../../scoped_models/scoped_user.dart';
 import '../../../components/expression_preview/expression_preview.dart';
@@ -63,46 +64,8 @@ class PackOpenState extends State<PackOpen> {
             PackOpenAppbar(widget.packTitle, widget.packUser, widget.packImage),
       ),
 
-      floatingActionButton: 
-      AnimatedOpacity(                                      
-        duration: Duration(milliseconds: 200),          
-        opacity: _isVisible ? 1 : 0,
-        child:       
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              // border: Border.all(color: Color(0xffeeeeee), width: .5),
-              borderRadius: BorderRadius.circular(25),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff999999),
-                  blurRadius: 3,
-                  offset: Offset(1, 2),
-                )
-              ],
-            ),
-            margin: EdgeInsets.only(left: 15, right: 15, bottom: 10),
-            // padding: EdgeInsets.symmetric(vertical: 15),
-            height: 50,
-            width: MediaQuery.of(context).size.width * .75,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(                
-                  width: MediaQuery.of(context).size.width * .37,
-                  child: Icon(CustomIcons.half_lotus, size: 17, color: JuntoPalette.juntoGrey),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * .37,
-                  child:
-                    RotatedBox(
-                      quarterTurns: 2,
-                      child: Icon(CustomIcons.triangle, size: 17, color: JuntoPalette.juntoGrey)
-                    )
-                )            
-            ],)
-          )
-        ),
+      floatingActionButton: PackOpenFAB(_isVisible),
+
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked ,
 
       body: ScopedModelDescendant<ScopedUser>(
