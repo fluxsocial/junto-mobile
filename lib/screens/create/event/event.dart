bf778b84970be7636171f32a5a387caaa6f80128
Eric Yang <eric@junto.love>
2019-06-18 20:40:35 -0400

    Refactor event expression and add CreateActions widget

--- lib/screens/create/event/event.dart
+++ lib/screens/create/event/event.dart
@@ -3,96 +3,105 @@ import 'package:flutter/material.dart';
 
 import '../../../typography/palette.dart';
 import '../../../typography/style.dart';
+import '../create_actions.dart';
 
 class CreateEvent extends StatelessWidget {
+  Map _eventExpression = {
+
+  };
+  
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-        children: <Widget>[
-          Container(
-            child: 
-            TextField(
-              buildCounter: (BuildContext context,
-                      {int currentLength, int maxLength, bool isFocused}) =>
-                  null,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Name of event',
-              ),
-              cursorColor: JuntoPalette.juntoGrey,
-              cursorWidth: 2,
-              style: JuntoStyles.lotusLongformTitle,
-              maxLines: 1,
-              maxLength: 80,
-            ),                   
-          ), 
-
-          // Container(
-          //   color: Color(0xfffbfbfb),
-          //   height: 200,
-          //   width: MediaQuery.of(context).size.width,
-          //   child: Center(child: Text('Add a cover photo (optional)'))
-          // ), 
-
-          Container(
-            child: 
-            TextField(
-              buildCounter: (BuildContext context,
-                      {int currentLength, int maxLength, bool isFocused}) =>
-                  null,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Date and Time',
-              ),
-              cursorColor: JuntoPalette.juntoGrey,
-              cursorWidth: 2,
-              style: JuntoStyles.lotusLongformTitle,
-              maxLines: 1,
-              maxLength: 80,
-            ),                   
-          ),
-
-          Container(
-            child: 
-            TextField(
-              buildCounter: (BuildContext context,
-                      {int currentLength, int maxLength, bool isFocused}) =>
-                  null,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Location',
-              ),
-              cursorColor: JuntoPalette.juntoGrey,
-              cursorWidth: 2,
-              style: JuntoStyles.lotusLongformTitle,
-              maxLines: 1,
-              maxLength: 80,
-            ),                   
-          ),
-
-          Container(
-            constraints: BoxConstraints(minHeight: 100, maxHeight: 240),
-            padding: EdgeInsets.only(bottom: 40),            
-            child: TextField(
+    return 
+    Expanded(child: Column(children: <Widget>[
+      Expanded(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+          children: <Widget>[
+            Container(
+              child: 
+              TextField(
                 buildCounter: (BuildContext context,
                         {int currentLength, int maxLength, bool isFocused}) =>
                     null,
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  hintText: 'Details',
+                  hintText: 'Name of event',
                 ),
                 cursorColor: JuntoPalette.juntoGrey,
                 cursorWidth: 2,
                 style: JuntoStyles.lotusLongformTitle,
-                maxLines: null,
-                textInputAction: TextInputAction.newline,
-              ),                
-          )                    
-   
+                maxLines: 1,
+                maxLength: 80,
+              ),                   
+            ), 
 
-      ],)
-    );
+            // Container(
+            //   color: Color(0xfffbfbfb),
+            //   height: 200,
+            //   width: MediaQuery.of(context).size.width,
+            //   child: Center(child: Text('Add a cover photo (optional)'))
+            // ), 
+
+            Container(
+              child: 
+              TextField(
+                buildCounter: (BuildContext context,
+                        {int currentLength, int maxLength, bool isFocused}) =>
+                    null,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Date and Time',
+                ),
+                cursorColor: JuntoPalette.juntoGrey,
+                cursorWidth: 2,
+                style: JuntoStyles.lotusLongformTitle,
+                maxLines: 1,
+                maxLength: 80,
+              ),                   
+            ),
+
+            Container(
+              child: 
+              TextField(
+                buildCounter: (BuildContext context,
+                        {int currentLength, int maxLength, bool isFocused}) =>
+                    null,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Location',
+                ),
+                cursorColor: JuntoPalette.juntoGrey,
+                cursorWidth: 2,
+                style: JuntoStyles.lotusLongformTitle,
+                maxLines: 1,
+                maxLength: 80,
+              ),                   
+            ),
+
+            Container(
+              constraints: BoxConstraints(minHeight: 100, maxHeight: 240),
+              padding: EdgeInsets.only(bottom: 40),            
+              child: TextField(
+                  buildCounter: (BuildContext context,
+                          {int currentLength, int maxLength, bool isFocused}) =>
+                      null,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Details',
+                  ),
+                  cursorColor: JuntoPalette.juntoGrey,
+                  cursorWidth: 2,
+                  style: JuntoStyles.lotusLongformTitle,
+                  maxLines: null,
+                  textInputAction: TextInputAction.newline,
+                ),                
+            )                      
+        ],)
+      ),
+
+      CreateActions(_eventExpression)
+    ],));
+
   }
 }
