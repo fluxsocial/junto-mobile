3d28b157aebbd5548588da5b66156dedbdac5d0b
Eric Yang <eric@junto.love>
2019-08-07 15:27:49 -0400

    Add TextField to GlobalSearch

--- lib/screens/global_search/global_search.dart
+++ lib/screens/global_search/global_search.dart
@@ -17,57 +17,107 @@ class GlobalSearchState extends State<GlobalSearch> {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: PreferredSize(
-
             preferredSize: Size.fromHeight(45),
             child: AppBar(
-              automaticallyImplyLeading: false,
-              brightness: Brightness.light,
-              iconTheme: IconThemeData(color: JuntoPalette.juntoSleek),
-              backgroundColor: Colors.white,
-              elevation: 0,
-              titleSpacing: 0,
-              title: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () => Navigator.pop(context),
-                            child: Icon(CustomIcons.back_arrow_left,
-                                color: JuntoPalette.juntoSleek, size: 24),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 5),
-                            child: Icon(Icons.search, size: 20)
+                automaticallyImplyLeading: false,
+                brightness: Brightness.light,
+                iconTheme: IconThemeData(color: JuntoPalette.juntoSleek),
+                backgroundColor: Colors.white,
+                elevation: 0,
+                titleSpacing: 0,
+                title: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Icon(CustomIcons.back_arrow_left,
+                          color: JuntoPalette.juntoSleek, size: 24),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5),
+                      color: Colors.white,
+                      width: MediaQuery.of(context).size.width * .8,
+                      child: Transform.translate(
+                        offset: Offset(0, 7),
+                        child: TextField(
+                        buildCounter: (BuildContext context,
+                              {int currentLength,
+                              int maxLength,
+                              bool isFocused}) =>
+                          null,                        
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'search..',
+                        ),
+                        cursorColor: JuntoPalette.juntoGrey,
+                        maxLines: 1,
+                        maxLength: 30,
+                        cursorWidth: 2,
+                      ),),
+                    ),
+                  ],
+                )
 
-                          )
-                        ],
-                      ),
-                    ],
-                  )),
-            )),
-        body: Column(children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                decoration: BoxDecoration(
+                // TextField(
+                //   decoration: InputDecoration(
+                //     border: InputBorder.none,
+                //     hintText: 'search..',
+                //   ),
+                //   cursorColor: JuntoPalette.juntoGrey,
+                //   cursorWidth: 2,
+                // ),
+                // GestureDetector(
+                //   onTap: () => Navigator.pop(context),
+                //   child: Icon(CustomIcons.back_arrow_left,
+                //       color: JuntoPalette.juntoSleek, size: 24),
+                // ),
+
+                // Container(
+                //     padding: EdgeInsets.symmetric(horizontal: 5),
+                //     child:
+                //         Row(
+                //           crossAxisAlignment: CrossAxisAlignment.center,
+                //           children: <Widget>[
+                //             GestureDetector(
+                //               onTap: () => Navigator.pop(context),
+                //               child: Icon(CustomIcons.back_arrow_left,
+                //                   color: JuntoPalette.juntoSleek, size: 24),
+                //             ),
+
+                //                 // TextField(
+                //                 //   decoration: InputDecoration(
+                //                 //     border: InputBorder.none,
+                //                 //     hintText: 'search..',
+                //                 //   ),
+                //                 //   cursorColor: JuntoPalette.juntoGrey,
+                //                 //   cursorWidth: 2,
+
+                //                 // ),
+                //           ],
+                //         ),
+                //     ),
+                )),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              decoration: BoxDecoration(
                   color: Colors.white,
-                  border: Border(bottom: BorderSide(color: Color(0xffeeeeee), width: 1))
-                ),
-                child: 
-                  Row(
-                    children: <Widget>[                      
-                      Text('Members'),
-                      SizedBox(width: 25),
-                      Text('Spheres')
-                    ],
-                  ),                
-              )                       
-        ],));
-        
-        // ListView(
-        //   children: <Widget>[],
-        // ));
+                  border: Border(
+                      bottom: BorderSide(color: Color(0xffeeeeee), width: 1))),
+              child: Row(
+                children: <Widget>[
+                  Text('Members'),
+                  SizedBox(width: 25),
+                  Text('Spheres')
+                ],
+              ),
+            )
+          ],
+        ));
+
+    // ListView(
+    //   children: <Widget>[],
+    // ));
   }
 }
