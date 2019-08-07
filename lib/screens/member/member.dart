0cc0b361c25199a42a6499d86b740f4ed0da5faa
Eric Yang <eric@junto.love>
2019-08-07 14:13:40 -0400

    Build member screen

--- lib/screens/member/member.dart
+++ lib/screens/member/member.dart
@@ -6,11 +6,11 @@ class JuntoMember extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(45),
-          child: MemberAppbar(
-              'sunyata'),
-        ),  
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(45),
+        child: MemberAppbar('sunyata'),
+      ),
+      backgroundColor: Colors.white,
 
       body: ListView(children: [
         Container(
@@ -27,9 +27,8 @@ class JuntoMember extends StatelessWidget {
                 decoration: BoxDecoration(
                     image: DecorationImage(
                         image: AssetImage(
-                            'assets/images/junto-mobile__den--photo.png'),
+                            'assets/images/junto-mobile__stillmind.png'),
                         fit: BoxFit.cover)),
-
                 child: Transform.translate(
                   offset: Offset(0, 120),
                   child: Container(
@@ -60,8 +59,11 @@ class JuntoMember extends StatelessWidget {
                   width: MediaQuery.of(context).size.width),
 
               Container(
-                padding: EdgeInsets.only(top: 10),
-                color: Colors.white,
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom:
+                            BorderSide(color: Color(0xffeeeeee), width: 1))),
                 width: MediaQuery.of(context).size.width,
                 child: Column(children: [
                   Container(
@@ -69,44 +71,24 @@ class JuntoMember extends StatelessWidget {
                     child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('ERIC YANG',
+                          Container(
+                              child:
+                                Text('Eric Yang',
                                     style: TextStyle(
                                         fontSize: 17,
                                         fontWeight: FontWeight.w700)),
-                                Text('sunyata',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                    ))
-                              ]),
+                            ),
 
-                          Row(children: <Widget>[
-                              GestureDetector(
-                                onTap: () {},
-                                child:  
-                                  Container(
-                                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                                      decoration: BoxDecoration(border: Border.all(color: Color(0xff555555), width: 1), borderRadius: BorderRadius.circular(2)),                               
-                                      child: Text('FOLLOW', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700))
-                                    ),
-                              ),
-
-                              GestureDetector(
-                                onTap: () {},
-                                child: 
-                                  Container(
-                                    margin: EdgeInsets.only(left: 5.0),
-                                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                                    decoration: BoxDecoration(border: Border.all(color: Color(0xff555555), width: 1), borderRadius: BorderRadius.circular(2)),                               
-                                    child: Text('JOIN PACK', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700))
-                                  ),                                    
-                              )                      
-                          ],)
-
-                       
+                            Row(children: <Widget>[
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Color(0xff333333), width: 1.5),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                                child: Text('connect', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700)),                                
+                              )
+                            ],)
                         ]),
                   ),
                   Container(
@@ -120,38 +102,6 @@ class JuntoMember extends StatelessWidget {
                             fontSize: 14,
                             fontWeight: FontWeight.w500,
                           ))),
-                  Container(
-                      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xffeeeeee), width: 1))),
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      margin: EdgeInsets.only(bottom: 10),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                        Container(
-                            child: Text('EXPRESSIONS',
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w700,
-                                    color: Color(0xff333333)))),
-
-                        // Container(
-                        //   child: Text('JOURNAL', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700, color: Color(0xff777777)))
-                        // ),
-
-                        // Container(
-                        //   child: Text('FAVORITES', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700, color: Color(0xff777777)))
-                        // ),
-
-                        // Container(
-                        //   child: Text('DRAFTS', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700, color: Color(0xff777777)))
-                        // ),
-                      ])),
-
-
-
-
-
-
                 ]),
               ),
             ],
