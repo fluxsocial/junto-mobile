74f717e6e89bceedf9cc116d89eb82abbb3fd455
Eric Yang <eric@junto.love>
2019-08-15 18:03:57 -0400

    Clean ExpressionOpenBottom

--- lib/screens/expression_open/expression_open.dart
+++ lib/screens/expression_open/expression_open.dart
@@ -23,176 +23,193 @@ class ExpressionOpen extends StatefulWidget {
 }
 
 class ExpressionOpenState extends State<ExpressionOpen> {
-  List comments;
-  bool _showCommentTextField = false;
-  bool _showReplies = false;
-  Widget _showRepliesText = Row(
-    children: <Widget>[
-      Text('SHOW REPLIES',
-          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
-      SizedBox(width: 5),
-      Icon(Icons.keyboard_arrow_down, size: 17, color: Color(0xff555555))
-    ],
-  );
+  // List comments;
+  // bool _showCommentTextField = false;
+  // bool _showReplies = false;
+  // Widget _showRepliesText = Row(
+  //   children: <Widget>[
+  //     Text('SHOW REPLIES',
+  //         style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+  //     SizedBox(width: 5),
+  //     Icon(Icons.keyboard_arrow_down, size: 17, color: Color(0xff555555))
+  //   ],
+  // );
    
 
   @override
   void initState() {
-    setState(() {
-      comments = widget.expression.comments;
-    });
+    // setState(() {
+    //   comments = widget.expression.comments;
+    // });
     
     super.initState();
   }   
-
 
   @override
   Widget build(BuildContext context) {
     TextEditingController commentController = TextEditingController();
  
     _buildExpression() {
-      if (widget.expression.expression['expression_type'] == 'longform') {
+      String expressionType = widget.expression.expression['entry']['expression_type'];
+
+      if (expressionType == 'longform') {
         return LongformOpen(widget.expression);
-      } else if (widget.expression.expression['expression_type'] ==
+      } else if (expressionType ==
           'shortform') {
         return ShortformOpen(widget.expression);
-      }
-    }
-
-
-    void _toggleReplies() {
-      if (_showReplies == false) {
-        setState(() {
-          _showReplies = true;
-          _showRepliesText = Row(
-            children: <Widget>[
-              Text('REPLIES',
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
-              SizedBox(width: 5),
-              Icon(Icons.keyboard_arrow_up, size: 17, color: Color(0xff555555))
-            ],
-          );
-        });
       } else {
-        setState(() {
-          _showReplies = false;
-          _showRepliesText = Row(
-            children: <Widget>[
-              Text('SHOW REPLIES',
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
-              SizedBox(width: 5),
-              Icon(Icons.keyboard_arrow_down,
-                  size: 17, color: Color(0xff555555))
-            ],
-          );
-        });
+        return SizedBox();
       }
     }
-    
 
-    return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(45.0), child: ExpressionOpenAppbar()),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: 
-              ListView(children: [
+    // void _toggleReplies() {
+    //   if (_showReplies == false) {
+    //     setState(() {
+    //       _showReplies = true;
+    //       _showRepliesText = Row(
+    //         children: <Widget>[
+    //           Text('REPLIES',
+    //               style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+    //           SizedBox(width: 5),
+    //           Icon(Icons.keyboard_arrow_up, size: 17, color: Color(0xff555555))
+    //         ],
+    //       );
+    //     });
+    //   } else {
+    //     setState(() {
+    //       _showReplies = false;
+    //       _showRepliesText = Row(
+    //         children: <Widget>[
+    //           Text('SHOW REPLIES',
+    //               style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+    //           SizedBox(width: 5),
+    //           Icon(Icons.keyboard_arrow_down,
+    //               size: 17, color: Color(0xff555555))
+    //         ],
+    //       );
+    //     });
+    //   }
+    // }
+
+      return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(45.0), child: ExpressionOpenAppbar()),    
+          backgroundColor: Colors.white,    
+        body: Column(children: <Widget>[
+          Expanded(
+            child: ListView(children: <Widget>[
                 ExpressionOpenTop(),
                 _buildExpression(),
-                ExpressionOpenBottom(
-                    channels: widget.expression.channels, time: '2'),
+                ExpressionOpenBottom(widget.expression)
 
-                // ExpressionOpenShowReplies(_toggleReplies, _showRepliesText),
+            ],)
+          )
+        ],)
+      );
 
-                    ListView(
-                      shrinkWrap: true,
-                      physics: ClampingScrollPhysics(),
-                      children: comments
-                          .map(
-                              (comment) => CommentPreview(comment))
-                          .toList(),
-                    )                                                    
-              ]),
-            ),
-            Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border(
-                        top: BorderSide(width: 1, color: Color(0xffeeeeee)))),
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      child: Row(children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.only(right: 10),
-                          child: ClipOval(
-                            child: Image.asset(
-                              'assets/images/junto-mobile__eric.png',
-                              height: 36.0,
-                              width: 36.0,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Container(
-                            decoration: BoxDecoration(
-                              color: Color(0xfff9f9f9),
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            width: MediaQuery.of(context).size.width - 135,
-                            constraints: BoxConstraints(
-                              maxHeight: 180
-                            ),                          
-                            child: TextField(
-                              controller: commentController,
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                // hintText: 'reply',
-                              ),
-                              maxLines: null,
-                              cursorColor: JuntoPalette.juntoGrey,
-                              cursorWidth: 2,
-                              style: TextStyle(fontSize: 17, color: Color(0xff333333)),
-                              textInputAction: TextInputAction.newline,
-                            ),
-                        ),                        
-                      ],)
-                    ),
+    // return Scaffold(
+    //   appBar: PreferredSize(
+    //       preferredSize: Size.fromHeight(45.0), child: ExpressionOpenAppbar()),
+    //   body: Container(
+    //     color: Colors.white,
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       children: <Widget>[
+    //         Expanded(
+    //           child: 
+    //           ListView(children: [
+    //             ExpressionOpenTop(),
+    //             _buildExpression(),
+    //             ExpressionOpenBottom(
+    //                 channels: widget.expression.channels, time: '2'),
 
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if(commentController.text != '') {
-                            comments.add(commentController.text);
-                          }
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          gradient: LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            stops: [0.1, 0.9],
-                            colors: [JuntoPalette.juntoBlue, JuntoPalette.juntoPurple]
-                          )
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: Text('REPLY', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 12))
-                      )
-                    )
-                  ],  
-                ))
-          ],
-        ),
-      ),
-      // bottomNavigationBar: ExpressionOpenBottomNav(_openComment)
-    );
+    //             // ExpressionOpenShowReplies(_toggleReplies, _showRepliesText),
+
+    //                 // ListView(
+    //                 //   shrinkWrap: true,
+    //                 //   physics: ClampingScrollPhysics(),
+    //                 //   children: comments
+    //                 //       .map(
+    //                 //           (comment) => CommentPreview(comment))
+    //                 //       .toList(),
+    //                 // )                                                    
+    //           ]),
+    //         ),
+    //         Container(
+    //             decoration: BoxDecoration(
+    //                 color: Colors.white,
+    //                 border: Border(
+    //                     top: BorderSide(width: 1, color: Color(0xffeeeeee)))),
+    //             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+    //             child: Row(
+    //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //               children: <Widget>[
+    //                 Container(
+    //                   child: Row(children: <Widget>[
+    //                     Container(
+    //                       margin: EdgeInsets.only(right: 10),
+    //                       child: ClipOval(
+    //                         child: Image.asset(
+    //                           'assets/images/junto-mobile__eric.png',
+    //                           height: 36.0,
+    //                           width: 36.0,
+    //                           fit: BoxFit.cover,
+    //                         ),
+    //                       ),
+    //                     ),
+    //                     Container(
+    //                         decoration: BoxDecoration(
+    //                           color: Color(0xfff9f9f9),
+    //                           borderRadius: BorderRadius.circular(10)
+    //                         ),
+    //                         width: MediaQuery.of(context).size.width - 135,
+    //                         constraints: BoxConstraints(
+    //                           maxHeight: 180
+    //                         ),                          
+    //                         child: TextField(
+    //                           controller: commentController,
+    //                           decoration: InputDecoration(
+    //                             border: InputBorder.none,
+    //                             // hintText: 'reply',
+    //                           ),
+    //                           maxLines: null,
+    //                           cursorColor: JuntoPalette.juntoGrey,
+    //                           cursorWidth: 2,
+    //                           style: TextStyle(fontSize: 17, color: Color(0xff333333)),
+    //                           textInputAction: TextInputAction.newline,
+    //                         ),
+    //                     ),                        
+    //                   ],)
+    //                 ),
+
+    //                 GestureDetector(
+    //                   onTap: () {
+    //                     // setState(() {
+    //                     //   if(commentController.text != '') {
+    //                     //     comments.add(commentController.text);
+    //                     //   }
+    //                     // });
+    //                   },
+    //                   child: Container(
+    //                     decoration: BoxDecoration(
+    //                       borderRadius: BorderRadius.circular(25),
+    //                       gradient: LinearGradient(
+    //                         begin: Alignment.centerLeft,
+    //                         end: Alignment.centerRight,
+    //                         stops: [0.1, 0.9],
+    //                         colors: [JuntoPalette.juntoBlue, JuntoPalette.juntoPurple]
+    //                       )
+    //                     ),
+    //                     padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+    //                     child: Text('REPLY', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 12))
+    //                   )
+    //                 )
+    //               ],  
+    //             ))
+    //       ],
+    //     ),
+    //   ),
+    //   // bottomNavigationBar: ExpressionOpenBottomNav(_openComment)
+    // );
   }
 }
--- lib/screens/expression_open/expression_open_bottom/expression_open_bottom.dart
+++ lib/screens/expression_open/expression_open_bottom/expression_open_bottom.dart
@@ -1,14 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 import '../../../custom_icons.dart';
 import './../expression_open_channel/expression_open_channel.dart';
 
 class ExpressionOpenBottom extends StatefulWidget {
-  final channels;
-  final String time;
+  final expression;
 
-  ExpressionOpenBottom({this.channels, this.time});
+  ExpressionOpenBottom(this.expression);
 
   @override
   State<StatefulWidget> createState() {
@@ -17,84 +15,29 @@ class ExpressionOpenBottom extends StatefulWidget {
 }
 
 class ExpressionOpenBottomState extends State<ExpressionOpenBottom> {
-  String _channelOne;
-  String _channelTwo;
-  String _channelThree;
 
-  @override
-  void initState() {
-    if(widget.channels == null) {
-      return;
-    } else if (widget.channels.length == 1) {
-      _channelOne = widget.channels[0];
-    } else if (widget.channels.length == 2 ) {
-      _channelOne = widget.channels[0];
-      _channelTwo = widget.channels[1];      
-    } else if(widget.channels.length == 3) {
-      _channelOne = widget.channels[0];
-      _channelTwo = widget.channels[1];
-      _channelThree = widget.channels[2];
-    }
-    
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
-    return 
-        Container(
-            margin: EdgeInsets.only(top: 7.5),
-            padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-            decoration: BoxDecoration(
-              border: Border(
-                bottom:
-                    BorderSide(color: Color(0xffeeeeee), width: .5),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [        
-                // _buildChannels(),
-                Row(children: <Widget>[
-                  // Container(
-                  //   margin: EdgeInsets.only(right: 15),
-                  //   child: Icon(Icons.face, size: 21),
-                  // ),
-                  // Container(child: Icon(CustomIcons.resonate, size: 21))
-
-                ],),
-
-                Container(
-                  margin: EdgeInsets.only(right: 10),
-                  child: Icon(CustomIcons.half_lotus, size: 15)
-                )
-              ]
-            ));  
-  }
-
-  _buildChannels() {
-    if (widget.channels == null) {
-      return Container(height: 0, width: 0);
-    } else {
-      return 
-        Container(
-          margin: EdgeInsets.only(bottom: 2.5),
-          child: 
-            Row(
-              children: [
-                _buildChannel(_channelOne),                
-                _channelTwo != null ? _buildChannel(_channelTwo) : SizedBox(),
-                _channelThree != null ? _buildChannel(_channelThree) : SizedBox()
-              ]
-            ),
-        );      
-    }
-  }      
-
-  _buildChannel(channel) {
     return Container(
-      margin: EdgeInsets.only(right: 5),
-      child: Text('#' + channel, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w700))
-      );      
+        margin: EdgeInsets.only(top: 7.5),
+        padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Color(0xffeeeeee), width: .5),
+          ),
+        ),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: <Widget>[
+                  SizedBox()
+                ],
+              ),
+              Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: Icon(CustomIcons.half_lotus, size: 15))
+            ]));
   }
 }
