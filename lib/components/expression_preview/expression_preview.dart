e15d0fe40f1f45e830ec5ee8aca1dbd199d91cac
Eric Yang <eric@junto.love>
2019-08-15 15:37:52 -0400

    Refactor LongformPreview widget

--- lib/components/expression_preview/expression_preview.dart
+++ lib/components/expression_preview/expression_preview.dart
@@ -2,13 +2,11 @@
 import 'package:flutter/material.dart';
 
 import './preview_profile.dart';
-import './preview_bottom.dart';
+import './preview_bottom.dart'; 
 import './longform_preview/longform_preview.dart';
 import './shortform_preview/shortform_preview.dart';
-import './bullet_preview/bullet_preview.dart';
 import './photo_preview/photo_preview.dart';
 import './event_preview/event_preview.dart';
-import './music_preview/music_preview.dart';
 import '../../screens/expression_open/expression_open.dart';
 
 /// Renders a concise overview of one given [Expression]. 
@@ -48,21 +46,16 @@ class ExpressionPreview extends StatelessWidget {
   }
 
   Widget _returnExpression() { 
-    // return Container(child: Text(expression.expression['entry']['expression']['title']));
     if (expression.expression['entry']['expression_type'] == 'longform') {
       return LongformPreview(expression);
     } 
     
-    // else if(expression.expression['expression_type'] == 'shortform') {
-    //   return ShortformPreview(expression.expression['expression_data']['ShortForm']['background'], expression.expression['expression_data']['ShortForm']['body']);
-    // } else if(expression.expression['expression_type'] == 'bulletform') {
-    //   return BulletPreview();
-    // } else if(expression.expression['expression_type'] == 'photoform') {
-    //   return PhotoPreview(expression.image, expression.imageCaption);
-    // } else if(expression.expression['expression_type'] == 'eventform') {
-    //   return EventPreview(expression.title, expression.location, expression.image);
-    // } else if(expression.expression['expression_type'] == 'musicform') {
-    //   return MusicPreview();
+    // else if(expression.expression['entry']['expression_type'] == 'shortform') {
+    //   return ShortformPreview(expression);
+    // } else if(expression.expression['entry']['expression_type'] == 'photoform') {
+    //   return PhotoPreview(expression);
+    // } else if(expression.expression['entry']['expression_type'] == 'eventform') {
+    //   return EventPreview(expression);
     // } else {
     //   return Container(width: 0, height: 0,);
     // }
--- lib/components/expression_preview/longform_preview/longform_preview.dart
+++ lib/components/expression_preview/longform_preview/longform_preview.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import './../../../typography/style.dart';
@@ -8,36 +7,18 @@ class LongformPreview extends StatelessWidget {
 
   LongformPreview(this.expression);
 
-  _buildTitle() {    
-    String expressionTitle = expression.expression['entry']['expression']['title'];
-
-    if(expressionTitle != '') {
-      return 
-          Container(
-            child: Text(
-              expressionTitle,
-              textAlign: TextAlign.left,
-              style: JuntoStyles.longformTitle,
-            ),
-          );     
-    } else {
-      return SizedBox();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return 
-    Container(
+    String expressionBody = expression.expression['entry']['expression']['body'];
+
+    return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-
           _buildTitle(),
-    
           Text(
-          'hello',
+            expressionBody,
             textAlign: TextAlign.left,
             maxLines: 4,
             overflow: TextOverflow.ellipsis,
@@ -47,4 +28,20 @@ class LongformPreview extends StatelessWidget {
       ),
     );
   }
+
+  _buildTitle() {
+    String expressionTitle =
+        expression.expression['entry']['expression']['title'];
+    if (expressionTitle != '') {
+      return Container(
+        child: Text(
+          expressionTitle,
+          textAlign: TextAlign.left,
+          style: JuntoStyles.longformTitle,
+        ),
+      );
+    } else {
+      return SizedBox();
+    }
+  }  
 }
