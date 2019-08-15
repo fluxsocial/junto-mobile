e68b07395ebc5240717a6e030244fe07f8e3a9ad
Nash Ramdial <mail@neevash.dev>
2019-08-15 13:54:46 -0400

    Replaced image picker modern with image picker

--- lib/screens/create/photo/photo.dart
+++ lib/screens/create/photo/photo.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker_modern/image_picker_modern.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 
 import './../create_actions.dart';
