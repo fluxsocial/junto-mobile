f08e71faaf74903ad6ecab7485a50b22fe8cb7a0
Nash Ramdial <mail@neevash.dev>
2019-08-20 15:52:55 -0400

    completed emoji selector

---
+++ lib/components/emoji_selector/emoji_selector.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/models/emoji_model.dart';
+
+class EmojiSelector extends StatelessWidget {
+  const EmojiSelector({
+    Key key,
+    @required this.parsedEmoji,
+    @required this.onEmojiSelected,
+  }) : super(key: key);
+
+  final List<Emoji> parsedEmoji;
+  final ValueChanged<Emoji> onEmojiSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.all(12.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 6,
+          crossAxisSpacing: 6.0,
+          mainAxisSpacing: 6.0,
+        ),
+        itemCount: parsedEmoji.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () => onEmojiSelected(parsedEmoji[index]),
+            child: Text(
+              parsedEmoji[index].code,
+              style: TextStyle(
+                fontSize: 26.0,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/screens/expression_open/expression_open_interactions/expression_open_interactions.dart
+++ lib/screens/expression_open/expression_open_interactions/expression_open_interactions.dart
@@ -2,9 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:junto_beta_mobile/components/emoji_selector/emoji_selector.dart';
+import 'package:junto_beta_mobile/models/emoji_model.dart';
 
 class ExpressionOpenInteractions extends StatelessWidget {
   final ValueNotifier<String> responses = ValueNotifier<String>('Respond');
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -41,56 +44,28 @@ class ExpressionOpenInteractions extends StatelessWidget {
   Future<void> _buildRespondModal(BuildContext context) async {
     final List<Emoji> parsedEmoji = <Emoji>[];
 
-    // Emojis sourced from https://github.com/omnidan/node-emoji/blob/master/lib/emoji.json
+    // Emoji sourced from https://github.com/omnidan/node-emoji/blob/master/lib/emoji.json
     final String emojiJson = await rootBundle.loadString('assets/emoji.json');
     final Map<String, dynamic> emoji = json.decode(emojiJson);
-    emoji.forEach((String key, dynamic value) {
-      parsedEmoji.add(
-        Emoji(key, value),
-      );
-    });
+    emoji.forEach(
+      (String key, dynamic value) {
+        parsedEmoji.add(
+          Emoji(key, value),
+        );
+      },
+    );
 
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (BuildContext context, StateSetter state) {
-            return Container(
-              color: Colors.white,
-              margin: const EdgeInsets.all(12.0),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 6,
-                  crossAxisSpacing: 6.0,
-                  mainAxisSpacing: 6.0,
-                ),
-                itemCount: parsedEmoji.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () {
-                      responses.value = parsedEmoji[index].code;
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(parsedEmoji[index].code),
-                  );
-                },
-              ),
-            );
+        return EmojiSelector(
+          parsedEmoji: parsedEmoji,
+          onEmojiSelected: (Emoji emoji) {
+            responses.value = emoji.code;
+            Navigator.of(context).pop();
           },
         );
       },
     );
-  }
-}
-
-class Emoji {
-  const Emoji(this.name, this.code);
-
-  final String name;
-  final String code;
-
-  @override
-  String toString() {
-    return 'Emoji{name="$name",  code="$code"}';
   }
 }
