0a9a605aefda6588f06fc772e54245aeff36bfa3
Nash Ramdial <mail@neevash.dev>
2019-08-21 17:04:19 -0400

    Created model to decode perspective response

--- lib/models/perspective.dart
+++ lib/models/perspective.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Perspective {
   const Perspective(this.name);
 
@@ -21,8 +23,55 @@ class Perspective {
   }
 
   Map<String, dynamic> toMap() {
-    return <String, String>{
+    return <String, dynamic>{
       'name': name,
     };
   }
+}
+
+/// Model containing the [address], [parent], [name], [privacy] and
+/// [channelType] of the given Perspective.
+class PerspectiveResponse {
+  PerspectiveResponse({
+    @required this.address,
+    @required this.parent,
+    @required this.name,
+    @required this.privacy,
+    @required this.channelType,
+  });
+
+  /// Creates a [PerspectiveResponse] from the decoded json data.
+  factory PerspectiveResponse.fromMap(Map<String, dynamic> json) {
+    return PerspectiveResponse(
+      address: json['address'],
+      parent: json['entry']['parent'],
+      name: json['entry']['name'],
+      privacy: json['entry']['privacy'],
+      channelType: json['entry']['channel_type'],
+    );
+  }
+
+  /// Address of the given [Perspective] on the server
+  final String address;
+
+  /// The object's parent
+  final String parent;
+
+  /// Name of the [Perspective]
+  final String name;
+
+  /// Whether this [Perspective] is open or private.
+  final String privacy;
+
+  /// String identifying the type of channel. `Perspective`
+  final String channelType;
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+        'entry': <String, dynamic>{
+          'parent': parent,
+          'name': name,
+          'privacy': privacy,
+          'channel_type': channelType,
+        }
+      };
 }
