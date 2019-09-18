41eba9724f858a343223840117dee983d9200b18
Eric Yang <eric@junto.love>
2019-09-18 18:25:34 +0100

    Link member appbar to more action items

--- lib/screens/member/member_appbar.dart
+++ lib/screens/member/member_appbar.dart
@@ -42,7 +42,7 @@ class MemberAppbar extends StatelessWidget {
                   ),
                 ),
               ),
-              GestureDetector(                
+              GestureDetector(
                 onTap: () {
                   MemberActionItems().buildMemberActionItems(context);
                 },
