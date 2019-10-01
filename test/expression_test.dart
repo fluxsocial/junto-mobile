21dd9f67f0d0e39b0f96d04e246d7cddf90c2387
Nash Ramdial <mail@neevash.dev>
2019-10-01 14:21:04 -0400

    added test for remaining expression functions

--- test/expression_test.dart
+++ test/expression_test.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:junto_beta_mobile/API.dart';
 import 'package:junto_beta_mobile/models/expression.dart';
+import 'package:junto_beta_mobile/models/resonation_model.dart';
+import 'package:junto_beta_mobile/models/user_model.dart';
 import 'package:junto_beta_mobile/providers/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,7 +13,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = null;
-  final CollectiveProvider _collectiveProvider = CollectiveProviderCentralized();
+  final CollectiveProvider _collectiveProvider =
+      CollectiveProviderCentralized();
 
   setUpAll(() {
     SharedPreferences.setMockInitialValues({
@@ -21,7 +24,8 @@ void main() {
 
   group('Create Expression', () {
     test('Create LongForm Expression ', () async {
-      final CentralizedExpressionResponse _response = await _collectiveProvider.createExpression(
+      final CentralizedExpressionResponse _response =
+          await _collectiveProvider.createExpression(
         CentralizedExpression(
           type: 'LongForm',
           expressionData: CentralizedLongFormExpression(
@@ -29,7 +33,9 @@ void main() {
             body: 'Hi there',
           ).toMap(),
           context: <String, dynamic>{
-            'Group': <String, dynamic>{'address': 'd7f5186b-0281-4723-b5d4-1ff24eb0beb2'}
+            'Group': <String, dynamic>{
+              'address': 'd7f5186b-0281-4723-b5d4-1ff24eb0beb2'
+            }
           },
         ),
       );
@@ -38,7 +44,8 @@ void main() {
       expect(_response.creator, isNotNull);
     });
     test('Create ShortForm Expression ', () async {
-      final CentralizedExpressionResponse _response = await _collectiveProvider.createExpression(
+      final CentralizedExpressionResponse _response =
+          await _collectiveProvider.createExpression(
         CentralizedExpression(
           type: 'ShortForm',
           expressionData: CentralizedShortFormExpression(
@@ -46,7 +53,9 @@ void main() {
             body: 'Test ShortForm expression',
           ).toMap(),
           context: <String, dynamic>{
-            'Group': <String, dynamic>{'address': 'd7f5186b-0281-4723-b5d4-1ff24eb0beb2'}
+            'Group': <String, dynamic>{
+              'address': 'd7f5186b-0281-4723-b5d4-1ff24eb0beb2'
+            }
           },
         ),
       );
@@ -57,11 +66,26 @@ void main() {
   });
 
   group('Expression Interaction', () {
+    /// Returns the test user expression
     test('Get User Expression', () async {
-      final CentralizedExpressionResponse _response =
-          await _collectiveProvider.getExpression('9f878873-3ad8-45e2-8f1b-5c3673f73e27');
+      final CentralizedExpressionResponse _response = await _collectiveProvider
+          .getExpression('9f878873-3ad8-45e2-8f1b-5c3673f73e27');
       expect(_response.type, 'ShortForm');
       expect(_response.creator.address, '85235b21-1725-4e89-b6fa-305df7978e52');
+    });
+
+    /// Ensure the value sent back is not null
+    test('Get Expression Comments', () async {
+      final List<Comment> _response = await _collectiveProvider
+          .getExpressionsComments('9f878873-3ad8-45e2-8f1b-5c3673f73e27');
+      expect(_response, isNotNull);
+    });
+
+    /// Ensure the value sent back is not null
+    test('Get Expression Resonation', () async {
+      final List<UserProfile> _response = await _collectiveProvider
+          .getExpressionsResonation('9f878873-3ad8-45e2-8f1b-5c3673f73e27');
+      expect(_response, isNotNull);
     });
   });
 }
