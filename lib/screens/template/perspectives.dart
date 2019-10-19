6d5b49df22d2f34a231ba1595c6b38f18703b3ab
Eric Yang <eric@junto.love>
2019-10-18 21:27:08 -0400

    Clean up code

--- lib/screens/template/perspectives.dart
+++ lib/screens/template/perspectives.dart
@@ -6,7 +6,6 @@ import 'package:junto_beta_mobile/styles.dart';
 class JuntoPerspectives extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return JuntoPerspectivesState();
   }
 }
@@ -14,7 +13,6 @@ class JuntoPerspectives extends StatefulWidget {
 class JuntoPerspectivesState extends State<JuntoPerspectives> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: Container(
           width: MediaQuery.of(context).size.width * .9,
@@ -35,8 +33,8 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
               children: <Widget>[
                 Container(
                   height: 45,
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: const BoxDecoration(
                     border: Border(
                       bottom: BorderSide(
                         color: Color(0xff4263A3),
@@ -49,50 +47,52 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
                     children: <Widget>[
                       Image.asset('assets/images/junto-mobile__logo--white.png',
                           height: 22.0, width: 22.0),
-                      SizedBox(width: 15),
-                      Container(
-                        width: MediaQuery.of(context).size.width * .72,
-                        alignment: Alignment.centerLeft,
-                        height: 33,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        decoration: BoxDecoration(
-                          color: Color(0xff4263A3),
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(Icons.search, size: 17, color: Colors.white),
-                            SizedBox(width: 5),
-                            Transform.translate(
-                              offset: Offset(0.0, 2.5),
-                              child: Container(
-                                width: MediaQuery.of(context).size.width * .72 -
-                                    45,
-                                child: TextField(
-                                  buildCounter: (
-                                    BuildContext context, {
-                                    int currentLength,
-                                    int maxLength,
-                                    bool isFocused,
-                                  }) =>
-                                      null,
-                                  decoration: const InputDecoration(
-                                    contentPadding: EdgeInsets.all(0),
-                                    border: InputBorder.none,
+                      const SizedBox(width: 15),
+                      Expanded(
+                        child: Container(
+                          alignment: Alignment.centerLeft,
+                          height: 33,
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            color: const Color(0xff4263A3),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.search, size: 17, color: Colors.white),
+                              const SizedBox(width: 5),
+                              Transform.translate(
+                                offset: const Offset(0.0, 2.5),
+                                child: Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * .72 -
+                                          45,
+                                  child: TextField(
+                                    buildCounter: (
+                                      BuildContext context, {
+                                      int currentLength,
+                                      int maxLength,
+                                      bool isFocused,
+                                    }) =>
+                                        null,
+                                    decoration: const InputDecoration(
+                                      contentPadding: EdgeInsets.all(0),
+                                      border: InputBorder.none,
+                                    ),
+                                    cursorColor: Colors.white,
+                                    cursorWidth: 2,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                        color: Color(0xffffffff),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500),
+                                    maxLength: 80,
+                                    textInputAction: TextInputAction.done,
                                   ),
-                                  cursorColor: Colors.white,
-                                  cursorWidth: 2,
-                                  maxLines: 1,
-                                  style: const TextStyle(
-                                      color: Color(0xffffffff),
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500),
-                                  maxLength: 80,
-                                  textInputAction: TextInputAction.done,
                                 ),
-                              ),
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       )
                     ],
@@ -101,9 +101,9 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
                 Expanded(
                   child: ListView(
                     children: <Widget>[
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       Container(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
@@ -120,7 +120,7 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
                           ],
                         ),
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       _buildPerspective('JUNTO', 'all'),
                       _buildPerspective('Connections', '99'),
                       _buildPerspective('Subscriptions', '220'),
@@ -133,33 +133,27 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
     );
   }
 
-  _buildPerspective(name, members) {
+  _buildPerspective(String name, String members) {
     return Container(
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Row(
             children: <Widget>[
               Container(
-                // padding: EdgeInsets.only(left: 15),
-                // decoration: BoxDecoration(
-                //   border: Border(
-                //     left: BorderSide(color: Color(0xffeeeeee), width: 1.5),
-                //   ),
-                // ),
                 child: Row(
                   children: <Widget>[
                     Container(
                       height: 30,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         border: Border(
                           left:
                               BorderSide(color: Color(0xffeeeeee), width: 1.5),
                         ),
                       ),
                     ),
-                    SizedBox(width: 10),
+                    const SizedBox(width: 10),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
@@ -171,7 +165,7 @@ class JuntoPerspectivesState extends State<JuntoPerspectives> {
                               letterSpacing: 1.2,
                               color: Colors.white),
                         ),
-                        SizedBox(height: 5),
+                        const SizedBox(height: 5),
                         Text(
                           members + ' members',
                           style: TextStyle(
