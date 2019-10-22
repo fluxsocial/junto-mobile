4457c3d8557f138e2c7750ba2272c4ea18ad5db4
Nash Ramdial <mail@neevash.dev>
2019-10-21 21:48:10 -0400

    support platform specific dialog

--- lib/utils/junto_dialog.dart
+++ lib/utils/junto_dialog.dart
@@ -1,25 +1,36 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:junto_beta_mobile/styles.dart';
 
 class JuntoDialog {
-  static void showJuntoDialog(
-      BuildContext context, String body, List<Widget> actions) {
+  static void showJuntoDialog(BuildContext context, String body, List<Widget> actions) {
     showDialog(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext context) {
         return Theme(
           data: ThemeData(dialogBackgroundColor: Colors.white),
-          child: CupertinoAlertDialog(
-            content: Text(
-              body,
-              style: JuntoStyles.lotusLongformBody,
-            ),
-            actions: <Widget>[
-              if (actions.isNotEmpty) ...actions else const SizedBox(),
-            ],
-          ),
+          child: Platform.isAndroid
+              ? AlertDialog(
+                  content: Text(
+                    body,
+                    style: JuntoStyles.lotusLongformBody,
+                  ),
+                  actions: <Widget>[
+                    if (actions.isNotEmpty) ...actions else const SizedBox(),
+                  ],
+                )
+              : CupertinoAlertDialog(
+                  content: Text(
+                    body,
+                    style: JuntoStyles.lotusLongformBody,
+                  ),
+                  actions: <Widget>[
+                    if (actions.isNotEmpty) ...actions else const SizedBox(),
+                  ],
+                ),
         );
       },
     );
