886a06b914a2ef443fa7e78081ec789a2a2225f6
Eric Yang <eric@junto.love>
2019-10-29 15:06:24 -0400

    Build expression context modal UI

--- lib/screens/expression_open/expression_open_appbar.dart
+++ lib/screens/expression_open/expression_open_appbar.dart
@@ -13,14 +13,13 @@ class ExpressionOpenAppbar extends StatelessWidget {
       elevation: 0,
       titleSpacing: 0,
       title: Container(
-        // padding: const EdgeInsets.only(right: 10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             GestureDetector(
                 onTap: () => Navigator.pop(context),
                 child: Container(
-                  padding: EdgeInsets.only(left: 10),
+                  padding: const EdgeInsets.only(left: 10),
                   color: Colors.transparent,
                   width: 42,
                   alignment: Alignment.centerLeft,
@@ -36,50 +35,13 @@ class ExpressionOpenAppbar extends StatelessWidget {
                   isScrollControlled: true,
                   context: context,
                   builder: (BuildContext context) {
-                    return Container(
-                      color: const Color(0xff737373),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height * .9,
-                        padding: const EdgeInsets.all(10),
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10),
-                          ),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const <Widget>[
-                                Text(
-                                  'Expression context',
-                                  style: TextStyle(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.w700,
-                                    color: Color(0xff333333),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            const Text(
-                              'This will show the context of each expression, and the icon that you pressed to open this modal up will be dynamic. For example, for a given expression this would show that it resides in the collective and is tagged into channels x, y, z.'
-                              ' Expressions that live in the collective context will have the collective icon (tbd). Those that live in spheres, pack, den will reflect the icons that represent those contexts as well. Hope this makes sense!',
-                              style: TextStyle(fontSize: 15, height: 1.4),
-                            )
-                          ],
-                        ),
-                      ),
-                    );
+                    return ExpressionOpenContext();
                   },
                 );
               },
               child: Container(
                 color: Colors.transparent,
-                padding: EdgeInsets.only(right: 10),
+                padding: const EdgeInsets.only(right: 10),
                 width: 42,
                 alignment: Alignment.centerRight,
                 child: const Icon(
@@ -107,6 +69,127 @@ class ExpressionOpenAppbar extends StatelessWidget {
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExpressionOpenContext extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff737373),
+      child: Container(
+        height: MediaQuery.of(context).size.height * .5,
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 10),
+            Text(
+              'CONTEXT',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+                color: const Color(0xff555555),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  height: 38.0,
+                  width: 38.0,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      stops: const <double>[0.3, 0.9],
+                      colors: <Color>[
+                        JuntoPalette.juntoSecondary,
+                        JuntoPalette.juntoPrimary,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: const Icon(
+                    CustomIcons.lotus,
+                    color: Colors.white,
+                    size: 15,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  "shared to 'Collective'",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xff333333),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  height: 38.0,
+                  width: 38.0,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      stops: const <double>[0.3, 0.9],
+                      colors: <Color>[
+                        JuntoPalette.juntoSecondary,
+                        JuntoPalette.juntoPrimary,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Icon(
+                    CustomIcons.hash,
+                    color: Colors.white,
+                    size: 15,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Flexible(
+                  child: Text(
+                    'tagged in #sustainability, #permaculture, #design',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xff333333),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'INTENTION',
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: const Color(0xff555555)),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Looking for feedback!',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            )
+          ],
         ),
       ),
     );
