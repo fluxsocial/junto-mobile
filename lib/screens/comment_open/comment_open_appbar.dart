9d2edef33c9909800b7f97d647675cd400a91205
Eric Yang <eric@junto.love>
2019-11-04 16:00:29 -0500

    Add theming to comment open

--- lib/screens/comment_open/comment_open_appbar.dart
+++ lib/screens/comment_open/comment_open_appbar.dart
@@ -9,11 +9,9 @@ class CommentOpenAppbar extends StatelessWidget {
       automaticallyImplyLeading: false,
       brightness: Brightness.light,
       iconTheme: const IconThemeData(color: JuntoPalette.juntoSleek),
-      backgroundColor: Colors.white,
       elevation: 0,
       titleSpacing: 0,
       title: Container(
-        // padding: const EdgeInsets.only(right: 10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
@@ -26,45 +24,19 @@ class CommentOpenAppbar extends StatelessWidget {
                 alignment: Alignment.centerLeft,
                 child: Icon(
                   CustomIcons.back,
-                  color: JuntoPalette.juntoGrey,
+                  color: Theme.of(context).primaryColorDark,
                   size: 17,
                 ),
               ),
             ),
-            // GestureDetector(
-            //   onTap: () {
-
-            //   },
-            //   child: Container(
-            //     color: Colors.transparent,
-            //     padding: EdgeInsets.only(right: 10),
-            //     width: 42,
-            //     alignment: Alignment.centerRight,
-            //     child: const Icon(
-            //       CustomIcons.lotus,
-            //       size: 20,
-            //       color: Color(0xff333333),
-            //     ),
-            //   ),
-            // )
           ],
         ),
       ),
       bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(1),
+        preferredSize: const Size.fromHeight(.75),
         child: Container(
           height: .75,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              stops: <double>[0.1, 0.9],
-              colors: <Color>[
-                JuntoPalette.juntoFade,
-                JuntoPalette.juntoFade,
-              ],
-            ),
-          ),
+          decoration: BoxDecoration(color: Theme.of(context).dividerColor),
         ),
       ),
     );
