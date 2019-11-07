176de716c153e39262eb416a7ff5a1f31b62ed05
Eric Yang <eric@junto.love>
2019-11-07 00:39:50 -0500

    Refactor and add theming to pack open

--- lib/screens/packs/pack_open/pack_open.dart
+++ lib/screens/packs/pack_open/pack_open.dart
@@ -24,7 +24,7 @@ class PackOpen extends StatefulWidget {
 class PackOpenState extends State<PackOpen> {
   // Controller for PageView
   PageController controller;
-  bool publicActive = true;
+  int _currentIndex = 0;
   final ValueNotifier<bool> _isVisible = ValueNotifier<bool>(true);
 
   @override
@@ -44,36 +44,99 @@ class PackOpenState extends State<PackOpen> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(45),
-          child: PackOpenAppbar(
-            pack: widget.pack
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(45),
+            child: PackOpenAppbar(pack: widget.pack),
           ),
-        ),
-        floatingActionButton: ValueListenableBuilder<bool>(
-          valueListenable: _isVisible,
-          builder: (BuildContext context, bool visible, Widget child) {
-            return AnimatedOpacity(
-              duration: const Duration(milliseconds: 300),
-              opacity: visible ? 1.0 : 0.0,
-              child: child,
-            );
-          },
-          child: ExpressionCenterFAB(expressionLayer: 'my pack'),
-        ),
-        endDrawer: PackDrawer(
-          pack: widget.pack,
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: PackOpenPublic(
-                fabVisible: _isVisible,
+          floatingActionButton: ValueListenableBuilder<bool>(
+            valueListenable: _isVisible,
+            builder: (BuildContext context, bool visible, Widget child) {
+              return AnimatedOpacity(
+                duration: const Duration(milliseconds: 300),
+                opacity: visible ? 1.0 : 0.0,
+                child: child,
+              );
+            },
+            child: const ExpressionCenterFAB(expressionLayer: 'my pack'),
+          ),
+          endDrawer: PackDrawer(
+            pack: widget.pack,
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                        color: Theme.of(context).dividerColor, width: .75),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width * .5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(CustomIcons.lotus,
+                              size: 20,
+                              color: _currentIndex == 0
+                                  ? Theme.of(context).primaryColorDark
+                                  : Theme.of(context).primaryColorLight),
+                          const SizedBox(width: 10),
+                          Text('Public',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                  color: _currentIndex == 0
+                                      ? Theme.of(context).primaryColorDark
+                                      : Theme.of(context).primaryColorLight))
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * .5,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(CustomIcons.packs,
+                              size: 17,
+                              color: _currentIndex == 1
+                                  ? Theme.of(context).primaryColorDark
+                                  : Theme.of(context).primaryColorLight),
+                          const SizedBox(width: 10),
+                          Text('Pack',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                  color: _currentIndex == 1
+                                      ? Theme.of(context).primaryColorDark
+                                      : Theme.of(context).primaryColorLight))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+              Expanded(
+                child: PageView(
+                  controller: controller,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                  children: <Widget>[
+                    PackOpenPublic(fabVisible: _isVisible),
+                    Center(
+                      child: Text('private pack expressions'),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
