3c21803c79e0b574afaf9a6303aac4e59cc0d942
Neevash Ramdial <mail@neevash.dev>
2019-11-24 21:30:53 -0400

    experimental implementation of querying

--- lib/screens/collective/collective.dart
+++ lib/screens/collective/collective.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:junto_beta_mobile/backend/repositories.dart';
 import 'package:junto_beta_mobile/models/expression.dart';
 import 'package:junto_beta_mobile/widgets/previews/expression_preview/expression_preview.dart';
@@ -32,9 +31,10 @@ class JuntoCollectiveState extends State<JuntoCollective> with HideFab {
     _collectiveController = ScrollController();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _collectiveController.addListener(_onScrollingHasChanged);
-      _collectiveController.position.isScrollingNotifier.addListener(
-        _onScrollingHasChanged,
-      );
+      if (_collectiveController.hasClients)
+        _collectiveController.position.isScrollingNotifier.addListener(
+          _onScrollingHasChanged,
+        );
     });
   }
 
@@ -75,45 +75,83 @@ class JuntoCollectiveState extends State<JuntoCollective> with HideFab {
   Widget build(BuildContext context) {
     return Container(
       color: Theme.of(context).colorScheme.background,
-      child: ListView(
-        controller: _collectiveController,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width * .5,
-                padding: const EdgeInsets.only(left: 10, right: 5, top: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    for (int index = 0; index < initialData.length + 1; index++)
-                      if (index == initialData.length)
-                        const SizedBox()
-                      else if (index.isEven)
-                        ExpressionPreview(expression: initialData[index])
-                  ],
+      child: FutureBuilder<List<CentralizedExpressionResponse>>(
+        future:
+            Provider.of<ExpressionRepo>(context).getCollectiveExpressions(null),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<List<CentralizedExpressionResponse>> snapshot,
+        ) {
+          if (snapshot.hasError) {
+            return Container(
+              child: const Text('Error occured'),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Container(
+              child: SizedBox.fromSize(
+                size: const Size.fromHeight(25.0),
+                child: const Center(
+                  child: CircularProgressIndicator(),
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * .5,
-                padding: const EdgeInsets.only(left: 5, right: 10, top: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    for (int index = 0; index < initialData.length + 1; index++)
-                      if (index == initialData.length)
-                        const SizedBox()
-                      else if (index.isOdd)
-                        ExpressionPreview(expression: initialData[index])
-                  ],
-                ),
-              ),
-            ],
-          )
-        ],
+            );
+          }
+          return ListView.builder(
+            controller: _collectiveController,
+            itemCount: snapshot.data.length,
+            cacheExtent: MediaQuery.of(context).size.height * 1.5,
+            itemBuilder: (BuildContext context, int index) {
+              return ExpressionPreview(expression: snapshot.data[index]);
+            },
+          );
+        },
       ),
     );
   }
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      color: Theme.of(context).colorScheme.background,
+//      child: ListView(
+//        controller: _collectiveController,
+//        children: <Widget>[
+//          Row(
+//            mainAxisAlignment: MainAxisAlignment.start,
+//            crossAxisAlignment: CrossAxisAlignment.start,
+//            children: <Widget>[
+//              Container(
+//                width: MediaQuery.of(context).size.width * .5,
+//                padding: const EdgeInsets.only(left: 10, right: 5, top: 10),
+//                child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.start,
+//                  children: <Widget>[
+//                    for (int index = 0; index < initialData.length + 1; index++)
+//                      if (index == initialData.length)
+//                        const SizedBox()
+//                      else if (index.isEven)
+//                        ExpressionPreview(expression: initialData[index])
+//                  ],
+//                ),
+//              ),
+//              Container(
+//                width: MediaQuery.of(context).size.width * .5,
+//                padding: const EdgeInsets.only(left: 5, right: 10, top: 10),
+//                child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.start,
+//                  children: <Widget>[
+//                    for (int index = 0; index < initialData.length + 1; index++)
+//                      if (index == initialData.length)
+//                        const SizedBox()
+//                      else if (index.isOdd)
+//                        ExpressionPreview(expression: initialData[index])
+//                  ],
+//                ),
+//              ),
+//            ],
+//          )
+//        ],
+//      ),
+//    );
+//  }
 }
