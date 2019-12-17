92f74b2cb47dc573fa933f519a76a3b731aa1f5a
Neevash Ramdial <mail@neevash.dev>
2019-12-17 11:08:32 -0400

    Mock data (#222)
    * mock user and sphere data
    
    * mock auth and user data
    
    * added driver and mock backend

---
+++ lib/backend/mock/mock_auth.dart
@@ -0,0 +1,22 @@
+import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/backend/mock/mock_data.dart';
+import 'package:junto_beta_mobile/models/user_model.dart';
+
+class MockAuth implements AuthenticationService {
+  @override
+  Future<UserData> loginUser(UserAuthLoginDetails details) async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    return kUserData;
+  }
+
+  @override
+  Future<void> logoutUser() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+  }
+
+  @override
+  Future<UserData> registerUser(UserAuthRegistrationDetails details) async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    return kUserData;
+  }
+}
