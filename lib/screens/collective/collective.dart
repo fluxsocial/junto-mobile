2b40d32554c8137206c09ac408385f9aa050f11b
Neevash Ramdial <mail@neevash.dev>
2019-12-19 20:47:38 -0400

    Add refresh indicator to home

--- lib/screens/collective/collective.dart
+++ lib/screens/collective/collective.dart
@@ -73,6 +73,10 @@ class JuntoCollectiveState extends State<JuntoCollective>
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    refreshData();
+  }
+
+  void refreshData() {
     _expressionProvider = Provider.of<ExpressionRepo>(context);
     _expressionCompleter =
         getCollectiveExpressions(contextType: 'Collective', paginationPos: 0);
@@ -133,10 +137,10 @@ class JuntoCollectiveState extends State<JuntoCollective>
   void _onDragUpdate(DragUpdateDetails details) {
     if (_dx == 0.0 &&
         details.globalPosition.dy != 0.0 &&
-        details.delta.direction > 0) {
+        details.delta.direction > 0.5) {
       return;
     } else {
-      if (details.globalPosition.dx > 0 &&
+      if (details.globalPosition.dx > 10 &&
           details.globalPosition.dx < MediaQuery.of(context).size.width * .9) {
         setState(() {
           _dx = details.globalPosition.dx;
@@ -208,105 +212,110 @@ class JuntoCollectiveState extends State<JuntoCollective>
       ),
 
       // dynamically render body
-      body: FutureBuilder<QueryExpressionResults>(
-        future: _expressionCompleter,
-        builder: (
-          BuildContext context,
-          AsyncSnapshot<QueryExpressionResults> snapshot,
-        ) {
-          if (snapshot.hasError) {
-            print('Error: ${snapshot.error}');
-            return const Center(
-              child: Text('hmm, something is up with our servers'),
-            );
-          }
-          if (snapshot.hasData) {
-            return CustomScrollView(
-              controller: _collectiveController,
-              slivers: <Widget>[
-                SliverPersistentHeader(
-                  delegate: CollectiveAppBar(
-                    expandedHeight: _showDegrees == true ? 135 : 85,
-                    degrees: _showDegrees,
-                    currentDegree: currentDegree,
-                    switchDegree: _switchDegree,
-                    appbarTitle: _appbarTitle,
-                    openPerspectivesDrawer: _openPerspectivesDrawer,
+      body: RefreshIndicator(
+        onRefresh: () async => refreshData,
+        child: FutureBuilder<QueryExpressionResults>(
+          future: _expressionCompleter,
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<QueryExpressionResults> snapshot,
+          ) {
+            if (snapshot.hasError) {
+              print('Error: ${snapshot.error}');
+              return const Center(
+                child: Text('hmm, something is up with our servers'),
+              );
+            }
+            if (snapshot.hasData) {
+              return CustomScrollView(
+                controller: _collectiveController,
+                slivers: <Widget>[
+                  SliverPersistentHeader(
+                    delegate: CollectiveAppBar(
+                      expandedHeight: _showDegrees == true ? 135 : 85,
+                      degrees: _showDegrees,
+                      currentDegree: currentDegree,
+                      switchDegree: _switchDegree,
+                      appbarTitle: _appbarTitle,
+                      openPerspectivesDrawer: _openPerspectivesDrawer,
+                    ),
+                    pinned: false,
+                    floating: true,
                   ),
-                  pinned: false,
-                  floating: true,
-                ),
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    <Widget>[
-                      Container(
-                        color: Theme.of(context).backgroundColor,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              width: MediaQuery.of(context).size.width * .5,
-                              padding: const EdgeInsets.only(
-                                top: 10,
-                                left: 10,
-                                right: 5,
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      <Widget>[
+                        Container(
+                          color: Theme.of(context).backgroundColor,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                width: MediaQuery.of(context).size.width * .5,
+                                padding: const EdgeInsets.only(
+                                  top: 10,
+                                  left: 10,
+                                  right: 5,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    for (int index = 0;
+                                        index <
+                                            snapshot.data.results.length + 1;
+                                        index++)
+                                      if (index == snapshot.data.results.length)
+                                        const SizedBox()
+                                      else if (index.isEven)
+                                        ExpressionPreview(
+                                          expression:
+                                              snapshot.data.results[index],
+                                        )
+                                  ],
+                                ),
                               ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  for (int index = 0;
-                                      index < snapshot.data.results.length + 1;
-                                      index++)
-                                    if (index == snapshot.data.results.length)
-                                      const SizedBox()
-                                    else if (index.isEven)
-                                      ExpressionPreview(
-                                        expression:
-                                            snapshot.data.results[index],
-                                      )
-                                ],
+                              Container(
+                                width: MediaQuery.of(context).size.width * .5,
+                                padding: const EdgeInsets.only(
+                                  top: 10,
+                                  left: 5,
+                                  right: 10,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    for (int index = 0;
+                                        index <
+                                            snapshot.data.results.length + 1;
+                                        index++)
+                                      if (index == snapshot.data.results.length)
+                                        const SizedBox()
+                                      else if (index.isOdd)
+                                        ExpressionPreview(
+                                          expression:
+                                              snapshot.data.results[index],
+                                        )
+                                  ],
+                                ),
                               ),
-                            ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * .5,
-                              padding: const EdgeInsets.only(
-                                top: 10,
-                                left: 5,
-                                right: 10,
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  for (int index = 0;
-                                      index < snapshot.data.results.length + 1;
-                                      index++)
-                                    if (index == snapshot.data.results.length)
-                                      const SizedBox()
-                                    else if (index.isOdd)
-                                      ExpressionPreview(
-                                        expression:
-                                            snapshot.data.results[index],
-                                      )
-                                ],
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                      ],
+                    ),
+                  )
+                ],
+              );
+            }
+            return Center(
+              child: Transform.translate(
+                offset: const Offset(0.0, 0.0),
+                child: JuntoProgressIndicator(),
+              ),
             );
-          }
-          return Center(
-            child: Transform.translate(
-              offset: const Offset(0.0, 0.0),
-              child: JuntoProgressIndicator(),
-            ),
-          );
-        },
+          },
+        ),
       ),
     );
   }
