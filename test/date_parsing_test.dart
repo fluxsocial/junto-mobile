26e1d270cfa39d5f54ebec001c756e09e3341d98
Neevash Ramdial <mail@neevash.dev>
2020-01-11 13:07:55 -0400

    Implemented date parsing

--- test/date_parsing_test.dart
+++ test/date_parsing_test.dart
@@ -1,4 +1,6 @@
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:junto_beta_mobile/widgets/utils/date_parsing.dart';
 import 'package:junto_beta_mobile/utils/utils.dart';
 
 void main() {
@@ -11,5 +13,40 @@ void main() {
     expect(result.day, 10);
     expect(result.hour, 12);
     expect(result.minute, 20);
+  });
+  testWidgets('Parse date for display', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (BuildContext context) {
+            DateTime time = DateTime.utc(2001, 06, 04);
+            String output = parseDate(context, time);
+            // call to material localization
+            expect(output, 'Monday, June 4, 2001');
+            time = DateTime.now();
+            // Testing today's date
+            output = parseDate(context, time);
+            expect(output, 'Today');
+            // Yesterday's date
+            time = DateTime(2020, 01, 10);
+            output = parseDate(context, time);
+            expect(output, 'Yesterday');
+            // days count
+            time = DateTime(2020, 01, 9);
+            output = parseDate(context, time);
+            expect(output, '2 days ago');
+
+            time = DateTime(2020, 01, 8);
+            output = parseDate(context, time);
+            expect(output, '3 days ago');
+
+            time = DateTime(2020, 01, 4);
+            output = parseDate(context, time);
+            expect(output, '7 days ago');
+            return const Placeholder();
+          },
+        ),
+      ),
+    );
   });
 }
