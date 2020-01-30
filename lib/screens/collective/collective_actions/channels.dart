28fca74b3410f689e3f196a3a2f2ae795a4148ed
Neevash Ramdial <mail@neevash.dev>
2020-01-29 21:13:43 -0400

    implemented channel search for actions

--- lib/screens/collective/collective_actions/channels.dart
+++ lib/screens/collective/collective_actions/channels.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/models/models.dart';
 import 'package:junto_beta_mobile/widgets/previews/channel_preview.dart';
+import 'package:provider/provider.dart';
 
 class JuntoChannels extends StatefulWidget {
-  const JuntoChannels({this.currentPerspective});
+  const JuntoChannels({this.currentPerspective, this.onChannelSelected});
 
   final String currentPerspective;
+  final ValueChanged<Channel> onChannelSelected;
+
   @override
   State<StatefulWidget> createState() {
     return JuntoChannelsState();
@@ -12,106 +19,162 @@ class JuntoChannels extends StatefulWidget {
 }
 
 class JuntoChannelsState extends State<JuntoChannels> {
+  TextEditingController textEditingController;
+  SearchRepo _searchRepo;
+  String query;
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _searchRepo = Provider.of<SearchRepo>(context);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    textEditingController.dispose();
+  }
+
+  void _onTextChange(String value) {
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    _timer = Timer(
+      const Duration(milliseconds: 300),
+      () => _updateQuery(
+        value,
+      ),
+    );
+  }
+
+  void _updateQuery(String value) {
+    setState(() {
+      query = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height - 150,
       child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              height: 80,
-              color: Theme.of(context).backgroundColor,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Image.asset('assets/images/junto-mobile__binoculars.png',
-                      height: 17, color: Theme.of(context).primaryColor),
-                  const SizedBox(width: 10),
-                  Text(
-                    widget.currentPerspective,
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                        color: Theme.of(context).primaryColor),
-                  )
-                ],
-              ),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            height: 80,
+            color: Theme.of(context).backgroundColor,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Image.asset('assets/images/junto-mobile__binoculars.png',
+                    height: 17, color: Theme.of(context).primaryColor),
+                const SizedBox(width: 10),
+                Text(
+                  widget.currentPerspective,
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).primaryColor),
+                )
+              ],
             ),
-            Container(
-              height: 70,
-              color: Theme.of(context).backgroundColor,
-              // color: Colors.green,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Transform.translate(
-                      offset: const Offset(0.0, 10.0),
-                      child: TextField(
-                        buildCounter: (
-                          BuildContext context, {
-                          int currentLength,
-                          int maxLength,
-                          bool isFocused,
-                        }) =>
-                            null,
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.all(0.0),
-                          hintText: 'filter by channel',
-                          border: InputBorder.none,
-                          hintStyle: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                              color: Theme.of(context).primaryColorLight),
-                        ),
-                        cursorColor: Theme.of(context).primaryColor,
-                        cursorWidth: 1,
-                        maxLines: 1,
-                        style: TextStyle(
-                            fontSize: 24,
+          ),
+          Container(
+            height: 70,
+            color: Theme.of(context).backgroundColor,
+            // color: Colors.green,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Transform.translate(
+                    offset: const Offset(0.0, 10.0),
+                    child: TextField(
+                      controller: textEditingController,
+                      buildCounter: (
+                        BuildContext context, {
+                        int currentLength,
+                        int maxLength,
+                        bool isFocused,
+                      }) =>
+                          null,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(0.0),
+                        hintText: 'filter by channel',
+                        border: InputBorder.none,
+                        hintStyle: TextStyle(
+                            fontSize: 20,
                             fontWeight: FontWeight.w500,
-                            color: Theme.of(context).primaryColor),
-                        maxLength: 80,
-                        textInputAction: TextInputAction.done,
+                            color: Theme.of(context).primaryColorLight),
                       ),
+                      cursorColor: Theme.of(context).primaryColor,
+                      cursorWidth: 1,
+                      maxLines: 1,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                          color: Theme.of(context).primaryColor),
+                      maxLength: 80,
+                      textInputAction: TextInputAction.done,
+                      onChanged: _onTextChange,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-
-            // Container(
-            //   height: 50,
-            //   color: Theme.of(context).backgroundColor,
-            //   child: Row(
-            //     children: <Widget>[
-            //       Icon(CustomIcons.packs, size: 17),
-            //       SizedBox(width: 10),
-            //       Text(
-            //         'JUNTO',
-            //         style: TextStyle(
-            //             fontSize: 14,
-            //             fontWeight: FontWeight.w500,
-            //             color: Theme.of(context).primaryColor),
-            //       )
-            //     ],
-            //   ),
-            // ),
-
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(0),
-                children: const <Widget>[
-                  ChannelPreview(channel: 'regenerative agriculture'),
-                  ChannelPreview(channel: 'biomimicry'),
-                  ChannelPreview(channel: 'austrian economics'),
-                  ChannelPreview(channel: 'conscious tech'),
-                  ChannelPreview(channel: 'implementation next build'),
-                ],
-              ),
+          ),
+          Expanded(
+            child: FutureBuilder<QueryResults<Channel>>(
+              future: _searchRepo.searchChannel(query),
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<QueryResults<Channel>> snapshot,
+              ) {
+                if (snapshot.hasData && !snapshot.hasError) {
+                  return ListView.builder(
+                    itemCount: snapshot.data.results.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Channel item = snapshot.data.results[index];
+                      return InkWell(
+                        onTap: () => widget.onChannelSelected(item),
+                        child: ChannelPreview(
+                          channel: item.name,
+                        ),
+                      );
+                    },
+                  );
+                }
+                if (snapshot.hasData && snapshot.data.results.isEmpty) {
+                  return Container(
+                    child: const Center(
+                      child: Text('Add new channel +'),
+                    ),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Container(
+                    child: Center(
+                      child: Text(snapshot.error.toString()),
+                    ),
+                  );
+                }
+                return Container(
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
             ),
-          ]),
+          ),
+        ],
+      ),
     );
   }
 }
