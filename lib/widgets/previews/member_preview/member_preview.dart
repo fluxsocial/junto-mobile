0e7f8cdb47d245388674d83eb689ba942e2be802
Eric Yang <eric@junto.love>
2020-02-04 01:01:25 +0100

    Implement CachedNetworkImage in member preview

--- lib/widgets/previews/member_preview/member_preview.dart
+++ lib/widgets/previews/member_preview/member_preview.dart
@@ -2,14 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/models/user_model.dart';
 import 'package:junto_beta_mobile/screens/member/member.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:junto_beta_mobile/app/custom_icons.dart';
 
 /// Render a list tile showing the user's profile image.
 /// Contains a callback [onUserTap] triggered when the user is selected.
 /// If this value is not specified and left null, the user will be redirected to
 /// the user "Den".
 class MemberPreview extends StatelessWidget {
-  const MemberPreview({Key key, this.profile})
-      : super(key: key);
+  const MemberPreview({Key key, this.profile}) : super(key: key);
 
   final UserProfile profile;
 
@@ -32,37 +33,58 @@ class MemberPreview extends StatelessWidget {
         color: Theme.of(context).colorScheme.background,
         child: Row(
           children: <Widget>[
-            if (profile.profilePicture != null ||
-                profile.profilePicture.isNotEmpty)
-              ClipOval(
-                child: Image.asset(
-                  'assets/images/junto-mobile__placeholder--member.png',
-                  // profile.profilePicture[0],
-                  height: 45.0,
-                  width: 45.0,
-                  fit: BoxFit.cover,
-                ),
-              )
-            else
-              Container(
-                  alignment: Alignment.center,
-                  height: 45.0,
-                  width: 45.0,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                      stops: const <double>[0.3, 0.9],
-                      colors: <Color>[
-                        Theme.of(context).colorScheme.secondary,
-                        Theme.of(context).colorScheme.primary
-                      ],
+            profile.profilePicture.isNotEmpty
+                ? ClipOval(
+                    child: CachedNetworkImage(
+                        imageUrl: profile.profilePicture[0],
+                        height: 45,
+                        width: 45,
+                        fit: BoxFit.cover,
+                        placeholder: (BuildContext context, String _) {
+                          return Container(
+                            alignment: Alignment.center,
+                            height: 45.0,
+                            width: 45.0,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomLeft,
+                                end: Alignment.topRight,
+                                stops: const <double>[0.3, 0.9],
+                                colors: <Color>[
+                                  Theme.of(context).colorScheme.secondary,
+                                  Theme.of(context).colorScheme.primary,
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Icon(
+                              CustomIcons.spheres,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                              size: 17,
+                            ),
+                          );
+                        }),
+                  )
+                : Container(
+                    alignment: Alignment.center,
+                    height: 45.0,
+                    width: 45.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        stops: const <double>[0.3, 0.9],
+                        colors: <Color>[
+                          Theme.of(context).colorScheme.secondary,
+                          Theme.of(context).colorScheme.primary
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(100),
                     ),
-                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset(
+                        'assets/images/junto-mobile__logo--white.png',
+                        height: 15),
                   ),
-                  child: Image.asset(
-                      'assets/images/junto-mobile__logo--white.png',
-                      height: 15)),
             Container(
               padding: const EdgeInsets.symmetric(
                 vertical: 15,
