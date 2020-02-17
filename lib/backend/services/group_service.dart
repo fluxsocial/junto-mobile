f29238084ba0ccfe89926378db5520d25806f716
Eric Yang <eric@junto.love>
2020-02-17 13:06:04 -0500

    Group service changes

--- lib/backend/services/group_service.dart
+++ lib/backend/services/group_service.dart
@@ -40,6 +40,8 @@ class GroupServiceCentralized implements GroupService {
       String groupAddress, List<Map<String, dynamic>> users) async {
     final http.Response _serverResponse = await client
         .postWithoutEncoding('/groups/$groupAddress/members', body: users);
+        print(_serverResponse.statusCode);
+        print(_serverResponse.body);
     JuntoHttp.handleResponse(_serverResponse);
   }
 
