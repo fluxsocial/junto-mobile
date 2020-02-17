b662d67a4f797eaa0d7fc5732fc7dc476a6039a3
Eric Yang <eric@junto.love>
2020-02-17 14:39:51 -0500

    clean up sphere, pack, and channel previews

--- lib/widgets/previews/channel_preview.dart
+++ lib/widgets/previews/channel_preview.dart
@@ -13,57 +13,54 @@ class ChannelPreview extends StatelessWidget {
       color: Theme.of(context).colorScheme.background,
       child: Row(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                height: 45.0,
-                width: 45.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
-                    stops: const <double>[0.2, 0.9],
-                    colors: <Color>[
-                      Theme.of(context).colorScheme.secondary,
-                      Theme.of(context).colorScheme.primary
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                child: const Icon(
-                  CustomIcons.hash,
-                  color: Colors.white,
-                  size: 15,
+          Container(
+            alignment: Alignment.center,
+            height: 45.0,
+            width: 45.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                stops: const <double>[0.2, 0.9],
+                colors: <Color>[
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.primary
+                ],
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: const Icon(
+              CustomIcons.hash,
+              color: Colors.white,
+              size: 15,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+              ),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      width: .5, color: Theme.of(context).dividerColor),
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width - 95,
-                padding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                ),
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                        width: .5, color: Theme.of(context).dividerColor),
-                  ),
-                ),
-                margin: const EdgeInsets.only(left: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(channel.name,
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.subtitle1),
-                    Text('x expressions',
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.bodyText1)
-                  ],
-                ),
-              )
-            ],
-          ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(channel.name,
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.subtitle1),
+                  Text('x expressions',
+                      textAlign: TextAlign.start,
+                      style: Theme.of(context).textTheme.bodyText1)
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
