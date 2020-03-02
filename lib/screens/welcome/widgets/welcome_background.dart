3eb429637dbc23805ce5c862ce28a60a54caa83d
Dominik Roszkowski <dominik@roszkowski.dev>
2020-03-02 21:15:09 +0100

    Don't go through white background when changing theme

--- lib/screens/welcome/widgets/welcome_background.dart
+++ lib/screens/welcome/widgets/welcome_background.dart
@@ -9,30 +9,64 @@ class WelcomeBackground extends StatelessWidget {
 
   final String _currentTheme;
 
+  static const Map<String, String> backgroundMap = <String, String>{
+    'aqueous': 'assets/images/junto-mobile__themes--aqueous.png',
+    'aqueous-night': 'assets/images/junto-mobile__themes--aqueous.png',
+    'royal': 'assets/images/junto-mobile__themes--royal.png',
+    'royal-night': 'assets/images/junto-mobile__themes--royal.png',
+    'rainbow': 'assets/images/junto-mobile__themes--rainbow.png',
+    'rainbow-night': 'assets/images/junto-mobile__themes--rainbow.png',
+    'default': 'assets/images/junto-mobile__themes--rainbow.png',
+  };
+
+  static const Widget defaultBackground = ImageBackground(
+    'assets/images/junto-mobile__themes--aqueous.png',
+    key: ValueKey<String>('aqueous'),
+  );
+
   @override
   Widget build(BuildContext context) {
+    String key = _currentTheme;
+    if (!backgroundMap.containsKey(_currentTheme)) {
+      key = 'default';
+    }
+
     return Container(
       child: AnimatedSwitcher(
         duration: const Duration(milliseconds: 300),
-        child: _setBackground(MediaQuery.of(context).size),
+        layoutBuilder: (Widget currentChild, List<Widget> previousChildren) {
+          return Stack(
+            children: <Widget>[
+              // this way there's always some widget in the background
+              // and we don't go through white background
+              defaultBackground,
+              ...previousChildren,
+              if (currentChild != null)
+                currentChild,
+            ],
+            alignment: Alignment.center,
+          );
+        },
+        child: ImageBackground(
+          backgroundMap[key],
+          key: ValueKey<String>(key),
+        ),
       ),
     );
   }
+}
 
-  Widget _setBackground(Size size) {
-    String imageAsset;
-    print(_currentTheme);
+class ImageBackground extends StatelessWidget {
+  const ImageBackground(
+    this.imageAsset, {
+    Key key,
+  }) : super(key: key);
 
-    if (_currentTheme == 'aqueous' || _currentTheme == 'aqueous-night') {
-      imageAsset = 'assets/images/junto-mobile__themes--aqueous.png';
-    } else if (_currentTheme == 'royal' || _currentTheme == 'royal-night') {
-      imageAsset = 'assets/images/junto-mobile__themes--royal.png';
-    } else if (_currentTheme == 'rainbow' || _currentTheme == 'rainbow-night') {
-      imageAsset = 'assets/images/junto-mobile__themes--rainbow.png';
-    } else {
-      imageAsset = 'assets/images/junto-mobile__themes--rainbow.png';
-    }
+  final String imageAsset;
 
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Image.asset(
       imageAsset,
       key: ValueKey<String>(imageAsset),
