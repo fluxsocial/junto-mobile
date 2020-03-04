1ed349cc41dc612f06dad430d5bdac8b435d3ead
Eric Yang <eric@junto.love>
2020-03-03 20:20:50 -0500

    polish member.dart ux/ui

--- lib/widgets/user_expressions.dart
+++ lib/widgets/user_expressions.dart
@@ -5,6 +5,7 @@ import 'package:junto_beta_mobile/models/models.dart';
 import 'package:junto_beta_mobile/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview.dart';
 import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
 import 'package:provider/provider.dart';
+import 'package:junto_beta_mobile/app/custom_icons.dart';
 
 /// Linear list of expressions created by the given [userProfile].
 class UserExpressions extends StatefulWidget {
@@ -62,58 +63,83 @@ class _UserExpressionsState extends State<UserExpressions> {
             color: Theme.of(context).colorScheme.background,
             child: ListView(
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Container(
-                      width: MediaQuery.of(context).size.width * .5,
-                      padding:
-                          const EdgeInsets.only(left: 10, right: 5, top: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
-                          for (int index = 0;
-                              index < snapshot.data.length + 1;
-                              index++)
-                            if (index == snapshot.data.length)
-                              const SizedBox()
-                            else if (index.isEven &&
-                                snapshot.data[index].privacy == widget.privacy)
-                              TwoColumnExpressionPreview(
-                                expression: snapshot.data[index],
-                                userAddress: widget.userProfile.address,
-                              )
-                            else
-                              const SizedBox()
-
-                          // even number indexes
+                          Image.asset('assets/images/junto-mobile__filter.png',
+                              height: 17),
+                          Row(
+                            children: <Widget>[
+                              Icon(CustomIcons.twocolumn, size: 20),
+                              const SizedBox(width: 10),
+                              Icon(CustomIcons.singlecolumn, size: 20),
+                            ],
+                          )
                         ],
                       ),
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * .5,
-                      padding:
-                          const EdgeInsets.only(left: 5, right: 10, top: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          // odd number indexes
-                          for (int index = 0;
-                              index < snapshot.data.length + 1;
-                              index++)
-                            if (index == snapshot.data.length)
-                              const SizedBox()
-                            else if (index.isOdd &&
-                                snapshot.data[index].privacy == widget.privacy)
-                              TwoColumnExpressionPreview(
-                                expression: snapshot.data[index],
-                                userAddress: widget.userProfile.address,
-                              )
-                            else
-                              const SizedBox()
-                        ],
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width * .5,
+                          padding: const EdgeInsets.only(
+                              left: 10, right: 5, top: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              for (int index = 0;
+                                  index < snapshot.data.length + 1;
+                                  index++)
+                                if (index == snapshot.data.length)
+                                  const SizedBox()
+                                else if (index.isEven &&
+                                    snapshot.data[index].privacy ==
+                                        widget.privacy)
+                                  TwoColumnExpressionPreview(
+                                    expression: snapshot.data[index],
+                                    userAddress: widget.userProfile.address,
+                                  )
+                                else
+                                  const SizedBox()
+
+                              // even number indexes
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .5,
+                          padding: const EdgeInsets.only(
+                              left: 5, right: 10, top: 10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              // odd number indexes
+                              for (int index = 0;
+                                  index < snapshot.data.length + 1;
+                                  index++)
+                                if (index == snapshot.data.length)
+                                  const SizedBox()
+                                else if (index.isOdd &&
+                                    snapshot.data[index].privacy ==
+                                        widget.privacy)
+                                  TwoColumnExpressionPreview(
+                                    expression: snapshot.data[index],
+                                    userAddress: widget.userProfile.address,
+                                  )
+                                else
+                                  const SizedBox()
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 )
