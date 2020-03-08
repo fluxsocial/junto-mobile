3efff20a898ed9bcae403ca32bfb67965c009a4a
Eric Yang <eric@junto.love>
2020-03-07 20:41:41 -0500

    Add validation for update group and refactor directories

---
+++ lib/screens/groups/groups_actions/spheres/sphere_open/action_items/creator/action_items.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:junto_beta_mobile/backend/repositories.dart';
+import 'package:junto_beta_mobile/models/models.dart';
+import 'package:junto_beta_mobile/screens/groups/groups_actions/spheres/sphere_open/action_items/creator/edit_group.dart';
+
+// This component is used in ExpressionPreview and ExpressionOpen
+// as the 'more' icon is pressed to view the action items
+// available for each expression
+class OwnerActionItems extends StatelessWidget {
+  const OwnerActionItems({
+    Key key,
+    @required this.sphere,
+  }) : super(key: key);
+
+  final Group sphere;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      child: Container(
+        height: MediaQuery.of(context).size.height * .4,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 5,
+                      width: MediaQuery.of(context).size.width * .1,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).dividerColor,
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      EditGroup.route(sphere),
+                    );
+                  },
+                  title: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.edit,
+                        size: 17,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const SizedBox(width: 15),
+                      Text(
+                        'Edit Sphere',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  onTap: () {
+                    Provider.of<GroupRepo>(context, listen: false)
+                        .deleteGroup(sphere.address);
+                  },
+                  title: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.delete,
+                        size: 17,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const SizedBox(width: 15),
+                      Text(
+                        'Delete Circle',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
