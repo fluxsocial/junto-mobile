5e7483b78003df9662d2c147ed3215ae94044d08
Dominik Roszkowski <dominik@roszkowski.dev>
2020-03-09 20:40:45 +0100

    Remove unnecessary color

--- lib/widgets/drawer/channel_preview.dart
+++ lib/widgets/drawer/channel_preview.dart
@@ -10,7 +10,6 @@ class FilterDrawerChannelPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.transparent,
       child: Row(
         children: <Widget>[
           Container(
