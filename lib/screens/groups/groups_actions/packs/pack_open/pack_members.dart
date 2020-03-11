30add18ffc3be72e8b9630b56150f0658c319b05
Eric Yang <eric@junto.love>
2020-03-11 13:45:28 -0400

    Design and build custom confirm dialog widget

--- lib/screens/groups/groups_actions/packs/pack_open/pack_members.dart
+++ lib/screens/groups/groups_actions/packs/pack_open/pack_members.dart
@@ -1,18 +1,16 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'package:junto_beta_mobile/models/models.dart';
-import 'package:junto_beta_mobile/screens/groups/groups_actions/packs/pack_open/pack_open_appbar.dart';
 import 'package:junto_beta_mobile/widgets/previews/member_preview/member_preview.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class PackOpenMembers extends StatelessWidget {
   const PackOpenMembers({this.getPackMembers});
 
   final Future<List<Users>> getPackMembers;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Users>>(
--- lib/screens/groups/groups_actions/spheres/sphere_open/action_items/creator/action_items.dart
+++ lib/screens/groups/groups_actions/spheres/sphere_open/action_items/creator/action_items.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:junto_beta_mobile/backend/repositories.dart';
 import 'package:junto_beta_mobile/models/models.dart';
 import 'package:junto_beta_mobile/screens/groups/groups_actions/spheres/sphere_open/action_items/creator/edit_group.dart';
+import 'package:junto_beta_mobile/widgets/dialogs/confirm_dialog.dart';
 
 // This component is used in ExpressionPreview and ExpressionOpen
 // as the 'more' icon is pressed to view the action items
@@ -18,91 +19,103 @@ class OwnerActionItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.transparent,
-      child: Container(
-        height: MediaQuery.of(context).size.height * .4,
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 10,
-        ),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: Colors.transparent,
+          child: Container(
+            height: MediaQuery.of(context).size.height * .4,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 10,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Container(
-                      height: 5,
-                      width: MediaQuery.of(context).size.width * .1,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).dividerColor,
-                        borderRadius: BorderRadius.circular(100),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          height: 5,
+                          width: MediaQuery.of(context).size.width * .1,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).dividerColor,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                      ],
+                    ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                          context,
+                          EditGroup.route(sphere),
+                        );
+                      },
+                      title: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.edit,
+                            size: 17,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          const SizedBox(width: 15),
+                          Text(
+                            'Edit Sphere',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      onTap: () {
+                        Navigator.pop(context);
+                        showDialog(
+                          context: context,
+                          builder: (context) => ConfirmDialog(
+                              confirmationText:
+                                  'Are you sure you want to delete this circle?'),
+                        );
+
+                        // Provider.of<GroupRepo>(context, listen: false)
+                        //     .deleteGroup(sphere.address);
+                      },
+                      title: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.delete,
+                            size: 17,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          const SizedBox(width: 15),
+                          Text(
+                            'Delete Circle',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-                ListTile(
-                  contentPadding: const EdgeInsets.all(0),
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.push(
-                      context,
-                      EditGroup.route(sphere),
-                    );
-                  },
-                  title: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.edit,
-                        size: 17,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      const SizedBox(width: 15),
-                      Text(
-                        'Edit Sphere',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                    ],
-                  ),
-                ),
-                ListTile(
-                  contentPadding: const EdgeInsets.all(0),
-                  onTap: () {
-                    Provider.of<GroupRepo>(context, listen: false)
-                        .deleteGroup(sphere.address);
-                  },
-                  title: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.delete,
-                        size: 17,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      const SizedBox(width: 15),
-                      Text(
-                        'Delete Circle',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                    ],
-                  ),
-                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
