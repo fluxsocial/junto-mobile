3bcb90f8a58538a95118a31f927bba3aa921e9c3
Neevash Ramdial <mail@neevash.dev>
2020-03-10 22:35:14 -0400

    update import

--- lib/widgets/appbar/appbar_search.dart
+++ lib/widgets/appbar/appbar_search.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
 import 'package:junto_beta_mobile/models/user_model.dart';
-import 'package:junto_beta_mobile/screens/collective/perspectives'
-    '/create_perspective/create_perspective.dart' show SelectedUsers;
+import 'package:junto_beta_mobile/screens/collective/perspectives/create_perspective.dart'
+    show SelectedUsers;
 import 'package:provider/provider.dart';
 
 // Junto app bar used throughout the main screens. Rendered in JuntoTemplate.
