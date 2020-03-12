59521c38ba67d226458e58954a437b07f0f629f1
Eric Yang <eric@junto.love>
2020-03-12 13:48:56 -0400

    Remove unnecessary filter channel drawer in sphere open

--- lib/screens/groups/groups_actions/spheres/sphere_open/sphere_open.dart
+++ lib/screens/groups/groups_actions/spheres/sphere_open/sphere_open.dart
@@ -94,168 +94,150 @@ class SphereOpenState extends State<SphereOpen> with HideFab {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: JuntoFilterDrawer(
-        leftDrawer: FilterDrawerContent(
-          //todo(dominikroszkowski): implement these
-          filterByChannel: (_) {},
-          channels: [],
-          resetChannels: () {},
-        ),
-        rightMenu: JuntoDrawer(),
-        scaffold: Scaffold(
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(45),
-            child: SphereOpenAppbar(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(45),
+        child: SphereOpenAppbar(
+            group: widget.group,
+            relationToGroup: relationToGroup,
+            userAddress: _userAddress),
+      ),
+      body: DefaultTabController(
+        length: _tabs.length,
+        child: NestedScrollView(
+          body: TabBarView(
+            children: <Widget>[
+              SphereOpenAbout(
+                getMembers: _getMembers(),
                 group: widget.group,
-                relationToGroup: relationToGroup,
-                userAddress: _userAddress),
-          ),
-          body: DefaultTabController(
-            length: _tabs.length,
-            child: NestedScrollView(
-              body: TabBarView(
-                children: <Widget>[
-                  SphereOpenAbout(
-                    getMembers: _getMembers(),
-                    group: widget.group,
-                  ),
-                  if (widget.group.address != null)
-                    GroupExpressions(
-                      group: widget.group,
-                      userAddress: _userAddress,
-                      expressionsPrivacy: 'Public',
-                    )
-                ],
               ),
-              physics: const ClampingScrollPhysics(),
-              headerSliverBuilder: (
-                BuildContext context,
-                bool innerBoxIsScrolled,
-              ) {
-                return <Widget>[
-                  SliverAppBar(
-                    brightness: Brightness.light,
-                    automaticallyImplyLeading: false,
-                    primary: false,
-                    actions: const <Widget>[
-                      SizedBox(
-                        height: 0,
-                        width: 0,
-                      ),
-                    ],
-                    backgroundColor: Theme.of(context).colorScheme.background,
-                    pinned: false,
-                    flexibleSpace: FlexibleSpaceBar(
-                      collapseMode: CollapseMode.pin,
-                      background: Column(
-                        children: <Widget>[
-                          widget.group.groupData.photo == ''
-                              ? Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * .3,
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      begin: Alignment.bottomLeft,
-                                      end: Alignment.topRight,
-                                      stops: const <double>[0.2, 0.9],
-                                      colors: <Color>[
-                                        Theme.of(context).colorScheme.secondary,
-                                        Theme.of(context).colorScheme.primary
-                                      ],
-                                    ),
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: Icon(
-                                    CustomIcons.spheres,
-                                    size: 60,
-                                    color:
-                                        Theme.of(context).colorScheme.onPrimary,
-                                  ),
-                                )
-                              : CachedNetworkImage(
-                                  imageUrl: widget.group.groupData.photo,
+              if (widget.group.address != null)
+                GroupExpressions(
+                  group: widget.group,
+                  userAddress: _userAddress,
+                  expressionsPrivacy: 'Public',
+                )
+            ],
+          ),
+          physics: const ClampingScrollPhysics(),
+          headerSliverBuilder: (
+            BuildContext context,
+            bool innerBoxIsScrolled,
+          ) {
+            return <Widget>[
+              SliverAppBar(
+                brightness: Brightness.light,
+                automaticallyImplyLeading: false,
+                primary: false,
+                actions: const <Widget>[
+                  SizedBox(
+                    height: 0,
+                    width: 0,
+                  ),
+                ],
+                backgroundColor: Theme.of(context).colorScheme.background,
+                pinned: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  background: Column(
+                    children: <Widget>[
+                      widget.group.groupData.photo == ''
+                          ? Container(
+                              height: MediaQuery.of(context).size.height * .3,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topRight,
+                                  stops: const <double>[0.2, 0.9],
+                                  colors: <Color>[
+                                    Theme.of(context).colorScheme.secondary,
+                                    Theme.of(context).colorScheme.primary
+                                  ],
+                                ),
+                              ),
+                              alignment: Alignment.center,
+                              child: Icon(
+                                CustomIcons.spheres,
+                                size: 60,
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                            )
+                          : CachedNetworkImage(
+                              imageUrl: widget.group.groupData.photo,
+                              width: MediaQuery.of(context).size.width,
+                              height: MediaQuery.of(context).size.height * .3,
+                              placeholder: (BuildContext context, String _) {
+                                return Container(
+                                  color: Theme.of(context).dividerColor,
                                   width: MediaQuery.of(context).size.width,
                                   height:
                                       MediaQuery.of(context).size.height * .3,
-                                  placeholder:
-                                      (BuildContext context, String _) {
-                                    return Container(
-                                      color: Theme.of(context).dividerColor,
-                                      width: MediaQuery.of(context).size.width,
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              .3,
-                                    );
-                                  },
-                                  fit: BoxFit.cover),
-                          Container(
-                            key: _keyFlexibleSpace,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: JuntoStyles.horizontalPadding,
-                                vertical: 15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Flexible(
-                                  child: Text(
-                                    widget.group.groupData.name,
-                                    style:
-                                        Theme.of(context).textTheme.headline4,
-                                  ),
-                                ),
-                                relationToGroup != null &&
-                                        !relationToGroup['creator'] &&
-                                        !relationToGroup['member'] &&
-                                        !relationToGroup['facilitator']
-                                    ? _joinGroup()
-                                    : const SizedBox(),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    expandedHeight: _flexibleHeightSpace == null
-                        ? 10000
-                        : MediaQuery.of(context).size.height * .3 +
-                            _flexibleHeightSpace,
-                    forceElevated: false,
-                  ),
-                  SliverPersistentHeader(
-                    pinned: true,
-                    delegate: JuntoAppBarDelegate(
-                      TabBar(
-                        labelPadding: const EdgeInsets.all(0),
-                        isScrollable: true,
-                        labelColor: Theme.of(context).primaryColorDark,
-                        unselectedLabelColor:
-                            Theme.of(context).primaryColorLight,
-                        labelStyle: Theme.of(context).textTheme.subtitle1,
-                        indicatorWeight: 0.0001,
-                        tabs: <Widget>[
-                          for (String name in _tabs)
-                            Container(
-                              margin: const EdgeInsets.only(right: 20),
-                              color: Theme.of(context).colorScheme.background,
-                              child: Tab(
-                                child: Text(
-                                  name,
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
+                                );
+                              },
+                              fit: BoxFit.cover),
+                      Container(
+                        key: _keyFlexibleSpace,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: JuntoStyles.horizontalPadding,
+                            vertical: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Flexible(
+                              child: Text(
+                                widget.group.groupData.name,
+                                style: Theme.of(context).textTheme.headline4,
                               ),
                             ),
-                        ],
+                            relationToGroup != null &&
+                                    !relationToGroup['creator'] &&
+                                    !relationToGroup['member'] &&
+                                    !relationToGroup['facilitator']
+                                ? _joinGroup()
+                                : const SizedBox(),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ];
-              },
-            ),
-          ),
+                    ],
+                  ),
+                ),
+                expandedHeight: _flexibleHeightSpace == null
+                    ? 10000
+                    : MediaQuery.of(context).size.height * .3 +
+                        _flexibleHeightSpace,
+                forceElevated: false,
+              ),
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: JuntoAppBarDelegate(
+                  TabBar(
+                    labelPadding: const EdgeInsets.all(0),
+                    isScrollable: true,
+                    labelColor: Theme.of(context).primaryColorDark,
+                    unselectedLabelColor: Theme.of(context).primaryColorLight,
+                    labelStyle: Theme.of(context).textTheme.subtitle1,
+                    indicatorWeight: 0.0001,
+                    tabs: <Widget>[
+                      for (String name in _tabs)
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          color: Theme.of(context).colorScheme.background,
+                          child: Tab(
+                            child: Text(
+                              name,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              )
+            ];
+          },
         ),
       ),
     );
