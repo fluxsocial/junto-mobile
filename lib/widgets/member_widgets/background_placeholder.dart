d1c57dc6e951004ee284f26d38c5e22e5e429364
Eric Yang <eric@junto.love>
2020-03-19 00:20:49 -0400

    More widget class refactor

---
+++ lib/widgets/member_widgets/background_placeholder.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MemberBackgroundPlaceholder extends StatelessWidget {
+  const MemberBackgroundPlaceholder({this.theme});
+
+  final String theme;
+
+  String _getBackgroundImageAsset() {
+    if (theme == 'rainbow' || theme == 'rainbow-night') {
+      return 'assets/images/junto-mobile__themes--rainbow.png';
+    } else if (theme == 'aqueous' || theme == 'aqueous-night') {
+      return 'assets/images/junto-mobile__themes--aqueous.png';
+    } else if (theme == 'royal' || theme == 'royal-night') {
+      return 'assets/images/junto-mobile__themes--royal.png';
+    } else {
+      return 'assets/images/junto-mobile__themes--rainbow.png';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * .2,
+      width: MediaQuery.of(context).size.width,
+      child: Image.asset(
+        _getBackgroundImageAsset(),
+        height: MediaQuery.of(context).size.height * .2,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
