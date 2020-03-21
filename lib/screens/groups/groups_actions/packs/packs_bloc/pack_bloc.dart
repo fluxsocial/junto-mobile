c9a02bbbfff3586364399d72fb5fbb96920ac684
Neevash Ramdial <mail@neevash.dev>
2020-03-21 14:13:34 -0400

    created pack bloc

---
+++ lib/screens/groups/groups_actions/packs/packs_bloc/pack_bloc.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/models/models.dart';
+import 'package:junto_beta_mobile/utils/junto_exception.dart';
+import 'package:meta/meta.dart';
+
+part 'pack_event.dart';
+part 'pack_state.dart';
+
+class PackBloc extends Bloc<PackEvent, PackState> {
+  PackBloc(this.expressionRepo, this.groupAddress);
+  final ExpressionRepo expressionRepo;
+  final String groupAddress;
+
+  Map<String, String> _params;
+
+  @override
+  PackState get initialState => PackInitial();
+
+  @override
+  Stream<PackState> mapEventToState(
+    PackEvent event,
+  ) async* {
+    if (event is FetchPacks) {
+      yield* _mapFetchPacksToState(event);
+    }
+  }
+
+  Stream<PackState> _mapFetchPacksToState(FetchPacks event) async* {
+    _params = <String, String>{
+      'context': groupAddress,
+      'context_type': 'Group',
+      'pagination_position': '0',
+    };
+    try {
+      yield PacksLoading();
+      final QueryResults<ExpressionResponse> results =
+          await expressionRepo.getPackExpressions(
+        _params,
+      );
+
+      List<ExpressionResponse> _public = results.results
+          .where((element) => element.privacy == 'Public')
+          .toList();
+      List<ExpressionResponse> _private = results.results
+          .where((element) => element.privacy == 'Private')
+          .toList();
+
+      final publicQueryResults = QueryResults(
+        results: _public,
+        lastTimestamp: results.lastTimestamp,
+      );
+      final privateQueryResults = QueryResults(
+        results: _private,
+        lastTimestamp: results.lastTimestamp,
+      );
+
+      yield PacksLoaded(publicQueryResults, privateQueryResults);
+    } on JuntoException catch (error) {
+      yield PacksError(error.message);
+    } catch (e, s) {
+      print(e);
+      print(s.toString());
+      yield PacksError();
+    }
+  }
+}
