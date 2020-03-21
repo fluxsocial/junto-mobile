b4bef891211866901524b1ed6eed00dd423acfd9
Eric Yang <eric@junto.love>
2020-03-20 22:02:35 -0400

    Dynamically display background placeholder according to theme in edit den

--- lib/screens/den/edit_den/edit_den_background_photo.dart
+++ lib/screens/den/edit_den/edit_den_background_photo.dart
@@ -2,11 +2,17 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
+import 'package:junto_beta_mobile/widgets/member_widgets/background_placeholder.dart';
 
 class EditDenBackgroundPhoto extends StatelessWidget {
-  const EditDenBackgroundPhoto({this.onPickPressed, this.backgroundPhotoFile});
+  const EditDenBackgroundPhoto({
+    this.onPickPressed,
+    this.backgroundPhotoFile,
+    this.currentTheme,
+  });
   final File backgroundPhotoFile;
   final Function onPickPressed;
+  final String currentTheme;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,14 +22,7 @@ class EditDenBackgroundPhoto extends StatelessWidget {
       child: Stack(
         children: <Widget>[
           if (backgroundPhotoFile == null)
-            Container(
-              height: MediaQuery.of(context).size.height * .2,
-              width: MediaQuery.of(context).size.width,
-              child: Image.asset(
-                'assets/images/junto-mobile__themes--rainbow.png',
-                fit: BoxFit.cover,
-              ),
-            )
+            MemberBackgroundPlaceholder(theme: currentTheme)
           else
             Container(
               height: MediaQuery.of(context).size.height * .2,
