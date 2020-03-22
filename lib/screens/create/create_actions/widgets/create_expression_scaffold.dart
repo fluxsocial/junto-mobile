750ff987b20b85994100dfaa2a0d0dae75136093
Eric Yang <eric@junto.love>
2020-03-22 00:01:26 -0400

    Add end drawer to create scaffold

--- lib/screens/create/create_actions/widgets/create_expression_scaffold.dart
+++ lib/screens/create/create_actions/widgets/create_expression_scaffold.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/expressions.dart';
 import 'package:junto_beta_mobile/screens/create/create_actions/widgets/create_app_bar.dart';
 import 'package:junto_beta_mobile/screens/create/create_actions/widgets/create_bottom_nav.dart';
+import 'package:junto_beta_mobile/widgets/drawer/junto_filter_drawer.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer.dart';
 
 class CreateExpressionScaffold extends StatelessWidget {
   const CreateExpressionScaffold({
@@ -20,15 +22,22 @@ class CreateExpressionScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CreateAppBar(
-        expressionType: expressionType,
-        onNext: onNext,
-      ),
-      resizeToAvoidBottomPadding: false,
-      floatingActionButton: showBottomNav ? const CreateBottomNav() : null,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: Column(
-        children: <Widget>[child],
+      body: JuntoFilterDrawer(
+        leftDrawer: null,
+        rightMenu: JuntoDrawer(),
+        scaffold: Scaffold(
+          appBar: CreateAppBar(
+            expressionType: expressionType,
+            onNext: onNext,
+          ),
+          resizeToAvoidBottomPadding: false,
+          floatingActionButton: showBottomNav ? const CreateBottomNav() : null,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          body: Column(
+            children: <Widget>[child],
+          ),
+        ),
       ),
     );
   }
