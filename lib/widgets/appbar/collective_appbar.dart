600b116e80cc5083c821d521f4022a07484d81ea
Neevash Ramdial <mail@neevash.dev>
2020-03-23 15:18:55 -0400

    wrapped icons in icon button

--- lib/widgets/appbar/collective_appbar.dart
+++ lib/widgets/appbar/collective_appbar.dart
@@ -21,127 +21,90 @@ class CollectiveAppBar extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Consumer<UserDataProvider>(
-      builder: (BuildContext context, UserDataProvider data, Widget child) {
-        return Container(
-          height: 135,
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 85,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.only(bottom: 10),
-                decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Theme.of(context).dividerColor,
-                        width: .75,
-                      ),
-                    ),
-                    color: Theme.of(context).backgroundColor),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.bottomLeft,
-                      padding: const EdgeInsets.only(left: 10),
-                      color: Colors.transparent,
-                      height: 36,
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset(
-                            'assets/images/junto-mobile__logo.png',
-                            height: 22.0,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          const SizedBox(width: 7.5),
-                          Text(
-                            appbarTitle ?? 'JUNTO',
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 42,
-                            color: Colors.transparent,
-                            alignment: Alignment.bottomRight,
-                            padding: const EdgeInsets.only(right: 10),
-                            child: Icon(CustomIcons.moon,
-                                size: 22,
-                                color: Theme.of(context).primaryColor),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 50,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).backgroundColor,
-                  border: Border(
-                    bottom: BorderSide(
-                        color: Theme.of(context).dividerColor, width: .75),
+    return Container(
+      height: 135,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 85,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Theme.of(context).dividerColor,
+                    width: .75,
                   ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                color: Theme.of(context).backgroundColor),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  padding: const EdgeInsets.only(left: 10),
+                  color: Colors.transparent,
+                  height: 36,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      const FilterDrawerButton(),
-                      Row(
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () => data
-                                ?.switchColumnLayout(ExpressionFeedLayout.two),
-                            child: Container(
-                              color: Colors.transparent,
-                              alignment: Alignment.centerRight,
-                              width: 38,
-                              child: Icon(
-                                CustomIcons.twocolumn,
-                                size: 20,
-                                color: data?.twoColumnView == true
-                                    ? Theme.of(context).primaryColorDark
-                                    : Theme.of(context).primaryColorLight,
-                              ),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () => data?.switchColumnLayout(
-                                ExpressionFeedLayout.single),
-                            child: Container(
-                              color: Colors.transparent,
-                              alignment: Alignment.centerRight,
-                              width: 38,
-                              child: Icon(
-                                CustomIcons.singlecolumn,
-                                size: 20,
-                                color: data?.twoColumnView == true
-                                    ? Theme.of(context).primaryColorLight
-                                    : Theme.of(context).primaryColorDark,
-                              ),
-                            ),
-                          ),
-                        ],
+                      Image.asset(
+                        'assets/images/junto-mobile__logo.png',
+                        height: 22.0,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const SizedBox(width: 7.5),
+                      Text(
+                        appbarTitle ?? 'JUNTO',
+                        style: Theme.of(context).textTheme.caption,
                       ),
                     ],
                   ),
                 ),
-              )
-            ],
+                Row(
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 42,
+                        color: Colors.transparent,
+                        alignment: Alignment.bottomRight,
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Icon(
+                          CustomIcons.moon,
+                          size: 22,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        );
-      },
+          Container(
+            height: 50,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).backgroundColor,
+              border: Border(
+                bottom: BorderSide(
+                    color: Theme.of(context).dividerColor, width: .75),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 2.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const FilterDrawerButton(),
+                  EndAppBarButtons(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -153,4 +116,57 @@ class CollectiveAppBar extends SliverPersistentHeaderDelegate {
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+}
+
+class EndAppBarButtons extends StatelessWidget {
+  const EndAppBarButtons({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<UserDataProvider>(
+      builder: (BuildContext context, UserDataProvider data, Widget child) {
+        return Row(
+          children: <Widget>[
+            Semantics(
+              button: true,
+              hint: 'Two column layout',
+              child: Material(
+                child: IconButton(
+                  onPressed: () =>
+                      data?.switchColumnLayout(ExpressionFeedLayout.two),
+                  icon: Icon(
+                    CustomIcons.twocolumn,
+                    size: 20,
+                    color: data?.twoColumnView == true
+                        ? Theme.of(context).primaryColorDark
+                        : Theme.of(context).primaryColorLight,
+                  ),
+                ),
+              ),
+            ),
+            Semantics(
+              button: true,
+              hint: 'Single Column Layout',
+              child: Material(
+                child: IconButton(
+                  onPressed: () => data?.switchColumnLayout(
+                    ExpressionFeedLayout.single,
+                  ),
+                  icon: Icon(
+                    CustomIcons.singlecolumn,
+                    size: 20,
+                    color: data?.twoColumnView == true
+                        ? Theme.of(context).primaryColorLight
+                        : Theme.of(context).primaryColorDark,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
