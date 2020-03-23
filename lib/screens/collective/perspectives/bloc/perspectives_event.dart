32f8bbf70b93b3b0035d90217462c61e2eb1a194
Dominik Roszkowski <dominik@roszkowski.dev>
2020-03-23 20:54:58 +0100

    Extend perspectives events

--- lib/screens/collective/perspectives/bloc/perspectives_event.dart
+++ lib/screens/collective/perspectives/bloc/perspectives_event.dart
@@ -9,8 +9,16 @@ class ChangePerspective extends PerspectivesEvent {
   ChangePerspective(this.perspectiveModel);
 }
 
-class CreatePerspective extends PerspectivesEvent {}
+class CreatePerspective extends PerspectivesEvent {
+  final String name;
+  final String description;
+  final List<String> members;
+  CreatePerspective(this.name, this.description, this.members);
+}
 
-class RemovePerspective extends PerspectivesEvent {}
+class RemovePerspective extends PerspectivesEvent {
+  final PerspectiveModel perspective;
+  RemovePerspective(this.perspective);
+}
 
 // class EditPerspective extends PerspectivesEvent {}
