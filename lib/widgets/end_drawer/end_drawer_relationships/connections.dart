b386d170037e88413aeff3e434a31b0cad090430
Eric Yang <eric@junto.love>
2020-03-24 16:08:22 -0400

    Build connections widget and implement in relations

---
+++ lib/widgets/end_drawer/end_drawer_relationships/connections.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:junto_beta_mobile/backend/repositories.dart';
+import 'package:junto_beta_mobile/models/models.dart';
+import 'package:junto_beta_mobile/widgets/previews/member_preview/member_preview.dart';
+import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer_relationships/error_widget.dart';
+import 'package:provider/provider.dart';
+
+class Connections extends StatefulWidget {
+  const Connections();
+
+  @override
+  State<StatefulWidget> createState() {
+    return ConnectionsState();
+  }
+}
+
+class ConnectionsState extends State<Connections> {
+  Future<dynamic> _userRelations;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _userRelations = getUserRelationships();
+  }
+
+  Future<dynamic> getUserRelationships() async {
+    final dynamic userRelations =
+        await Provider.of<UserRepo>(context, listen: false).userRelations();
+
+    return userRelations;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<dynamic>(
+      future: _userRelations,
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          // get list of connections
+          final List<UserProfile> _connectionsMembers =
+              snapshot.data['connections']['results'];
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            children: _connectionsMembers
+                .map(
+                  (dynamic connection) => MemberPreview(profile: connection),
+                )
+                .toList(),
+          );
+        } else if (snapshot.hasError) {
+          print(snapshot.error);
+          return JuntoErrorWidget(errorMessage: 'Hmm, something went wrong');
+        }
+        return Center(
+          child: JuntoProgressIndicator(),
+        );
+      },
+    );
+  }
+}
