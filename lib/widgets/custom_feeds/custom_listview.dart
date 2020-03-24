0731d3b5b729d591145f4a5925cdb6fa69919275
Neevash Ramdial <mail@neevash.dev>
2020-03-24 15:30:08 -0400

    created callback for pagination pos

--- lib/widgets/custom_feeds/custom_listview.dart
+++ lib/widgets/custom_feeds/custom_listview.dart
@@ -9,64 +9,80 @@ class TwoColumnListView extends StatelessWidget {
     Key key,
     @required this.data,
     @required this.privacyLayer,
+    this.scrollChanged,
   }) : super(key: key);
 
   final List<ExpressionResponse> data;
   final String privacyLayer;
+  final ValueChanged<ScrollNotification> scrollChanged;
+
+  bool _onScrollNotification(ScrollNotification scrollNotification) {
+    final metrics = scrollNotification.metrics;
+    double scrollPercent = (metrics.pixels / metrics.maxScrollExtent) * 100;
+    if (scrollPercent.roundToDouble() == 60.0) {
+      scrollChanged(scrollNotification);
+      return true;
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      shrinkWrap: true,
-      physics: const ClampingScrollPhysics(),
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width * .5,
-              padding: const EdgeInsets.only(left: 10, right: 5, top: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  for (int index = 0; index < data.length + 1; index++)
-                    if (index == data.length)
-                      const SizedBox()
-                    else if (index.isEven &&
-                        data[index].privacy == privacyLayer)
-                      TwoColumnExpressionPreview(
-                        expression: data[index],
-                      )
-                    else
-                      const SizedBox()
+    return NotificationListener(
+      onNotification: _onScrollNotification,
+      child: ListView(
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * .5,
+                padding: const EdgeInsets.only(left: 10, right: 5, top: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    for (int index = 0; index < data.length + 1; index++)
+                      if (index == data.length)
+                        const SizedBox()
+                      else if (index.isEven &&
+                          data[index].privacy == privacyLayer)
+                        TwoColumnExpressionPreview(
+                          expression: data[index],
+                        )
+                      else
+                        const SizedBox()
 
-                  // even number indexes
-                ],
+                    // even number indexes
+                  ],
+                ),
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * .5,
-              padding: const EdgeInsets.only(left: 5, right: 10, top: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  // odd number indexes
-                  for (int index = 0; index < data.length + 1; index++)
-                    if (index == data.length)
-                      const SizedBox()
-                    else if (index.isOdd && data[index].privacy == privacyLayer)
-                      TwoColumnExpressionPreview(
-                        expression: data[index],
-                      )
-                    else
-                      const SizedBox()
-                ],
+              Container(
+                width: MediaQuery.of(context).size.width * .5,
+                padding: const EdgeInsets.only(left: 5, right: 10, top: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    // odd number indexes
+                    for (int index = 0; index < data.length + 1; index++)
+                      if (index == data.length)
+                        const SizedBox()
+                      else if (index.isOdd &&
+                          data[index].privacy == privacyLayer)
+                        TwoColumnExpressionPreview(
+                          expression: data[index],
+                        )
+                      else
+                        const SizedBox()
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ],
+            ],
+          )
+        ],
+      ),
     );
   }
 }
@@ -76,61 +92,77 @@ class TwoColumnSliverListView extends StatelessWidget {
   const TwoColumnSliverListView({
     Key key,
     @required this.data,
+    this.scrollChanged,
   }) : super(key: key);
 
   final List<ExpressionResponse> data;
 
+  final ValueChanged<ScrollNotification> scrollChanged;
+
+  bool _onScrollNotification(ScrollNotification scrollNotification) {
+    final metrics = scrollNotification.metrics;
+    double scrollPercent = (metrics.pixels / metrics.maxScrollExtent) * 100;
+    if (scrollPercent.roundToDouble() == 60.0) {
+      scrollChanged(scrollNotification);
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width * .5,
-            padding: const EdgeInsets.only(
-              top: 10,
-              left: 10,
-              right: 5,
+    return NotificationListener(
+      onNotification: _onScrollNotification,
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * .5,
+              padding: const EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 5,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  for (int index = 0; index < data.length + 1; index++)
+                    if (index == data.length)
+                      const SizedBox()
+                    else if (index.isEven)
+                      TwoColumnExpressionPreview(
+                        key: ValueKey<String>(data[index].address),
+                        expression: data[index],
+                      )
+                ],
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                for (int index = 0; index < data.length + 1; index++)
-                  if (index == data.length)
-                    const SizedBox()
-                  else if (index.isEven)
-                    TwoColumnExpressionPreview(
-                      key: ValueKey<String>(data[index].address),
-                      expression: data[index],
-                    )
-              ],
+            Container(
+              width: MediaQuery.of(context).size.width * .5,
+              padding: const EdgeInsets.only(
+                top: 10,
+                left: 5,
+                right: 10,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  for (int index = 0; index < data.length + 1; index++)
+                    if (index == data.length)
+                      const SizedBox()
+                    else if (index.isOdd)
+                      TwoColumnExpressionPreview(
+                        key: ValueKey<String>(data[index].address),
+                        expression: data[index],
+                      )
+                ],
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * .5,
-            padding: const EdgeInsets.only(
-              top: 10,
-              left: 5,
-              right: 10,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                for (int index = 0; index < data.length + 1; index++)
-                  if (index == data.length)
-                    const SizedBox()
-                  else if (index.isOdd)
-                    TwoColumnExpressionPreview(
-                      key: ValueKey<String>(data[index].address),
-                      expression: data[index],
-                    )
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -142,28 +174,44 @@ class SingleColumnListView extends StatelessWidget {
     Key key,
     @required this.data,
     @required this.privacyLayer,
+    this.scrollChanged,
   }) : super(key: key);
 
   final List<ExpressionResponse> data;
   final String privacyLayer;
 
+  final ValueChanged<ScrollNotification> scrollChanged;
+
+  bool _onScrollNotification(ScrollNotification scrollNotification) {
+    final metrics = scrollNotification.metrics;
+    double scrollPercent = (metrics.pixels / metrics.maxScrollExtent) * 100;
+    if (scrollPercent.roundToDouble() == 60.0) {
+      scrollChanged(scrollNotification);
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      child: ListView(
-        shrinkWrap: true,
-        physics: const ClampingScrollPhysics(),
-        children: <Widget>[
-          for (int index = 0; index < data.length + 1; index++)
-            if (index == data.length)
-              const SizedBox()
-            else if (data[index].privacy == privacyLayer)
-              SingleColumnExpressionPreview(
-                key: ValueKey<String>(data[index].address),
-                expression: data[index],
-              )
-        ],
+    return NotificationListener(
+      onNotification: _onScrollNotification,
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        child: ListView(
+          shrinkWrap: true,
+          physics: const ClampingScrollPhysics(),
+          children: <Widget>[
+            for (int index = 0; index < data.length + 1; index++)
+              if (index == data.length)
+                const SizedBox()
+              else if (data[index].privacy == privacyLayer)
+                SingleColumnExpressionPreview(
+                  key: ValueKey<String>(data[index].address),
+                  expression: data[index],
+                )
+          ],
+        ),
       ),
     );
   }
@@ -171,29 +219,46 @@ class SingleColumnListView extends StatelessWidget {
 
 /// Sliver implementation of the custom `ListView` used across Junto.
 class SingleColumnSliverListView extends StatelessWidget {
-  const SingleColumnSliverListView(
-      {Key key, @required this.data, @required this.privacyLayer})
-      : super(key: key);
+  const SingleColumnSliverListView({
+    Key key,
+    @required this.data,
+    @required this.privacyLayer,
+    @required this.scrollChanged,
+  }) : super(key: key);
 
   final List<ExpressionResponse> data;
   final String privacyLayer;
+  final ValueChanged<ScrollNotification> scrollChanged;
+
+  bool _onScrollNotification(ScrollNotification scrollNotification) {
+    final metrics = scrollNotification.metrics;
+    double scrollPercent = (metrics.pixels / metrics.maxScrollExtent) * 100;
+    if (scrollPercent.roundToDouble() == 60.0) {
+      scrollChanged(scrollNotification);
+      return true;
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).backgroundColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          for (int index = 0; index < data.length + 1; index++)
-            if (index == data.length)
-              const SizedBox()
-            else if (data[index].privacy == privacyLayer)
-              SingleColumnExpressionPreview(
-                key: ValueKey<String>(data[index].address),
-                expression: data[index],
-              )
-        ],
+    return NotificationListener(
+      onNotification: _onScrollNotification,
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            for (int index = 0; index < data.length + 1; index++)
+              if (index == data.length)
+                const SizedBox()
+              else if (data[index].privacy == privacyLayer)
+                SingleColumnExpressionPreview(
+                  key: ValueKey<String>(data[index].address),
+                  expression: data[index],
+                )
+          ],
+        ),
       ),
     );
   }
