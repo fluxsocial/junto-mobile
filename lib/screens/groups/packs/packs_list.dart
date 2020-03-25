251ddc5e805f8ce55838ca618727bab6da0994e3
Dominik Roszkowski <dominik@roszkowski.dev>
2020-03-25 11:19:17 +0100

    Extract files

--- lib/screens/groups/packs/packs_list.dart
+++ lib/screens/groups/packs/packs_list.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:junto_beta_mobile/backend/backend.dart';
 import 'package:junto_beta_mobile/models/models.dart';
-import 'package:junto_beta_mobile/screens/groups/bloc/group_bloc.dart';
-import 'package:junto_beta_mobile/utils/utils.dart';
-import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer_relationships/error_widget.dart';
-import 'package:junto_beta_mobile/widgets/previews/pack_preview/pack_preview.dart';
-import 'package:junto_beta_mobile/widgets/previews/pack_preview/pack_request.dart';
-import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
-import 'package:provider/provider.dart';
+import 'package:junto_beta_mobile/screens/groups/packs/my_packs.dart';
+import 'package:junto_beta_mobile/screens/groups/packs/pack_requests.dart';
 
 class PacksList extends StatefulWidget {
   const PacksList({this.selectedGroup});
@@ -118,149 +111,13 @@ class PacksListState extends State<PacksList> {
                 });
               },
               children: <Widget>[
-                MyPacks(
-                  selectedGroup: widget.selectedGroup,
-                ),
+                MyPacks(selectedGroup: widget.selectedGroup),
                 PackRequests(),
               ],
             ),
           )
         ],
       ),
-    );
-  }
-}
-
-class PackRequests extends StatefulWidget {
-  @override
-  _PackRequestsState createState() => _PackRequestsState();
-}
-
-class _PackRequestsState extends State<PackRequests> {
-  Widget _loader() {
-    return Expanded(
-      child: Center(
-        child: Transform.translate(
-          offset: const Offset(0.0, -50),
-          child: JuntoProgressIndicator(),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<UserDataProvider>(builder: (context, user, _) {
-      return Column(
-        children: <Widget>[
-          BlocBuilder<GroupBloc, GroupBlocState>(
-            builder: (BuildContext context, GroupBlocState state) {
-              if (state is GroupLoading) {
-                return _loader();
-              }
-              if (state is GroupLoaded) {
-                return Expanded(
-                  child: ListView(
-                    padding: const EdgeInsets.all(0),
-                    children: <Widget>[
-                      for (Group packRequest
-                          in state.notifications.groupJoinNotifications)
-                        if (packRequest.groupType == 'Pack')
-                          PackRequest(
-                            userProfile: user.userProfile,
-                            pack: packRequest,
-                            refreshGroups: () async {
-                              context.bloc<GroupBloc>().add(FetchMyPack());
-                            },
-                          )
-                    ],
-                  ),
-                );
-              }
-              if (state is GroupError) {
-                return Expanded(
-                  child: Center(
-                    child: Transform.translate(
-                      offset: const Offset(0.0, -50),
-                      child: const Text('Hmm, something is up with our server'),
-                    ),
-                  ),
-                );
-              }
-              return _loader();
-            },
-          ),
-        ],
-      );
-    });
-  }
-}
-
-class MyPacks extends StatefulWidget {
-  const MyPacks({
-    Key key,
-    @required this.selectedGroup,
-  }) : super(key: key);
-  final ValueChanged<Group> selectedGroup;
-
-  @override
-  _MyPacksState createState() => _MyPacksState();
-}
-
-class _MyPacksState extends State<MyPacks> with ListDistinct {
-  UserData _userProfile;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _userProfile = Provider.of<UserDataProvider>(context).userProfile;
-  }
-
-  Widget _loader() {
-    return Expanded(
-      child: Center(
-        child: Transform.translate(
-          offset: const Offset(0.0, -50),
-          child: JuntoProgressIndicator(),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        BlocBuilder<GroupBloc, GroupBlocState>(
-          builder: (context, state) {
-            if (state is GroupLoading) {
-              return _loader();
-            }
-            if (state is GroupLoaded) {
-              return Expanded(
-                  child: ListView(
-                padding: const EdgeInsets.all(0),
-                children: <Widget>[
-                  for (Group group in state.groups)
-                    GestureDetector(
-                      onTap: () => widget.selectedGroup(group),
-                      child: PackPreview(
-                        group: group,
-                        userProfile: _userProfile,
-                      ),
-                    )
-                ],
-              ));
-            }
-            if (state is GroupError) {
-              JuntoErrorWidget(
-                errorMessage: state.groupError,
-              );
-            }
-            return SizedBox();
-          },
-        ),
-      ],
     );
   }
 }
