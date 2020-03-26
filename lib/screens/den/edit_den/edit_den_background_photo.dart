35c8d1841e30552e9f75859ec0c3f9954e5c75ad
Eric <37339459+sunyatax@users.noreply.github.com>
2020-03-26 05:00:51 -0400

    Revert "resolve den crash"
    This reverts commit 97425fdaa3661ab8e55110775a4c3661ffa6c4d1.

--- lib/screens/den/edit_den/edit_den_background_photo.dart
+++ lib/screens/den/edit_den/edit_den_background_photo.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
+import 'package:junto_beta_mobile/widgets/member_widgets/background_placeholder.dart';
 import 'package:junto_beta_mobile/models/user_model.dart';
 import 'package:junto_beta_mobile/widgets/member_widgets/background_photo.dart';
-import 'package:junto_beta_mobile/widgets/member_widgets/background_placeholder.dart';
 
 class EditDenBackgroundPhoto extends StatelessWidget {
   const EditDenBackgroundPhoto({
@@ -13,24 +13,10 @@ class EditDenBackgroundPhoto extends StatelessWidget {
     this.backgroundPhotoFile,
     this.currentTheme,
   });
-
   final UserData profile;
   final File backgroundPhotoFile;
   final Function onPickPressed;
   final String currentTheme;
-
-  Widget _backgroundImage() {
-    if (backgroundPhotoFile == null) {
-      MemberBackgroundPlaceholder(theme: currentTheme);
-    }
-    if (backgroundPhotoFile == profile.user.profilePicture.isNotEmpty) {
-      return MemberBackgroundPhoto(
-        profile: profile,
-      );
-    }
-    return MemberBackgroundPlaceholder(theme: currentTheme);
-  }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -39,7 +25,21 @@ class EditDenBackgroundPhoto extends StatelessWidget {
       },
       child: Stack(
         children: <Widget>[
-          if (profile != null) _backgroundImage(),
+          if (profile != null)
+            if (backgroundPhotoFile == null)
+              profile.user.backgroundPhoto.isNotEmpty ||
+                      profile.user.backgroundPhoto != ''
+                  ? MemberBackgroundPhoto(profile: profile)
+                  : MemberBackgroundPlaceholder(theme: currentTheme)
+            else
+              Container(
+                height: MediaQuery.of(context).size.height * .2,
+                width: MediaQuery.of(context).size.width,
+                child: Image.file(
+                  backgroundPhotoFile,
+                  fit: BoxFit.cover,
+                ),
+              ),
           Container(
             height: MediaQuery.of(context).size.height * .2,
             width: MediaQuery.of(context).size.width,
