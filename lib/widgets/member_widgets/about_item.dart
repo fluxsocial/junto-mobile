cf562ff6918676ec52fcd957405d49fe2ec36fad
Eric Yang <eric@junto.love>
2020-03-26 01:40:40 -0400

    Enable wrap for about items

--- lib/widgets/member_widgets/about_item.dart
+++ lib/widgets/member_widgets/about_item.dart
@@ -13,48 +13,50 @@ class AboutItem extends StatelessWidget {
     return item.isNotEmpty && item[0].isNotEmpty
         ? Container(
             margin: const EdgeInsets.symmetric(vertical: 5),
+            width: MediaQuery.of(context).size.width,
             child: Row(
               children: <Widget>[
                 icon,
                 const SizedBox(width: 5),
-                GestureDetector(
-                  onTap: () async {
-                    if (isWebsite) {
-                      String url = item[0];
-                      if (!item[0].startsWith('https://') ||
-                          !item[0].startsWith('http:') ||
-                          !item[0].startsWith('https:')) {
-                        url = 'https://${item[0]}';
-                      }
-                      if (await canLaunch(url)) {
-                        await launch(url);
+                Flexible(
+                  child: GestureDetector(
+                    onTap: () async {
+                      if (isWebsite) {
+                        String url = item[0];
+                        if (!item[0].startsWith('https://') ||
+                            !item[0].startsWith('http:') ||
+                            !item[0].startsWith('https:')) {
+                          url = 'https://${item[0]}';
+                        }
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) =>
+                                const SingleActionDialog(
+                              dialogText:
+                                  'Could not launch this website. Try typing it in your browser.',
+                            ),
+                          );
+                        }
                       } else {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) =>
-                              const SingleActionDialog(
-                            dialogText:
-                                'Could not launch this website. Try typing it in your browser.',
-                          ),
-                        );
+                        return;
                       }
-                    } else {
-                      return;
-                    }
-                  },
-                  child: Text(
-                    item[0],
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: isWebsite
-                          ? Colors.blue
-                          : Theme.of(context).primaryColor,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600,
+                    },
+                    child: Text(
+                      item[0],
+                      // maxLines: 2,
+                      style: TextStyle(
+                        color: isWebsite
+                            ? Colors.blue
+                            : Theme.of(context).primaryColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           )
