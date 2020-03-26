525eb957ae244af60cd0a89f202ef5d74d75b2c1
Neevash Ramdial <mail@neevash.dev>
2020-03-26 17:54:58 -0400

    remove notification listener
    Since these views are wrapped in other listviews, the notification events are not triggered.

--- lib/widgets/custom_feeds/single_listview.dart
+++ lib/widgets/custom_feeds/single_listview.dart
@@ -57,42 +57,27 @@ class SingleColumnSliverListView extends StatelessWidget {
     Key key,
     @required this.data,
     @required this.privacyLayer,
-    @required this.scrollChanged,
   }) : super(key: key);
 
   final List<ExpressionResponse> data;
   final String privacyLayer;
-  final ValueChanged<ScrollNotification> scrollChanged;
-
-  bool _onScrollNotification(ScrollNotification scrollNotification) {
-    final metrics = scrollNotification.metrics;
-    double scrollPercent = (metrics.pixels / metrics.maxScrollExtent) * 100;
-    if (scrollPercent.roundToDouble() == 60.0) {
-      scrollChanged(scrollNotification);
-      return true;
-    }
-    return false;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener(
-      onNotification: _onScrollNotification,
-      child: Container(
-        color: Theme.of(context).backgroundColor,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            for (int index = 0; index < data.length + 1; index++)
-              if (index == data.length)
-                const SizedBox()
-              else if (data[index].privacy == privacyLayer)
-                SingleColumnExpressionPreview(
-                  key: ValueKey<String>(data[index].address),
-                  expression: data[index],
-                )
-          ],
-        ),
+    return Container(
+      color: Theme.of(context).backgroundColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          for (int index = 0; index < data.length + 1; index++)
+            if (index == data.length)
+              const SizedBox()
+            else if (data[index].privacy == privacyLayer)
+              SingleColumnExpressionPreview(
+                key: ValueKey<String>(data[index].address),
+                expression: data[index],
+              )
+        ],
       ),
     );
   }
