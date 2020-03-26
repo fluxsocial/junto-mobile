b0b0e2fb8a2c6246aaca2271fc5e9a7398390064
Neevash Ramdial <mail@neevash.dev>
2020-03-26 12:19:45 -0400

    resolved overflow

--- lib/widgets/appbar/collective_appbar.dart
+++ lib/widgets/appbar/collective_appbar.dart
@@ -28,60 +28,62 @@ class CollectiveAppBar extends SliverPersistentHeaderDelegate {
           color: Colors.blue,
           child: Column(
             children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height * .1,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.only(bottom: 10),
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Theme.of(context).dividerColor,
-                      width: .75,
-                    ),
-                  ),
-                  color: Theme.of(context).backgroundColor,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      alignment: Alignment.bottomLeft,
-                      padding: const EdgeInsets.only(left: 10),
-                      color: Colors.transparent,
-                      height: 36,
-                      child: Row(
-                        children: <Widget>[
-                          Image.asset(
-                            'assets/images/junto-mobile__logo.png',
-                            height: 22.0,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          const SizedBox(width: 7.5),
-                          Text(
-                            appbarTitle ?? 'JUNTO',
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                        ],
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * .1,
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Theme.of(context).dividerColor,
+                        width: .75,
                       ),
                     ),
-                    Row(
-                      children: <Widget>[
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 42,
-                            color: Colors.transparent,
-                            alignment: Alignment.bottomRight,
-                            padding: const EdgeInsets.only(right: 10),
-                            child: Icon(CustomIcons.moon,
-                                size: 22,
-                                color: Theme.of(context).primaryColor),
-                          ),
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        padding: const EdgeInsets.only(left: 10),
+                        color: Colors.transparent,
+                        height: 36,
+                        child: Row(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/junto-mobile__logo.png',
+                              height: 22.0,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            const SizedBox(width: 7.5),
+                            Text(
+                              appbarTitle ?? 'JUNTO',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ],
                         ),
-                      ],
-                    )
-                  ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              width: 42,
+                              color: Colors.transparent,
+                              alignment: Alignment.bottomRight,
+                              padding: const EdgeInsets.only(right: 10),
+                              child: Icon(CustomIcons.moon,
+                                  size: 22,
+                                  color: Theme.of(context).primaryColor),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
               Container(
