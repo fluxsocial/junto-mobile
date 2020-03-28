4d83b1c5adb7dbb642f73821b926129eb4ee303f
Neevash Ramdial <mail@neevash.dev>
2020-03-28 12:54:39 -0400

    fixed pagination

--- lib/screens/collective/perspectives/expression_feed.dart
+++ lib/screens/collective/perspectives/expression_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:junto_beta_mobile/screens/collective/bloc/collective_bloc.dart';
 import 'package:junto_beta_mobile/screens/collective/perspectives/appbar_wrapper.dart';
@@ -13,7 +14,7 @@ enum ExpressionFeedLayout { single, two }
 /// Homepage feed containing a List of expression for the given perspective.
 /// The following parameters must be supplied:
 ///  - [collectiveController]
-class ExpressionFeed extends StatelessWidget {
+class ExpressionFeed extends StatefulWidget {
   const ExpressionFeed({
     Key key,
     @required this.collectiveController,
@@ -22,16 +23,59 @@ class ExpressionFeed extends StatelessWidget {
   final ScrollController collectiveController;
 
   @override
+  _ExpressionFeedState createState() => _ExpressionFeedState();
+}
+
+class _ExpressionFeedState extends State<ExpressionFeed> {
+  ScrollController _controller;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _controller = widget.collectiveController;
+    _controller.addListener(_scrollListener);
+  }
+
+  @override
+  void didUpdateWidget(ExpressionFeed oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.collectiveController != widget.collectiveController) {
+      _controller.removeListener(_scrollListener);
+      _controller = widget.collectiveController;
+      _controller.addListener(_scrollListener);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.removeListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_controller.hasClients) {
+      final ScrollDirection direction =
+          _controller.position.userScrollDirection;
+      final double pixels = _controller.position.pixels;
+      final double maxExtent = _controller.position.maxScrollExtent;
+      double percent = (pixels / maxExtent) * 100;
+      if (percent.roundToDouble() == 60 &&
+          direction == ScrollDirection.reverse) {
+        context.bloc<CollectiveBloc>().add(FetchMoreCollective());
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ExpressionScrollRefresh(
       child: BlocBuilder<CollectiveBloc, CollectiveState>(
         builder: (BuildContext context, CollectiveState state) {
           if (state is CollectiveError) {
-            //TODO(Nash): Add illustration for error state.
             return CollectiveErrorLabel();
           }
           return CustomScrollView(
-            controller: collectiveController,
+            controller: widget.collectiveController,
             slivers: <Widget>[
               AppBarWrapper(
                 title: state is CollectivePopulated ? state.name : 'JUNTO',
