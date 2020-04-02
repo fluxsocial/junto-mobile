fb9865822867e2fcceef80d1ab595b053bf2dedb
Eric Yang <eric@junto.love>
2020-04-02 17:01:19 -0400

    Resolve feature discovery in den and groups

--- lib/screens/den/den.dart
+++ lib/screens/den/den.dart
@@ -19,6 +19,7 @@ import 'package:junto_beta_mobile/widgets/member_widgets/about_member.dart';
 import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
 import 'package:junto_beta_mobile/widgets/utils/hide_fab.dart';
 import 'package:provider/provider.dart';
+import 'package:feature_discovery/feature_discovery.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Displays the user's DEN or "profile screen"
@@ -105,54 +106,56 @@ class JuntoDenState extends State<JuntoDen>
 
   @override
   Widget build(BuildContext context) {
-    return NotificationListener<ScrollUpdateNotification>(
-      onNotification: (value) => hideOrShowFab(value, _isVisible),
-      child: BlocProvider(
-        create: (context) => DenBloc(
-            Provider.of<UserRepo>(context, listen: false),
-            Provider.of<UserDataProvider>(context, listen: false))
-          ..add(LoadDen(_userProfile.user.address)),
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: JuntoFilterDrawer(
-            leftDrawer:
-                const FilterDrawerContent(ExpressionContextType.Collective),
-            rightMenu: JuntoDrawer(),
-            scaffold: Scaffold(
-              appBar: _constructAppBar(),
-              floatingActionButton: ValueListenableBuilder<bool>(
-                valueListenable: _isVisible,
-                builder: (
-                  BuildContext context,
-                  bool visible,
-                  Widget child,
-                ) {
-                  return AnimatedOpacity(
-                    duration: const Duration(milliseconds: 300),
-                    opacity: visible ? 1.0 : 0.0,
-                    child: child,
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 25),
-                  child: BottomNav(
-                    actionsVisible: false,
-                    onLeftButtonTap: () {
-                      // open about page
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute<dynamic>(
-                          builder: (BuildContext context) =>
-                              AboutMember(profile: _userProfile),
-                        ),
-                      );
-                    },
+    return FeatureDiscovery(
+      child: NotificationListener<ScrollUpdateNotification>(
+        onNotification: (value) => hideOrShowFab(value, _isVisible),
+        child: BlocProvider(
+          create: (context) => DenBloc(
+              Provider.of<UserRepo>(context, listen: false),
+              Provider.of<UserDataProvider>(context, listen: false))
+            ..add(LoadDen(_userProfile.user.address)),
+          child: Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: JuntoFilterDrawer(
+              leftDrawer:
+                  const FilterDrawerContent(ExpressionContextType.Collective),
+              rightMenu: JuntoDrawer(),
+              scaffold: Scaffold(
+                appBar: _constructAppBar(),
+                floatingActionButton: ValueListenableBuilder<bool>(
+                  valueListenable: _isVisible,
+                  builder: (
+                    BuildContext context,
+                    bool visible,
+                    Widget child,
+                  ) {
+                    return AnimatedOpacity(
+                      duration: const Duration(milliseconds: 300),
+                      opacity: visible ? 1.0 : 0.0,
+                      child: child,
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 25),
+                    child: BottomNav(
+                      actionsVisible: false,
+                      onLeftButtonTap: () {
+                        // open about page
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute<dynamic>(
+                            builder: (BuildContext context) =>
+                                AboutMember(profile: _userProfile),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.centerDocked,
+                body: _buildBody(),
               ),
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerDocked,
-              body: _buildBody(),
             ),
           ),
         ),
