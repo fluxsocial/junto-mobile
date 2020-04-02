ca0292c1d0bf6d09fb224f0f2857be8230730ab3
Eric Yang <eric@junto.love>
2020-04-02 18:18:20 -0400

    Build learn more section in tutorial and implement in lotus

--- lib/widgets/tutorial/described_feature_overlay.dart
+++ lib/widgets/tutorial/described_feature_overlay.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:feature_discovery/feature_discovery.dart';
 
-class JuntoDescribedFeatureOverlay extends StatelessWidget {
+class JuntoDescribedFeatureOverlay extends StatefulWidget {
   const JuntoDescribedFeatureOverlay({
     this.icon,
     this.featureId,
@@ -11,6 +11,8 @@ class JuntoDescribedFeatureOverlay extends StatelessWidget {
     this.learnMore = false,
     this.isLastFeature = false,
     this.oneFeature = false,
+    this.hasUpNext = false,
+    this.learnMoreText,
     this.child,
   });
 
@@ -24,8 +26,20 @@ class JuntoDescribedFeatureOverlay extends StatelessWidget {
   final bool isLastFeature;
   // only one feature in tutorial
   final bool oneFeature;
+  // has an up next / learn more section
+  final bool hasUpNext;
+  final String learnMoreText;
   final Widget child;
 
+  @override
+  State<StatefulWidget> createState() {
+    return JuntoDescribedFeatureOverlayState();
+  }
+}
+
+class JuntoDescribedFeatureOverlayState
+    extends State<JuntoDescribedFeatureOverlay> {
+  bool upNextVisible = false;
   _actionItemButton(BuildContext context, String name, Function onPressed) {
     return GestureDetector(
       onTap: onPressed,
@@ -57,47 +71,155 @@ class JuntoDescribedFeatureOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DescribedFeatureOverlay(
-      tapTarget: icon,
-      featureId: featureId,
+      tapTarget: widget.icon,
+      featureId: widget.featureId,
       backgroundColor: Theme.of(context).accentColor,
-      contentLocation: contentLocation,
+      contentLocation: widget.contentLocation,
       overflowMode: OverflowMode.extendBackground,
-      title: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: Text(
-          title,
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ),
       targetColor: Theme.of(context).backgroundColor,
       enablePulsingAnimation: false,
-      description: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      description: Stack(
         children: <Widget>[
-          if (learnMore)
-            _actionItemButton(
-              context,
-              'Learn More',
-              () {},
-            ),
-          if (!isLastFeature && !oneFeature)
-            _actionItemButton(
-              context,
-              'Next Feature',
-              () async => FeatureDiscovery.completeCurrentStep(context),
-            ),
-          _actionItemButton(
-            context,
-            'Dismiss',
-            () async => FeatureDiscovery.dismissAll(context),
+          _tutorialDescription(),
+          _learnMore(
+            context: context,
+            learnMoreText: widget.learnMoreText,
+            hasUpNext: false,
           ),
         ],
       ),
-      child: child,
+      child: widget.child,
+    );
+  }
+
+  Widget _tutorialDescription() {
+    return AnimatedOpacity(
+      opacity: upNextVisible ? 0 : 1,
+      duration: Duration(milliseconds: 300),
+      child: Visibility(
+        visible: !upNextVisible,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                widget.title,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            if (widget.learnMore)
+              _actionItemButton(
+                context,
+                'Learn More',
+                () {
+                  setState(() {
+                    upNextVisible = true;
+                  });
+                },
+              ),
+            if (!widget.isLastFeature && !widget.oneFeature)
+              _actionItemButton(
+                context,
+                'Next Feature',
+                () async => FeatureDiscovery.completeCurrentStep(context),
+              ),
+            _actionItemButton(
+              context,
+              'Dismiss',
+              () async => FeatureDiscovery.dismissAll(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _learnMore(
+      {BuildContext context,
+      String learnMoreText,
+      bool hasUpNext,
+      String upNext}) {
+    return AnimatedOpacity(
+      opacity: upNextVisible ? 1 : 0,
+      duration: Duration(milliseconds: 300),
+      child: Visibility(
+        visible: upNextVisible,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'DESIGN INSPIRATION',
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              learnMoreText,
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            if (hasUpNext)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 25),
+                  Text(
+                    'UP NEXT',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  upNextVisible = false;
+                });
+              },
+              child: Container(
+                color: Colors.transparent,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      'BACK',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
