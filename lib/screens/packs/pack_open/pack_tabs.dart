158cc7596d34f7b548e6514fa235e57deeccd35f
Neevash Ramdial <mail@neevash.dev>
2020-04-02 22:05:46 -0400

    wired refresh pack expressions

--- lib/screens/packs/pack_open/pack_tabs.dart
+++ lib/screens/packs/pack_open/pack_tabs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:junto_beta_mobile/models/models.dart';
 import 'package:junto_beta_mobile/screens/packs/pack_open/pack_members.dart';
+import 'package:junto_beta_mobile/screens/packs/packs_bloc/pack_bloc.dart';
 import 'package:junto_beta_mobile/widgets/custom_feeds/group_expressions.dart';
 import 'package:junto_beta_mobile/widgets/drawer/junto_filter_drawer.dart';
 
@@ -11,6 +13,10 @@ class PackTabs extends StatelessWidget {
   }) : super(key: key);
 
   final Group group;
+
+  Future<void> _fetchMore(BuildContext context) async {
+    context.bloc<PackBloc>().add(RefreshPacks());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,19 +35,28 @@ class PackTabs extends StatelessWidget {
         },
         child: TabBarView(
           children: <Widget>[
-            GroupExpressions(
-              key: const PageStorageKey<String>('public-pack'),
-              group: group,
-              privacy: 'Public',
+            RefreshIndicator(
+              onRefresh: () => _fetchMore(context),
+              child: GroupExpressions(
+                key: const PageStorageKey<String>('public-pack'),
+                group: group,
+                privacy: 'Public',
+              ),
             ),
-            GroupExpressions(
-              key: const PageStorageKey<String>('private-pack'),
-              group: group,
-              privacy: 'Private',
+            RefreshIndicator(
+              onRefresh: () => _fetchMore(context),
+              child: GroupExpressions(
+                key: const PageStorageKey<String>('private-pack'),
+                group: group,
+                privacy: 'Private',
+              ),
             ),
-            PackOpenMembers(
-              key: UniqueKey(),
-              packAddress: group.address,
+            RefreshIndicator(
+              onRefresh: () => _fetchMore(context),
+              child: PackOpenMembers(
+                key: UniqueKey(),
+                packAddress: group.address,
+              ),
             )
           ],
         ),
