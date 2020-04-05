64e9e17f7be7e4788258c6a1ef8095345a8b4244
Neevash Ramdial <mail@neevash.dev>
2020-04-05 13:37:50 -0400

    created button for fetching expressions

--- lib/screens/collective/perspectives/collective_populated_list.dart
+++ lib/screens/collective/perspectives/collective_populated_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:junto_beta_mobile/app/custom_icons.dart';
 import 'package:junto_beta_mobile/backend/backend.dart';
 import 'package:junto_beta_mobile/screens/collective/bloc/collective_bloc.dart';
 import 'package:junto_beta_mobile/widgets/custom_feeds/custom_listview.dart';
@@ -15,6 +17,7 @@ class CollectivePopulatedList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context);
     if (state.results.length == 0) {
       return SliverToBoxAdapter(
         child: const SizedBox(),
@@ -48,6 +51,19 @@ class CollectivePopulatedList extends StatelessWidget {
                     privacyLayer: 'Public',
                   ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: media.padding.bottom + 60.0,
+                top: 12.0,
+              ),
+              child: FlatButton.icon(
+                onPressed: () {
+                  context.bloc<CollectiveBloc>().add(FetchMoreCollective());
+                },
+                icon: Icon(CustomIcons.lotus),
+                label: Text('More'),
               ),
             ),
           ],
