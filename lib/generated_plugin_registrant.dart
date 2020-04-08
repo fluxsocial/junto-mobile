6e39a427ecd811aaf998d83d90ac147b547066e2
Dominik Roszkowski <dominik@roszkowski.dev>
2020-04-08 11:47:00 +0200

    Convert iOS project to Swift

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,6 +5,7 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:audioplayers/audioplayers_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -12,6 +13,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  AudioplayersPlugin.registerWith(registry.registrarFor(AudioplayersPlugin));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
