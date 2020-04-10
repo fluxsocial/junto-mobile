cde0ac43e948177e4ff3f6112dead801e952ff48
Neevash Ramdial <mail@neevash.dev>
2020-04-09 21:57:09 -0400

    implement caching methods

--- lib/backend/services/hive_service.dart
+++ lib/backend/services/hive_service.dart
@@ -1,37 +1,46 @@
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:junto_beta_mobile/models/models.dart';
-import 'package:path_provider/path_provider.dart';
 
 abstract class LocalCache {
   Future<void> insertExpressions(List<ExpressionResponse> expressions);
 
-  Future<List<ExpressionResponse>> retriveExpressions();
+  Future<List<ExpressionResponse>> retrieveExpressions();
 }
 
 class HiveCache implements LocalCache {
-  HiveCache(this.boxName) {
+  HiveCache() {
     init();
   }
 
-  final String boxName;
-
   Future<void> init() async {
-    final dir = await getApplicationDocumentsDirectory();
-    Hive.init(dir.path);
-    Hive.registerAdapter(ExpressionModelAdapter());
+    await Hive.initFlutter();
+    Hive.registerAdapter(ExpressionResponseAdapter());
+    Hive.registerAdapter(ShortFormExpressionAdapter());
+    Hive.registerAdapter(PhotoFormExpressionAdapter());
+    Hive.registerAdapter(LongFormExpressionAdapter());
+    Hive.registerAdapter(UserProfileAdapter());
   }
 
   @override
   Future<void> insertExpressions(List<ExpressionResponse> expressions) async {
+    final box = await Hive.openLazyBox<ExpressionResponse>('expressions');
     for (ExpressionResponse expression in expressions) {
-      final box = await Hive.openBox('expressions');
-      box.put(expression.address, expression)
+      if (!box.containsKey(expression.address)) {
+        box.put(expression.address, expression);
+      }
     }
+    retrieveExpressions();
   }
 
   @override
-  Future<List<ExpressionResponse>> retriveExpressions() {
-    // TODO: implement retriveExpressions
-    throw UnimplementedError();
+  Future<List<ExpressionResponse>> retrieveExpressions() async {
+    final box = await Hive.openLazyBox<ExpressionResponse>('expressions');
+    List<ExpressionResponse> items = [];
+    for (dynamic key in box.keys) {
+      ExpressionResponse res = await box.get(key);
+      items.add(res);
+    }
+    return items;
   }
 }
