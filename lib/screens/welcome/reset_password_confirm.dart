a306c6ea023de51cceeea988cf05a9ddfb8b2b64
Eric Yang <eric@junto.love>
2020-04-17 19:50:28 -0400

    Fix layout of all reset and sign in

--- lib/screens/welcome/reset_password_confirm.dart
+++ lib/screens/welcome/reset_password_confirm.dart
@@ -11,63 +11,68 @@ class ResetPasswordConfirm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          margin: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * .3,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        SignUpTextField(
-                          hint: S.of(context).welcome_password_hint,
-                          maxLength: 100,
-                          textInputActionType: TextInputAction.next,
-                          onSubmit: () {
-                            FocusScope.of(context).nextFocus();
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          textCapitalization: TextCapitalization.none,
-                        ),
-                        const SizedBox(height: 60),
-                        SignUpTextField(
-                          hint: S.of(context).welcome_confirm_password,
-                          maxLength: 100,
-                          textInputActionType: TextInputAction.next,
-                          onSubmit: () {
-                            FocusScope.of(context).nextFocus();
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          textCapitalization: TextCapitalization.none,
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 120),
-                      child: CallToActionButton(
-                        callToAction: () {},
-                        title: S.of(context).welcome_password_confirm,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: SignInBackNav(signInController: signInController),
+      ),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SignUpTextField(
+                    hint: S.of(context).welcome_verification_code,
+                    maxLength: 100,
+                    textInputActionType: TextInputAction.next,
+                    onSubmit: () {
+                      FocusScope.of(context).nextFocus();
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textCapitalization: TextCapitalization.none,
+                  ),
+                  const SizedBox(height: 45),
+                  SignUpTextField(
+                    hint: S.of(context).welcome_password_hint,
+                    maxLength: 100,
+                    textInputActionType: TextInputAction.next,
+                    onSubmit: () {
+                      FocusScope.of(context).nextFocus();
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textCapitalization: TextCapitalization.none,
+                  ),
+                  const SizedBox(height: 45),
+                  SignUpTextField(
+                    hint: S.of(context).welcome_confirm_password,
+                    maxLength: 100,
+                    textInputActionType: TextInputAction.next,
+                    onSubmit: () {
+                      FocusScope.of(context).nextFocus();
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textCapitalization: TextCapitalization.none,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 120),
+              child: CallToActionButton(
+                callToAction: () {},
+                title: S.of(context).welcome_password_confirm,
+              ),
+            ),
+          ],
         ),
-        SignInBackNav(signInController: signInController),
-      ],
+      ),
     );
   }
 }
--- lib/screens/welcome/reset_password_request.dart
+++ lib/screens/welcome/reset_password_request.dart
@@ -11,51 +11,52 @@ class ResetPasswordRequest extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(),
-                    SignUpTextField(
-                      hint: S.of(context).welcome_email_hint,
-                      maxLength: 100,
-                      textInputActionType: TextInputAction.next,
-                      onSubmit: () {
-                        FocusScope.of(context).nextFocus();
-                      },
-                      keyboardType: TextInputType.emailAddress,
-                      textCapitalization: TextCapitalization.none,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 120),
-                      child: CallToActionButton(
-                        callToAction: () {
-                          signInController.nextPage(
-                            curve: Curves.easeIn,
-                            duration: const Duration(milliseconds: 300),
-                          );
-                        },
-                        title: S.of(context).welcome_reset_password,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: SignInBackNav(signInController: signInController),
+      ),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SignUpTextField(
+                    hint: S.of(context).welcome_email_hint,
+                    maxLength: 100,
+                    textInputActionType: TextInputAction.next,
+                    onSubmit: () {
+                      FocusScope.of(context).nextFocus();
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textCapitalization: TextCapitalization.none,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 120),
+              child: CallToActionButton(
+                callToAction: () {
+                  signInController.nextPage(
+                    curve: Curves.easeIn,
+                    duration: const Duration(milliseconds: 300),
+                  );
+                },
+                title: S.of(context).welcome_reset_password,
+              ),
+            ),
+          ],
         ),
-        SignInBackNav(signInController: signInController),
-      ],
+      ),
     );
   }
 }
