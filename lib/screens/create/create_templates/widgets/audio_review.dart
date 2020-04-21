d3c8013854b5d19992ee76ea719df69d5c75415d
Dominik Roszkowski <dominik@roszkowski.dev>
2020-04-21 13:07:06 +0200

    Fix keyboard capitalization on audio review page

--- lib/screens/create/create_templates/widgets/audio_review.dart
+++ lib/screens/create/create_templates/widgets/audio_review.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/generated/l10n.dart';
 import 'package:junto_beta_mobile/screens/create/create_templates/audio_service.dart';
 import 'package:provider/provider.dart';
 import 'audio_play.dart';
@@ -13,97 +14,96 @@ class AudioReview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<AudioService>(builder: (context, audio, child) {
       return ListView(
+        physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width * 2 / 3,
-            child: Stack(
-              children: <Widget>[
-                if (audioPhotoBackground == null)
+          GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width * 2 / 3,
+              child: Stack(
+                children: <Widget>[
+                  if (audioPhotoBackground == null)
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width * 2 / 3,
+                      child: Image.asset(
+                        'assets/images/junto-mobile__themes--rainbow.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  if (audioPhotoBackground != null)
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width * 2 / 3,
+                      child: Image.file(
+                        audioPhotoBackground,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   Container(
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.width * 2 / 3,
-                    child: Image.asset(
-                      'assets/images/junto-mobile__themes--rainbow.png',
-                      fit: BoxFit.cover,
-                    ),
+                    color: Colors.black38,
                   ),
-                if (audioPhotoBackground != null)
                   Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.width * 2 / 3,
-                    child: Image.file(
-                      audioPhotoBackground,
-                      fit: BoxFit.cover,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 20,
                     ),
-                  ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.width * 2 / 3,
-                  color: Colors.black38,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 20,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          autofocus: false,
-                          buildCounter: (
-                            BuildContext context, {
-                            int currentLength,
-                            int maxLength,
-                            bool isFocused,
-                          }) =>
-                              null,
-                          decoration: InputDecoration(
-                            hintMaxLines: 25,
-                            hintStyle: TextStyle(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              hintMaxLines: 25,
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                              border: InputBorder.none,
+                              hintText: S.of(context).audio_title,
+                              counter: SizedBox(),
+                            ),
+                            cursorColor: Colors.white,
+                            cursorWidth: 2,
+                            maxLines: null,
+                            style: const TextStyle(
                               color: Colors.white,
                               fontSize: 20,
                               fontWeight: FontWeight.w700,
                             ),
-                            border: InputBorder.none,
-                            hintText: 'Title (optional)',
+                            maxLength: 80,
+                            textInputAction: TextInputAction.done,
+                            keyboardAppearance: Theme.of(context).brightness,
+                            textCapitalization: TextCapitalization.sentences,
                           ),
-                          cursorColor: Colors.white,
-                          cursorWidth: 2,
-                          maxLines: null,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                          ),
-                          maxLength: 80,
-                          textInputAction: TextInputAction.done,
-                          keyboardAppearance: Theme.of(context).brightness,
                         ),
-                      ),
-                      Row(
-                        children: <Widget>[
-                          AudioPlayButton(),
-                          AudioSeek(),
-                          Text(
-                            AudioPosition().getCurrentPosition(audio) +
-                                ' / ' +
-                                AudioPosition().getMaxDuration(audio),
-                            style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
+                        Row(
+                          children: <Widget>[
+                            AudioPlayButton(),
+                            AudioSeek(),
+                            Text(
+                              AudioPosition().getCurrentPosition(audio) +
+                                  ' / ' +
+                                  AudioPosition().getMaxDuration(audio),
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
