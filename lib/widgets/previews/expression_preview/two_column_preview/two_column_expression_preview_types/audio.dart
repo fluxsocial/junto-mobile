61a58bb64fb22b29d5492bd3806eb8f1e4b7a040
Eric <37339459+sunyatax@users.noreply.github.com>
2020-04-29 20:06:43 -0400

    Revert "Polish two column audio preview"
    This reverts commit 8a80f401c098affcd53ef015d810becece38d507.

--- lib/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview_types/audio.dart
+++ lib/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview_types/audio.dart
@@ -28,24 +28,6 @@ class AudioPreview extends StatelessWidget {
             ),
             fit: BoxFit.cover,
           ),
-          gradient: LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-            stops: const <double>[0.1, 0.9],
-            colors: audioGradients.isNotEmpty && audioGradients.length == 2
-                ? <Color>[
-                    HexColor.fromHex(audioGradients[0]),
-                    HexColor.fromHex(audioGradients[1]),
-                  ]
-                : <Color>[
-                    Colors.transparent,
-                    Colors.transparent,
-                  ],
-          ),
-          border: Border.all(
-            color: Theme.of(context).dividerColor,
-            width: .5,
-          ),
         ),
         constraints: BoxConstraints(
           minHeight: MediaQuery.of(context).size.height * .24,
@@ -59,20 +41,19 @@ class AudioPreview extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            if (audioTitle.isNotEmpty)
-              Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                child: Text(
-                  audioTitle,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.w700,
-                    color: audioGradients.isNotEmpty || audioPhoto.isNotEmpty
-                        ? Colors.white
-                        : Theme.of(context).primaryColor,
-                  ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                audioTitle,
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                  color: audioGradients.isNotEmpty || audioPhoto.isNotEmpty
+                      ? Colors.white
+                      : Theme.of(context).primaryColor,
                 ),
               ),
+            ),
             Image.asset(
               'assets/images/junto-mobile__waveform.png',
               height: 38,
