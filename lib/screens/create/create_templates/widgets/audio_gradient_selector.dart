e0a6a29626742180c9483d297094249b0105cb78
Eric <37339459+sunyatax@users.noreply.github.com>
2020-04-29 20:06:43 -0400

    Revert "Build gradient cross fade"
    This reverts commit 0065a0301236651286fa496270fcb501c94cca95.

--- lib/screens/create/create_templates/widgets/audio_gradient_selector.dart
+++ lib/screens/create/create_templates/widgets/audio_gradient_selector.dart
@@ -5,89 +5,80 @@ import 'package:junto_beta_mobile/widgets/utils/hex_color.dart';
 import 'package:provider/provider.dart';
 
 class AudioGradientSelector extends StatelessWidget {
-  const AudioGradientSelector({this.toggleGradientSelector});
-
-  final Function toggleGradientSelector;
+  const AudioGradientSelector();
 
   @override
   Widget build(BuildContext context) {
     return Consumer<AudioService>(builder: (context, audio, child) {
-      return Column(children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border(
-              top: BorderSide(
-                color: Theme.of(context).dividerColor,
-                width: .75,
+      return SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border(
+                  top: BorderSide(
+                    color: Theme.of(context).dividerColor,
+                    width: .75,
+                  ),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 15,
+              ),
+              height: 68,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Expanded(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        AudioGradientSelectorItem('8E8098', '307FAA'),
+                        AudioGradientSelectorItem('6F51A8', 'E8B974'),
+                        AudioGradientSelectorItem('2E4F78', '6397C7'),
+                        AudioGradientSelectorItem('719cf4', 'ffc7e4'),
+                        AudioGradientSelectorItem('639acf', '7bdaa5'),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          padding: const EdgeInsets.symmetric(
-            vertical: 15,
-          ),
-          height: 68,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Expanded(
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    AudioGradientSelectorItem('8E8098', '307FAA'),
-                    AudioGradientSelectorItem('6F51A8', 'E8B974'),
-                    AudioGradientSelectorItem('2E4F78', '6397C7'),
-                    AudioGradientSelectorItem('719cf4', 'ffc7e4'),
-                    AudioGradientSelectorItem('639acf', '7bdaa5'),
-                    AudioGradientSelectorItem('8E8098', '307FAA'),
-                    AudioGradientSelectorItem('6F51A8', 'E8B974'),
-                    AudioGradientSelectorItem('2E4F78', '6397C7'),
-                    AudioGradientSelectorItem('719cf4', 'ffc7e4'),
-                    AudioGradientSelectorItem('639acf', '7bdaa5'),
-                  ],
-                ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 15,
               ),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 15,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  toggleGradientSelector();
-                },
-                child: Icon(
-                  CustomIcons.cancel,
-                  size: 20,
-                  color: Theme.of(context).primaryColor,
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    CustomIcons.cancel,
+                    size: 20,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  Text(
+                    'GRADIENT',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      color: Theme.of(context).primaryColor,
+                      letterSpacing: 1.4,
+                    ),
+                  ),
+                  Icon(
+                    Icons.format_color_reset,
+                    size: 20,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ],
               ),
-              Text(
-                'GRADIENT',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).primaryColor,
-                  letterSpacing: 1.4,
-                ),
-              ),
-              Icon(
-                Icons.format_color_reset,
-                size: 20,
-                color: Theme.of(context).primaryColor,
-              ),
-            ],
-          ),
-        ),
-      ]);
+            ),
+          ]));
     });
   }
 }
