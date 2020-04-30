4d728b5816f4461cb8836771446873330b8c203c
Eric <37339459+sunyatax@users.noreply.github.com>
2020-04-29 20:14:27 -0400

    Revert "Revert "Build gradient cross fade""
    This reverts commit 4ab48b3b78928b754e988e10824193740b3de834.

--- lib/screens/create/create_templates/widgets/audio_bottom_tools.dart
+++ lib/screens/create/create_templates/widgets/audio_bottom_tools.dart
@@ -1,84 +1,134 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/screens/create/create_templates/audio_service.dart';
 import 'package:provider/provider.dart';
+import 'audio_gradient_selector.dart';
 
-class AudioBottomTools extends StatelessWidget {
+class AudioBottomTools extends StatefulWidget {
   const AudioBottomTools({this.onPickPressed, this.resetAudioPhotoBackground});
 
   final Function resetAudioPhotoBackground;
   final Function onPickPressed;
   @override
+  State<StatefulWidget> createState() {
+    return AudioBottomToolsState();
+  }
+}
+
+class AudioBottomToolsState extends State<AudioBottomTools> {
+  bool gradientSelectorVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          AnimatedCrossFade(
+            crossFadeState: gradientSelectorVisible
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            duration: const Duration(
+              milliseconds: 200,
+            ),
+            firstChild: AudioBottomToolsDefault(
+              resetAudioPhotoBackground: widget.resetAudioPhotoBackground,
+              onPickPressed: widget.onPickPressed,
+              toggleGradientSelector: _toggleGradientSelector,
+            ),
+            secondChild: AudioGradientSelector(
+              toggleGradientSelector: _toggleGradientSelector,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _toggleGradientSelector() {
+    setState(() {
+      if (gradientSelectorVisible) {
+        gradientSelectorVisible = false;
+      } else {
+        gradientSelectorVisible = true;
+      }
+    });
+  }
+}
+
+class AudioBottomToolsDefault extends StatelessWidget {
+  const AudioBottomToolsDefault({
+    this.onPickPressed,
+    this.resetAudioPhotoBackground,
+    this.toggleGradientSelector,
+  });
+
+  final Function resetAudioPhotoBackground;
+  final Function onPickPressed;
+  final Function toggleGradientSelector;
+  @override
   Widget build(BuildContext context) {
     return Consumer<AudioService>(builder: (context, audio, child) {
-      return SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+      return Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border(
+            top: BorderSide(
+              color: Theme.of(context).dividerColor,
+              width: .75,
+            ),
+          ),
+        ),
+        child: Row(
           children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border(
-                  top: BorderSide(
-                    color: Theme.of(context).dividerColor,
-                    width: .75,
+            Expanded(
+              child: InkWell(
+                onTap: () async {
+                  await audio.resetRecording();
+                  resetAudioPhotoBackground();
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  color: Colors.transparent,
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.keyboard_arrow_left,
+                    color: Theme.of(context).primaryColorLight,
+                    size: 28,
                   ),
                 ),
               ),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: InkWell(
-                      onTap: () async {
-                        await audio.resetRecording();
-                        resetAudioPhotoBackground();
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 25),
-                        color: Colors.transparent,
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.keyboard_arrow_left,
-                          color: Theme.of(context).primaryColorLight,
-                          size: 28,
-                        ),
-                      ),
-                    ),
+            ),
+            Expanded(
+              child: InkWell(
+                onTap: () {
+                  toggleGradientSelector();
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  color: Colors.transparent,
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.invert_colors,
+                    color: Theme.of(context).primaryColorLight,
+                    size: 24,
                   ),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () async {
-                        // show gradient selector
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 25),
-                        color: Colors.transparent,
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.invert_colors,
-                          color: Theme.of(context).primaryColorLight,
-                          size: 24,
-                        ),
-                      ),
-                    ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: InkWell(
+                onTap: () async {
+                  onPickPressed();
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  color: Colors.transparent,
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Theme.of(context).primaryColorLight,
+                    size: 20,
                   ),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () async {
-                        onPickPressed();
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 25),
-                        color: Colors.transparent,
-                        alignment: Alignment.center,
-                        child: Icon(
-                          Icons.camera_alt,
-                          color: Theme.of(context).primaryColorLight,
-                          size: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
