e01e5ccdbe62ceb8f9af0f4adf391fec00bfa795
Neevash Ramdial <mail@neevash.dev>
2020-04-30 17:46:13 -0400

    Created class for storing and mapping hive keys

---
+++ lib/hive_keys.dart
@@ -0,0 +1,15 @@
+/// Various item keys used to store data in Hive boxes
+class HiveKeys {
+  /// Used to access
+  static const kisLoggedIn = 'isLoggedIn';
+  static const kUserFollowPerspectiveId = 'userFollowPerspectiveId';
+  static const kUserId = 'userId';
+  static const kUserData = 'user_data';
+  static const kLayoutView = 'twoColumnView';
+}
+
+/// Class containing the name of the different "boxes" used by the application.
+class HiveBoxes {
+  /// Used to access the application box.
+  static const kAppBox = 'app';
+}
