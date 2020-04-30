b035b5aea6634b41c7370875480d8897cf6e1f5f
Eric <37339459+sunyatax@users.noreply.github.com>
2020-04-29 20:06:43 -0400

    Revert "Clean up audio gradient selector"
    This reverts commit cc993bd8a59c4c82000b9361b3cae0d00b7611e5.

--- lib/screens/create/create_templates/widgets/audio_gradient_selector.dart
+++ lib/screens/create/create_templates/widgets/audio_gradient_selector.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/screens/create/create_templates/audio_service.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
-import 'package:junto_beta_mobile/widgets/utils/hex_color.dart';
 import 'package:provider/provider.dart';
 
 class AudioGradientSelector extends StatelessWidget {
@@ -27,7 +26,7 @@ class AudioGradientSelector extends StatelessWidget {
               padding: const EdgeInsets.symmetric(
                 vertical: 15,
               ),
-              height: 68,
+              height: 60,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -37,11 +36,78 @@ class AudioGradientSelector extends StatelessWidget {
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        AudioGradientSelectorItem('8E8098', '307FAA'),
-                        AudioGradientSelectorItem('6F51A8', 'E8B974'),
-                        AudioGradientSelectorItem('2E4F78', '6397C7'),
-                        AudioGradientSelectorItem('719cf4', 'ffc7e4'),
-                        AudioGradientSelectorItem('639acf', '7bdaa5'),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 30,
+                          margin: EdgeInsets.only(left: 15, right: 15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: LinearGradient(colors: [
+                              Colors.blue,
+                              Colors.orange,
+                            ]),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -80,31 +146,5 @@ class AudioGradientSelector extends StatelessWidget {
             ),
           ]));
     });
-  }
-}
-
-class AudioGradientSelectorItem extends StatelessWidget {
-  const AudioGradientSelectorItem(this.hexOne, this.hexTwo);
-
-  final String hexOne;
-  final String hexTwo;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 38,
-      width: 38,
-      margin: EdgeInsets.only(
-        left: 15,
-        right: 15,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100),
-        gradient: LinearGradient(colors: [
-          HexColor.fromHex(hexOne),
-          HexColor.fromHex(hexTwo),
-        ]),
-      ),
-    );
   }
 }
