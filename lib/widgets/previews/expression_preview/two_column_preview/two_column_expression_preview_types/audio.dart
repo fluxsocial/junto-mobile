ea480ec46e728773fec42b7fbf3651d90c7a44f2
Eric <37339459+sunyatax@users.noreply.github.com>
2020-04-29 20:06:43 -0400

    Revert "Finish building two column audio preview"
    This reverts commit 2d99705b77b2abeb392377b4fd8ffc2dd81d22f3.

--- lib/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview_types/audio.dart
+++ lib/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview_types/audio.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/models/expression.dart';
 import 'package:junto_beta_mobile/widgets/utils/hex_color.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class AudioPreview extends StatelessWidget {
   AudioPreview({@required this.expression});
@@ -12,183 +13,75 @@ class AudioPreview extends StatelessWidget {
     final audioGradients = expression.expressionData.gradient;
     final audioPhoto = expression.expressionData.photo;
 
-    Widget _displayAudioPreview() {
-      if (audioGradients.isEmpty && audioPhoto.isEmpty) {
-        return AudioPreviewDefault(
-          title: audioTitle,
-        );
-      } else if (audioPhoto.isNotEmpty) {
-        return AudioPreviewWithPhoto(
-          title: audioTitle,
-          photo: audioPhoto,
-        );
-      } else if (audioGradients.isNotEmpty && audioGradients.length == 2) {
-        return AudioPreviewWithGradients(
-          gradients: audioGradients,
-          title: audioTitle,
-        );
-      } else {
-        return AudioPreviewDefault(title: audioTitle);
-      }
-    }
-
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Container(
-        child: _displayAudioPreview(),
-      ),
-    );
-  }
-}
-
-class AudioPreviewWaveform extends StatelessWidget {
-  AudioPreviewWaveform({this.color});
-  final Color color;
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(
-      'assets/images/junto-mobile__waveform.png',
-      height: 38,
-      color: color,
-    );
-  }
-}
-
-class AudioPreviewTitle extends StatelessWidget {
-  AudioPreviewTitle({this.title, this.color});
-  final String title;
-  final Color color;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 15),
-      child: Text(
-        title,
-        maxLines: 3,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-          fontSize: 17,
-          fontWeight: FontWeight.w700,
-          color: color,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    );
-  }
-}
-
-class AudioPreviewDefault extends StatelessWidget {
-  AudioPreviewDefault({this.title});
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 25.0,
-        vertical: 50.0,
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).dividerColor,
-          width: .5,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          if (title.isNotEmpty)
-            AudioPreviewTitle(
-              title: title,
-              color: Theme.of(context).primaryColor,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+              Colors.black38,
+              BlendMode.srcOver,
             ),
-          AudioPreviewWaveform(
-            color: Theme.of(context).primaryColor,
+            image: NetworkImage(
+              audioPhoto,
+            ),
+            fit: BoxFit.cover,
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class AudioPreviewWithGradients extends StatelessWidget {
-  AudioPreviewWithGradients({this.title, this.gradients});
-  final String title;
-  final List<String> gradients;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 25.0,
-        vertical: 50.0,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
+          gradient: LinearGradient(
             begin: Alignment.bottomLeft,
             end: Alignment.topRight,
-            stops: const <double>[
-              0.1,
-              0.9
-            ],
-            colors: <Color>[
-              HexColor.fromHex(gradients[0]),
-              HexColor.fromHex(gradients[1]),
-            ]),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          if (title.isNotEmpty)
-            AudioPreviewTitle(
-              title: title,
-              color: Colors.white,
-            ),
-          AudioPreviewWaveform(
-            color: Colors.white,
+            stops: const <double>[0.1, 0.9],
+            colors: audioGradients.isNotEmpty && audioGradients.length == 2
+                ? <Color>[
+                    HexColor.fromHex(audioGradients[0]),
+                    HexColor.fromHex(audioGradients[1]),
+                  ]
+                : <Color>[
+                    Colors.transparent,
+                    Colors.transparent,
+                  ],
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class AudioPreviewWithPhoto extends StatelessWidget {
-  AudioPreviewWithPhoto({this.title, this.photo});
-  final String title;
-  final String photo;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 25.0,
-        vertical: 50.0,
-      ),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          colorFilter: ColorFilter.mode(
-            Colors.black38,
-            BlendMode.srcOver,
+          border: Border.all(
+            color: Theme.of(context).dividerColor,
+            width: .5,
           ),
-          image: NetworkImage(
-            photo,
-          ),
-          fit: BoxFit.cover,
         ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          if (title.isNotEmpty)
-            AudioPreviewTitle(
-              title: title,
-              color: Colors.white,
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height * .24,
+        ),
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 25.0,
+          vertical: 50.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (audioTitle.isNotEmpty)
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  audioTitle,
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w700,
+                    color: audioGradients.isNotEmpty || audioPhoto.isNotEmpty
+                        ? Colors.white
+                        : Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            Image.asset(
+              'assets/images/junto-mobile__waveform.png',
+              height: 38,
+              color: audioGradients.isNotEmpty || audioPhoto.isNotEmpty
+                  ? Colors.white
+                  : Theme.of(context).primaryColor,
             ),
-          AudioPreviewWaveform(
-            color: Colors.white,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
