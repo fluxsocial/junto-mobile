22636f24952ba59ab489b2e627b667716a6b6402
Eric Yang <eric@junto.love>
2020-05-05 22:33:33 -0500

    Finish audio caption display

--- lib/screens/expression_open/expressions/audio_open.dart
+++ lib/screens/expression_open/expressions/audio_open.dart
@@ -19,7 +19,6 @@ class AudioOpen extends StatelessWidget {
     final audioCaption = expression.expressionData.caption;
 
     Widget _displayAudioOpen() {
-      print(audioCaption);
       if (audioGradients.isEmpty && audioPhoto.isEmpty) {
         return AudioOpenDefault(
           title: audioTitle,
@@ -29,11 +28,13 @@ class AudioOpen extends StatelessWidget {
         return AudioOpenWithPhoto(
           title: audioTitle,
           photo: audioPhoto,
+          caption: audioCaption,
         );
       } else if (audioGradients.isNotEmpty && audioGradients.length == 2) {
         return AudioOpenWithGradients(
           gradients: audioGradients,
           title: audioTitle,
+          caption: audioCaption,
         );
       } else {
         return AudioOpenDefault(
@@ -76,6 +77,27 @@ class AudioOpenTitle extends StatelessWidget {
   }
 }
 
+class AudioOpenCaption extends StatelessWidget {
+  const AudioOpenCaption({this.caption});
+  final String caption;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 10,
+      ),
+      child: Text(
+        caption,
+        style: TextStyle(
+          fontSize: 17,
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+}
+
 class AudioOpenDefault extends StatelessWidget {
   AudioOpenDefault({
     this.title,
@@ -85,142 +107,146 @@ class AudioOpenDefault extends StatelessWidget {
   final String caption;
   @override
   Widget build(BuildContext context) {
-    print(caption);
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      constraints: BoxConstraints(
-        minHeight: MediaQuery.of(context).size.height * .17,
-      ),
-      padding: const EdgeInsets.symmetric(vertical: 25),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).dividerColor,
-          width: .5,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: title.isNotEmpty
-            ? MainAxisAlignment.spaceBetween
-            : MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          if (title.isNotEmpty)
-            AudioOpenTitle(
-              title: title,
-              color: Theme.of(context).primaryColor,
-            ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: AudioPlaybackRow(hasBackground: false),
-          ),
-          Container(
-            child: Text(
-              caption,
-              style: TextStyle(
-                fontSize: 17,
-                color: Theme.of(context).primaryColor,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Column(
+            mainAxisAlignment: title.isNotEmpty
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              if (title.isNotEmpty)
+                AudioOpenTitle(
+                  title: title,
+                  color: Theme.of(context).primaryColor,
+                ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: AudioPlaybackRow(hasBackground: false),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+        AudioOpenCaption(caption: caption),
+      ],
     );
   }
 }
 
 class AudioOpenWithGradients extends StatelessWidget {
-  AudioOpenWithGradients({this.title, this.gradients});
+  AudioOpenWithGradients({
+    this.title,
+    this.caption,
+    this.gradients,
+  });
   final String title;
+  final String caption;
   final List<String> gradients;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      constraints: BoxConstraints(
-        minHeight: MediaQuery.of(context).size.height * .17,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-            stops: const <double>[
-              0.1,
-              0.9
-            ],
-            colors: <Color>[
-              HexColor.fromHex(gradients[0]),
-              HexColor.fromHex(gradients[1]),
-            ]),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          title.isNotEmpty
-              ? AudioOpenTitle(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height * .17,
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                stops: const <double>[
+                  0.1,
+                  0.9
+                ],
+                colors: <Color>[
+                  HexColor.fromHex(gradients[0]),
+                  HexColor.fromHex(gradients[1]),
+                ]),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              if (title.isNotEmpty)
+                AudioOpenTitle(
                   title: title,
                   color: Colors.white,
-                )
-              : const SizedBox(),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-            ),
-            child: AudioPlaybackRow(
-              hasBackground: true,
-            ),
+                ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: AudioPlaybackRow(
+                  hasBackground: true,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        AudioOpenCaption(caption: caption),
+      ],
     );
   }
 }
 
 class AudioOpenWithPhoto extends StatelessWidget {
-  AudioOpenWithPhoto({this.title, this.photo});
+  AudioOpenWithPhoto({this.title, this.caption, this.photo});
   final String title;
+  final String caption;
   final String photo;
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          foregroundDecoration: BoxDecoration(color: Colors.black38),
-          width: MediaQuery.of(context).size.width,
-          child: CachedNetworkImage(
-            imageUrl: photo,
-            placeholder: (BuildContext context, String _) {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width * 2 / 3,
-                color: Theme.of(context).dividerColor,
-              );
-            },
-          ),
+        Stack(
+          children: <Widget>[
+            Container(
+              foregroundDecoration: BoxDecoration(color: Colors.black38),
+              width: MediaQuery.of(context).size.width,
+              child: CachedNetworkImage(
+                imageUrl: photo,
+                placeholder: (BuildContext context, String _) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width * 2 / 3,
+                    color: Theme.of(context).dividerColor,
+                  );
+                },
+              ),
+            ),
+            if (title.isNotEmpty)
+              Positioned(
+                top: 25,
+                left: 0,
+                right: 0,
+                child: AudioOpenTitle(
+                  title: title,
+                  color: Colors.white,
+                ),
+              ),
+            Positioned(
+              bottom: 25,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: AudioPlaybackRow(
+                  hasBackground: true,
+                ),
+              ),
+            )
+          ],
         ),
-        if (title.isNotEmpty)
-          Positioned(
-            top: 25,
-            left: 0,
-            right: 0,
-            child: AudioOpenTitle(
-              title: title,
-              color: Colors.white,
-            ),
-          ),
-        Positioned(
-          bottom: 25,
-          left: 0,
-          right: 0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10,
-            ),
-            child: AudioPlaybackRow(
-              hasBackground: true,
-            ),
-          ),
-        )
+        AudioOpenCaption(caption: caption),
       ],
     );
   }
