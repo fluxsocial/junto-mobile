3b449d930f6a8a0d8fdc4c52b0a851f45ac28698
Eric Yang <eric@junto.love>
2020-05-07 15:29:12 -0500

    Account for light shortform in expression preview/open

--- lib/screens/expression_open/expressions/shortform_open.dart
+++ lib/screens/expression_open/expressions/shortform_open.dart
@@ -9,6 +9,8 @@ class ShortformOpen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String _hexOne = expression.expressionData.background[0];
+    final String _hexTwo = expression.expressionData.background[1];
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -16,8 +18,8 @@ class ShortformOpen extends StatelessWidget {
           end: Alignment.topRight,
           stops: const <double>[0.1, 0.9],
           colors: <Color>[
-            HexColor.fromHex(expression.expressionData.background[0]),
-            HexColor.fromHex(expression.expressionData.background[1]),
+            HexColor.fromHex(_hexOne),
+            HexColor.fromHex(_hexTwo),
           ],
         ),
       ),
@@ -32,10 +34,12 @@ class ShortformOpen extends StatelessWidget {
       child: SelectableText(
         expression.expressionData.body,
         textAlign: TextAlign.center,
-        style: const TextStyle(
+        style: TextStyle(
           fontSize: 20.0,
           fontWeight: FontWeight.w700,
-          color: Colors.white,
+          color: _hexOne.contains('fff') || _hexTwo.contains('fff')
+              ? Color(0xff333333)
+              : Colors.white,
         ),
       ),
     );
