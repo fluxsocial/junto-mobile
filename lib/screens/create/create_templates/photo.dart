d2081cdbbf93505f384aa1bb11d2d9f5e53c147d
Eric Yang <eric@junto.love>
2020-05-08 12:18:19 -0500

    Add newline keyboard for photo captions

--- lib/screens/create/create_templates/photo.dart
+++ lib/screens/create/create_templates/photo.dart
@@ -261,15 +261,17 @@ class CreatePhotoState extends State<CreatePhoto> {
                 ),
                 child: TextField(
                   controller: _captionController,
-                  textInputAction: TextInputAction.done,
+                  textInputAction: TextInputAction.newline,
                   decoration: const InputDecoration(
-                    hintText: 'write a caption..',
+                    hintText: 'Write a caption...',
                     border: InputBorder.none,
                   ),
                   cursorColor: Theme.of(context).primaryColor,
                   cursorWidth: 1,
                   maxLines: null,
-                  style: Theme.of(context).textTheme.caption,
+                  style: Theme.of(context).textTheme.caption.copyWith(
+                        fontSize: 17,
+                      ),
                   keyboardAppearance: Theme.of(context).brightness,
                 ),
               )
