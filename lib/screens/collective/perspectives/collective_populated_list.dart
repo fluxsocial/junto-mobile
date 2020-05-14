08fef04e3465e084cd359bd7aebcc45e58ae621a
Neevash Ramdial <mail@neevash.dev>
2020-05-13 20:56:23 -0400

    add staggered view builder

--- lib/screens/collective/perspectives/collective_populated_list.dart
+++ lib/screens/collective/perspectives/collective_populated_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/models/expression.dart';
 import 'package:junto_beta_mobile/screens/collective/bloc/collective_bloc.dart';
-import 'package:junto_beta_mobile/widgets/custom_feeds/custom_listview.dart';
 import 'package:junto_beta_mobile/widgets/custom_feeds/single_listview.dart';
 import 'package:junto_beta_mobile/widgets/fetch_more.dart';
+import 'package:junto_beta_mobile/widgets/previews/expression_preview/two_column_preview/two_column_expression_preview.dart';
 import 'package:provider/provider.dart';
 
 class CollectivePopulatedList extends StatelessWidget {
@@ -29,12 +31,9 @@ class CollectivePopulatedList extends StatelessWidget {
               AnimatedSwitcher(
                 duration: kThemeChangeDuration,
                 child: snapshot.twoColumnView
-                    ? TwoColumnSliverListView(
-                        key: ValueKey<String>('two-column'),
-                        data: state.results,
-                      )
+                    ? _TwoColumnList(data: state.results)
                     : SingleColumnSliverListView(
-                        key: ValueKey<String>('singlxe-column'),
+                        key: ValueKey<String>('single-column'),
                         data: state.results,
                         privacyLayer: 'Public',
                       ),
@@ -50,5 +49,34 @@ class CollectivePopulatedList extends StatelessWidget {
         );
       });
     }
+  }
+}
+
+class _TwoColumnList extends StatelessWidget {
+  const _TwoColumnList({Key key, @required this.data}) : super(key: key);
+  final List<ExpressionResponse> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12),
+      height: MediaQuery.of(context).size.height,
+      child: StaggeredGridView.countBuilder(
+        key: ValueKey<String>('two-column'),
+        crossAxisCount: 4,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 4.0,
+        staggeredTileBuilder: (index) {
+          return StaggeredTile.fit(2);
+        },
+        itemBuilder: (context, index) {
+          return TwoColumnExpressionPreview(
+            key: ValueKey<String>(data[index].address),
+            expression: data[index],
+          );
+        },
+        itemCount: data.length,
+      ),
+    );
   }
 }
