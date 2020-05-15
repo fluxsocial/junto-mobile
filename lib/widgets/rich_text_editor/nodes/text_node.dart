da4a0540fd92e9d28100e5190df03979940cb916
Dominik Roszkowski <dominik@roszkowski.dev>
2020-05-15 23:21:10 +0200

    Implement basic RichTextController

--- lib/widgets/rich_text_editor/nodes/text_node.dart
+++ lib/widgets/rich_text_editor/nodes/text_node.dart
@@ -34,7 +34,7 @@ class TextNode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: node.controller,
+      controller: node.text,
       focusNode: node.focus,
       decoration: null,
       style: _getTextStyle(),
