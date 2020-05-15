09555518fe69a8eb65d5d50318d0a325ef2f54f5
Eric Yang <eric@junto.love>
2020-05-14 21:10:35 -0500

    Fix create perspective error validation

--- lib/widgets/avatars/member_avatar.dart
+++ lib/widgets/avatars/member_avatar.dart
@@ -1,6 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:junto_beta_mobile/utils/cache_manager.dart';
 import 'package:junto_beta_mobile/widgets/avatars/member_avatar_placeholder.dart';
 import 'package:junto_beta_mobile/widgets/image_wrapper.dart';
 
@@ -16,16 +14,15 @@ class MemberAvatar extends StatelessWidget {
         ? Container(
             child: ClipOval(
               child: ImageWrapper(
-                imageUrl: profilePicture[0],
-                height: diameter,
-                width: diameter,
-                fit: BoxFit.cover,
-                placeholder: (BuildContext context, String _) {
-                  return MemberAvatarPlaceholder(
-                    diameter: diameter,
-                  );
-                },
-              ),
+                  imageUrl: profilePicture[0],
+                  height: diameter,
+                  width: diameter,
+                  fit: BoxFit.cover,
+                  placeholder: (BuildContext context, String _) {
+                    return MemberAvatarPlaceholder(
+                      diameter: diameter,
+                    );
+                  }),
             ),
           )
         : MemberAvatarPlaceholder(
