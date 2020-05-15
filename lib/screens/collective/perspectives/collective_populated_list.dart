5ea4bf4e33df9eab93f5f4f76162a1823c8d26a6
Neevash Ramdial <mail@neevash.dev>
2020-05-15 17:04:28 -0400

    minor clean up

--- lib/screens/collective/perspectives/collective_populated_list.dart
+++ lib/screens/collective/perspectives/collective_populated_list.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:junto_beta_mobile/backend/backend.dart';
 import 'package:junto_beta_mobile/screens/collective/bloc/collective_bloc.dart';
 import 'package:junto_beta_mobile/widgets/custom_feeds/custom_listview.dart';
 import 'package:junto_beta_mobile/widgets/custom_feeds/single_listview.dart';
-import 'package:junto_beta_mobile/widgets/fetch_more.dart';
 import 'package:provider/provider.dart';
 
 class CollectivePopulatedList extends StatelessWidget {
@@ -22,30 +20,21 @@ class CollectivePopulatedList extends StatelessWidget {
         child: const SizedBox(),
       );
     } else {
-      return Consumer<UserDataProvider>(builder: (context, snapshot, _) {
-        return SliverList(
-          delegate: SliverChildListDelegate(
-            <Widget>[
-              AnimatedSwitcher(
-                duration: kThemeChangeDuration,
-                child: snapshot.twoColumnView
-                    ? TwoColumnList(data: state.results)
-                    : SingleColumnSliverListView(
-                        key: ValueKey<String>('single-column'),
-                        data: state.results,
-                        privacyLayer: 'Public',
-                      ),
-              ),
-              if (state.availableMore == true && state.loadingMore != true)
-                FetchMoreButton(
-                  onPressed: () => context.bloc<CollectiveBloc>().add(
-                        FetchMoreCollective(),
-                      ),
-                ),
-            ],
-          ),
-        );
-      });
+      return Consumer<UserDataProvider>(
+        builder: (context, snapshot, _) {
+          if (snapshot.twoColumnView) {
+            return TwoColumnList(
+              data: state.results,
+              useSliver: true,
+            );
+          }
+          return SingleColumnSliverListView(
+            key: ValueKey<String>('single-column'),
+            data: state.results,
+            privacyLayer: 'Public',
+          );
+        },
+      );
     }
   }
 }
