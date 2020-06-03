38b2cbcccc55dfc55588164209d3b915b2ec2eac
Eric Yang <eric@junto.love>
2020-06-03 17:33:30 -0400

    Polish create perspective button and text fields

---
+++ lib/screens/collective/collective_actions/create_perspective_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CreatePerspectiveButton extends StatelessWidget {
+  const CreatePerspectiveButton({
+    this.cta,
+    this.title,
+  });
+
+  final Function cta;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: cta,
+      child: Container(
+        padding: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+          top: 5,
+          bottom: 5,
+        ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
