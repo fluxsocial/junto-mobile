2e7392849c2c0f3b5e0eb5a51809c045c6d9bc4e
Eric Yang <eric@junto.love>
2020-06-03 14:46:01 -0400

    Remove slideable

--- lib/screens/collective/perspectives/perspective_item.dart
+++ lib/screens/collective/perspectives/perspective_item.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
 import 'package:junto_beta_mobile/models/models.dart';
-import 'package:junto_beta_mobile/screens/collective/collective_actions/edit_perspective.dart';
-import 'package:junto_beta_mobile/screens/collective/perspectives/bloc/perspectives_bloc.dart';
-import 'package:junto_beta_mobile/widgets/dialogs/confirm_dialog.dart';
 import 'package:junto_beta_mobile/screens/collective/perspectives/about_perspective.dart';
 import 'package:junto_beta_mobile/app/themes_provider.dart';
 import 'package:provider/provider.dart';
@@ -46,103 +41,62 @@ class PerspectiveItem extends StatelessWidget {
     return Consumer<JuntoThemesProvider>(builder: (context, theme, child) {
       return GestureDetector(
         onTap: onTap,
-        child: Slidable(
-          enabled: !perspective.isDefault,
-          actionPane: const SlidableDrawerActionPane(),
-          actionExtentRatio: 0.2,
-          secondaryActions: <Widget>[
-            IconSlideAction(
-              color: Theme.of(context).dividerColor,
-              iconWidget: Icon(Icons.edit,
-                  size: 15, color: Theme.of(context).primaryColor),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  CupertinoPageRoute<dynamic>(
-                    builder: (BuildContext context) => EditPerspective(
-                      perspective: perspective,
-                    ),
-                  ),
-                );
-              },
-            ),
-            IconSlideAction(
-              color: Colors.red,
-              iconWidget: Icon(Icons.delete, size: 15, color: Colors.white),
-              onTap: () async {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => ConfirmDialog(
-                    confirmationText:
-                        'Are you sure you want to delete this perspective?',
-                    confirm: () {
-                      context
-                          .bloc<PerspectivesBloc>()
-                          .add(RemovePerspective(perspective));
-                    },
-                  ),
-                );
-              },
-            ),
-          ],
-          key: Key(perspective.address),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              border: Border(
-                bottom: BorderSide(
-                  color: Theme.of(context).dividerColor,
-                  width: .75,
-                ),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border(
+              bottom: BorderSide(
+                color: Theme.of(context).dividerColor,
+                width: .75,
               ),
             ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Image.asset(
-                      _buildIcon(theme.themeName),
-                      height: 20,
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      perspective.name,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  ],
-                ),
-                GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      builder: (BuildContext context) => AboutPerspective(
-                        incomingPerspective: perspective,
-                      ),
-                    );
-                  },
-                  child: Container(
-                    width: 38,
-                    alignment: Alignment.centerRight,
-                    color: Colors.transparent,
-                    child: Icon(
-                      CustomIcons.morevertical,
-                      size: 17,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.asset(
+                    _buildIcon(theme.themeName),
+                    height: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    perspective.name,
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
                       color: Theme.of(context).primaryColor,
                     ),
                   ),
+                ],
+              ),
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    builder: (BuildContext context) => AboutPerspective(
+                      incomingPerspective: perspective,
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 38,
+                  alignment: Alignment.centerRight,
+                  color: Colors.transparent,
+                  child: Icon(
+                    CustomIcons.morevertical,
+                    size: 17,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
