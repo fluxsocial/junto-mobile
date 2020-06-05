701d42501ca66816543a10ec12c69fda43248bcc
Eric Yang <eric@junto.love>
2020-06-05 14:20:34 -0400

    Account for dark-night logo and perspective

--- lib/screens/collective/perspectives/perspective_item.dart
+++ lib/screens/collective/perspectives/perspective_item.dart
@@ -31,6 +31,16 @@ class PerspectiveItem extends StatelessWidget {
       return 'assets/images/junto-mobile__perspective--aqueous.png';
     } else if (theme == 'royal' || theme == 'royal-night') {
       return 'assets/images/junto-mobile__perspective--purpgold.png';
+    } else if (theme == 'fire' || theme == 'fire-night') {
+      return 'assets/images/junto-mobile__perspective--fire.png';
+    } else if (theme == 'forest' || theme == 'forest-night') {
+      return 'assets/images/junto-mobile__perspective--forest.png';
+    } else if (theme == 'sand' || theme == 'sand-night') {
+      return 'assets/images/junto-mobile__perspective--sand.png';
+    } else if (theme == 'dark') {
+      return 'assets/images/junto-mobile__perspective--dark.png';
+    } else if (theme == 'dark-night') {
+      return 'assets/images/junto-mobile__perspective--white.png';
     } else {
       return 'assets/images/junto-mobile__perspective--rainbow.png';
     }
