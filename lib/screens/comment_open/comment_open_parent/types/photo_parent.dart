f813faa910eed16392ea1a1d342c33d10f0f5e2f
Eric Yang <eric@junto.love>
2020-06-08 16:32:27 -0400

    Build dynamic preview for comment parent

---
+++ lib/screens/comment_open/comment_open_parent/types/photo_parent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PhotoParent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/screens/comment_open/comment_open_parent/types/shortform_parent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ShortformParent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
