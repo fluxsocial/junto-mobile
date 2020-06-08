d98b1617e6f44cc498e60f29de3891e8e17bda3c
Eric Yang <eric@junto.love>
2020-06-08 16:49:42 -0400

    Build photo comment parent preview

--- lib/screens/comment_open/comment_open_parent/types/photo_parent.dart
+++ lib/screens/comment_open/comment_open_parent/types/photo_parent.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/widgets/image_wrapper.dart';
+import 'package:junto_beta_mobile/models/models.dart';
 
 class PhotoParent extends StatelessWidget {
+  const PhotoParent({this.expression});
+  final ExpressionResponse expression;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: MediaQuery.of(context).size.width / 3 * 2,
+        width: MediaQuery.of(context).size.width,
+        child: ImageWrapper(
+          imageUrl: expression.expressionData.image,
+          placeholder: (BuildContext context, String _) {
+            return Container(
+              color: Theme.of(context).dividerColor,
+              height: MediaQuery.of(context).size.width / 3 * 2,
+              width: MediaQuery.of(context).size.width,
+            );
+          },
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
