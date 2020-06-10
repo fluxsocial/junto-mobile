5d59b1b39f11e05c08368a1ca622937adc99077e
Neevash Ramdial <mail@neevash.dev>
2020-06-09 21:33:32 -0400

    check created at before tutorial display

--- lib/backend/repositories/onboarding_repo.dart
+++ lib/backend/repositories/onboarding_repo.dart
@@ -1,8 +1,10 @@
 import 'package:hive/hive.dart';
+import 'package:junto_beta_mobile/backend/backend.dart';
 import 'package:junto_beta_mobile/hive_keys.dart';
 
 class OnBoardingRepo {
-  OnBoardingRepo() {
+  OnBoardingRepo(UserDataProvider repo) {
+    _repo = repo;
     _loadTutorialState();
   }
   bool _showLotusTutorial = false;
@@ -12,6 +14,7 @@ class OnBoardingRepo {
   bool _showDenTutorial = false;
   bool _showCreateTutorial = false;
   Box _appBox;
+  UserDataProvider _repo;
 
   bool get showLotusTutorial => _showLotusTutorial;
   bool get showPerspectiveTutorial => _showPerspectiveTutorial;
@@ -21,47 +24,49 @@ class OnBoardingRepo {
   bool get showCreateTutorial => _showCreateTutorial;
 
   Future<void> _loadTutorialState() async {
-    return;
-//    _appBox = await Hive.box(HiveBoxes.kAppBox);
-//    _showLotusTutorial = await _appBox.get(HiveKeys.kShowLotusTutorial) ?? true;
-//    _showCollectiveTutorial =
-//        await _appBox.get(HiveKeys.kShowCollectiveTutorial) ?? true;
-//    _showPackTutorial = await _appBox.get(HiveKeys.kShowPackTutorial) ?? true;
-//    _showDenTutorial = await _appBox.get(HiveKeys.kShowDenTutorial) ?? true;
-//    _showPerspectiveTutorial =
-//        await _appBox.get(HiveKeys.kShowPerspectiveTutorial) ?? true;
-//    _showCreateTutorial =
-//        await _appBox.get(HiveKeys.kShowCreateTutorial) ?? true;
+    final time = DateTime.now();
+    if (_repo.userProfile != null &&
+        _repo.userProfile.pack.createdAt.difference(time).inMinutes < 2) {
+      _appBox = await Hive.box(HiveBoxes.kAppBox);
+      _showLotusTutorial =
+          await _appBox.get(HiveKeys.kShowLotusTutorial) ?? true;
+      _showCollectiveTutorial =
+          await _appBox.get(HiveKeys.kShowCollectiveTutorial) ?? true;
+      _showPackTutorial = await _appBox.get(HiveKeys.kShowPackTutorial) ?? true;
+      _showDenTutorial = await _appBox.get(HiveKeys.kShowDenTutorial) ?? true;
+      _showPerspectiveTutorial =
+          await _appBox.get(HiveKeys.kShowPerspectiveTutorial) ?? true;
+      _showCreateTutorial =
+          await _appBox.get(HiveKeys.kShowCreateTutorial) ?? true;
+    }
   }
 
   Future<void> setViewed(String key, bool value) async {
-    //TODO: Re-enable once api is updated
-    return;
-//    switch (key) {
-//      case HiveKeys.kShowLotusTutorial:
-//        _showLotusTutorial = false;
-//        await _appBox.put(HiveKeys.kShowLotusTutorial, value);
-//        return;
-//      case HiveKeys.kShowCollectiveTutorial:
-//        _showCollectiveTutorial = false;
-//        await _appBox.put(HiveKeys.kShowCollectiveTutorial, value);
-//        return;
-//      case HiveKeys.kShowPackTutorial:
-//        _showPackTutorial = false;
-//        await _appBox.put(HiveKeys.kShowPackTutorial, value);
-//        return;
-//      case HiveKeys.kShowDenTutorial:
-//        _showDenTutorial = false;
-//        await _appBox.put(HiveKeys.kShowDenTutorial, value);
-//        return;
-//      case HiveKeys.kShowPerspectiveTutorial:
-//        _showPerspectiveTutorial = false;
-//        await _appBox.put(HiveKeys.kShowPerspectiveTutorial, value);
-//        return;
-//      case HiveKeys.kShowCreateTutorial:
-//        _showCreateTutorial = false;
-//        await _appBox.put(HiveKeys.kShowCreateTutorial, value);
-//        return;
-//    }
+    switch (key) {
+      case HiveKeys.kShowLotusTutorial:
+        _showLotusTutorial = false;
+        await _appBox.put(HiveKeys.kShowLotusTutorial, value);
+        return;
+      case HiveKeys.kShowCollectiveTutorial:
+        _showCollectiveTutorial = false;
+        await _appBox.put(HiveKeys.kShowCollectiveTutorial, value);
+        return;
+      case HiveKeys.kShowPackTutorial:
+        _showPackTutorial = false;
+        await _appBox.put(HiveKeys.kShowPackTutorial, value);
+        return;
+      case HiveKeys.kShowDenTutorial:
+        _showDenTutorial = false;
+        await _appBox.put(HiveKeys.kShowDenTutorial, value);
+        return;
+      case HiveKeys.kShowPerspectiveTutorial:
+        _showPerspectiveTutorial = false;
+        await _appBox.put(HiveKeys.kShowPerspectiveTutorial, value);
+        return;
+      case HiveKeys.kShowCreateTutorial:
+        _showCreateTutorial = false;
+        await _appBox.put(HiveKeys.kShowCreateTutorial, value);
+        return;
+    }
   }
 }
