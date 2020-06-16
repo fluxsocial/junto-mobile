732b7da70eb8b96d51d349cfdc81a28fe653ae3f
Eric Yang <eric@junto.love>
2020-06-16 16:10:44 -0400

    Create junto white sand palette and implement across sign up flow

--- lib/screens/welcome/widgets/sign_in_back_nav.dart
+++ lib/screens/welcome/widgets/sign_in_back_nav.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
+import 'package:junto_beta_mobile/app/themes_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:junto_beta_mobile/app/palette.dart';
 
 class SignInBackNav extends StatelessWidget {
   SignInBackNav({this.signInController});
@@ -7,35 +10,38 @@ class SignInBackNav extends StatelessWidget {
   final PageController signInController;
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      backgroundColor: Colors.transparent,
-      title: Row(
-        children: <Widget>[
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              signInController.previousPage(
-                curve: Curves.easeIn,
-                duration: const Duration(milliseconds: 300),
-              );
-              if (FocusScope.of(context).hasFocus) {
-                FocusScope.of(context).unfocus();
-              }
-            },
-            child: Container(
-              width: 38,
-              height: 38,
-              alignment: Alignment.centerLeft,
-              child: Icon(
-                CustomIcons.back,
-                color: Colors.white70,
-                size: 20,
+    return Consumer<JuntoThemesProvider>(builder: (context, theme, child) {
+      return AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        title: Row(
+          children: <Widget>[
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                signInController.previousPage(
+                  curve: Curves.easeIn,
+                  duration: const Duration(milliseconds: 300),
+                );
+                if (FocusScope.of(context).hasFocus) {
+                  FocusScope.of(context).unfocus();
+                }
+              },
+              child: Container(
+                width: 38,
+                height: 38,
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  CustomIcons.back,
+                  color:
+                      JuntoPalette().juntoWhite(theme: theme).withOpacity(.70),
+                  size: 20,
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
