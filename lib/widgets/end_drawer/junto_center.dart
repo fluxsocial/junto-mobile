1431439f2de02454eccbee9de2f08542a94e63d7
Eric Yang <eric@junto.love>
2020-06-23 17:48:31 -0400

    Clean up feedback feed

--- lib/widgets/end_drawer/junto_center.dart
+++ lib/widgets/end_drawer/junto_center.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:junto_beta_mobile/widgets/end_drawer/junto_center/junto_center_appbar.dart';
 import 'package:junto_beta_mobile/widgets/end_drawer/junto_center/junto_center_fab.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/junto_center/junto_center_feedback.dart';
 import 'package:feature_discovery/feature_discovery.dart';
-import 'package:junto_beta_mobile/models/models.dart';
-import 'package:junto_beta_mobile/backend/backend.dart';
-import 'package:junto_beta_mobile/widgets/previews/expression_preview/single_column_preview/single_column_expression_preview.dart';
-import 'package:provider/provider.dart';
 
 class JuntoCommunityCenter extends StatelessWidget {
   final List<String> _tabs = ['FEEDBACK', 'UPDATES'];
@@ -39,49 +36,7 @@ class JuntoCommunityCenter extends StatelessWidget {
               },
               body: TabBarView(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      FutureBuilder<QueryResults<ExpressionResponse>>(
-                        future:
-                            Provider.of<ExpressionRepo>(context, listen: false)
-                                .getCollectiveExpressions(
-                          {
-                            'context': '48b97134-1a4d-deb0-b27c-9bcdfc33f386',
-                            'context_type': 'Group',
-                            'pagination_position': '0',
-                          },
-                        ),
-                        builder: (BuildContext context,
-                            AsyncSnapshot<QueryResults<ExpressionResponse>>
-                                snapshot) {
-                          if (snapshot.hasData) {
-                            print(snapshot.data);
-                            return Expanded(
-                              child: ListView(
-                                padding: const EdgeInsets.all(0),
-                                children: snapshot.data.results
-                                    .map(
-                                      (dynamic expression) =>
-                                          SingleColumnExpressionPreview(
-                                        expression: expression,
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            );
-                          } else if (snapshot.hasError) {
-                            print(snapshot.error);
-                            return Center(
-                              child: Text('Hmm, something went wrong'),
-                            );
-                          }
-                          return Center(
-                            child: Text('Hi'),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                  JuntoCommunityCenterFeedback(),
                   SizedBox(),
                 ],
               ),
---
+++ lib/widgets/end_drawer/junto_center/junto_center_feedback.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:junto_beta_mobile/models/models.dart';
+import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/widgets/previews/expression_preview/single_column_preview/single_column_expression_preview.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer_relationships/error_widget.dart';
+import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
+import 'package:provider/provider.dart';
+
+class JuntoCommunityCenterFeedback extends StatelessWidget {
+  final String communityCenterAddress = '48b97134-1a4d-deb0-b27c-9bcdfc33f386';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FutureBuilder<QueryResults<ExpressionResponse>>(
+          future: Provider.of<ExpressionRepo>(context, listen: false)
+              .getCollectiveExpressions(
+            {
+              'context': '48b97134-1a4d-deb0-b27c-9bcdfc33f386',
+              'context_type': 'Group',
+              'pagination_position': '0',
+            },
+          ),
+          builder: (BuildContext context,
+              AsyncSnapshot<QueryResults<ExpressionResponse>> snapshot) {
+            if (snapshot.hasData) {
+              print(snapshot.data);
+              return Expanded(
+                child: ListView(
+                  padding: const EdgeInsets.all(0),
+                  children: snapshot.data.results
+                      .map(
+                        (dynamic expression) => SingleColumnExpressionPreview(
+                          expression: expression,
+                        ),
+                      )
+                      .toList(),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return JuntoErrorWidget(
+                errorMessage: 'Hmm, something went wrong',
+              );
+            }
+            return Center(
+              child: JuntoProgressIndicator(),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
