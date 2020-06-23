8dfe04b11d9ed834a097cdf831bde6dfa1094dbf
Eric Yang <eric@junto.love>
2020-06-23 13:16:34 -0400

    Build community center appbar + template

--- lib/widgets/end_drawer/junto_center.dart
+++ lib/widgets/end_drawer/junto_center.dart
@@ -1,66 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:junto_beta_mobile/app/custom_icons.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/junto_center/junto_center_appbar.dart';
+import 'package:feature_discovery/feature_discovery.dart';
 
 class JuntoCommunityCenter extends StatelessWidget {
+  final List<String> _tabs = ['UPDATES', 'FEEDBACK'];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(45),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Theme.of(context).backgroundColor,
-          iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
-          elevation: 0,
-          titleSpacing: 0,
-          brightness: Theme.of(context).brightness,
-          title: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.only(left: 10),
-                    width: 42,
-                    height: 42,
-                    alignment: Alignment.centerLeft,
-                    color: Colors.transparent,
-                    child: Icon(
-                      CustomIcons.back,
-                      color: Theme.of(context).primaryColorDark,
-                      size: 17,
-                    ),
+    return FeatureDiscovery(
+      child: Scaffold(
+        body: DefaultTabController(
+          length: _tabs.length,
+          child: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+              return <Widget>[
+                SliverPersistentHeader(
+                  delegate: JuntoCommunityCenterAppbar(
+                    expandedHeight:
+                        MediaQuery.of(context).size.height * .11 + 50,
+                    tabs: _tabs,
                   ),
+                  floating: true,
+                  pinned: false,
                 ),
-                Text(
-                  'Community Center',
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-                const SizedBox(width: 42)
+              ];
+            },
+            body: TabBarView(
+              children: <Widget>[
+                SizedBox(),
+                SizedBox(),
               ],
             ),
           ),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(.75),
-            child: Container(
-              height: .75,
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Theme.of(context).dividerColor,
-                    width: .75,
-                  ),
-                ),
-              ),
-            ),
-          ),
         ),
-      ),
-      body: Column(
-        children: <Widget>[],
       ),
     );
   }
