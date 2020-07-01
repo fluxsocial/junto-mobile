dbe5586f5601bfb3102b8c27e6b043b58b11ffce
Neevash Ramdial <mail@neevash.dev>
2020-07-01 18:42:54 -0400

    import clean up

--- lib/screens/den/edit_den/edit_den_button.dart
+++ lib/screens/den/edit_den/edit_den_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:junto_beta_mobile/screens/den/edit_den/edit_den.dart';
 
 class EditDenButton extends StatelessWidget {
   final VoidCallback onTap;
