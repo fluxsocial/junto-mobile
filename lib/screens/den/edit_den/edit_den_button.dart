17e5f42ec53710ac0a0f18af87ce7917e0291b0e
Neevash Ramdial <mail@neevash.dev>
2020-07-01 18:42:03 -0400

    minor bug fix

--- lib/screens/den/edit_den/edit_den_button.dart
+++ lib/screens/den/edit_den/edit_den_button.dart
@@ -3,17 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/screens/den/edit_den/edit_den.dart';
 
 class EditDenButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const EditDenButton({Key key, this.onTap}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          CupertinoPageRoute<dynamic>(
-            builder: (BuildContext context) => JuntoEditDen(),
-          ),
-        );
-      },
+      onTap: onTap,
       child: Container(
         margin: const EdgeInsets.only(left: 15),
         padding: const EdgeInsets.symmetric(
