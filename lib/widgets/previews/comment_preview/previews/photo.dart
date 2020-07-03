edc1fe07f9d613886c47bfa9ef63e59d2766daa0
Eric Yang <eric@junto.love>
2020-07-03 15:28:45 -0400

    Add dynamic comment previews for all types

---
+++ lib/widgets/previews/comment_preview/previews/photo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/models/expression.dart';
+import 'package:junto_beta_mobile/widgets/image_wrapper.dart';
+
+// Displays the given [image] and [imageCaption]
+class PhotoPreview extends StatelessWidget {
+  const PhotoPreview({
+    Key key,
+    @required this.comment,
+  }) : super(key: key);
+
+  final Comment comment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: ImageWrapper(
+        imageUrl: comment.expressionData.thumbnail600,
+        placeholder: (BuildContext context, String _) {
+          return Container(
+            color: Theme.of(context).dividerColor,
+            height: MediaQuery.of(context).size.width / 3 * 2,
+            width: MediaQuery.of(context).size.width,
+          );
+        },
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
