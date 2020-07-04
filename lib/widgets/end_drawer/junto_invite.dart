e2335b5acaf9101c75fa453867fdde61c4e1324e
Eric Yang <eric@junto.love>
2020-07-04 18:07:36 -0400

    Build invite in end drawer and template ui

---
+++ lib/widgets/end_drawer/junto_invite.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:junto_beta_mobile/backend/backend.dart';
+
+import 'junto_invite_appbar.dart';
+
+class JuntoInvite extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(45),
+        child: JuntoInviteAppBar(),
+      ),
+      body: Consumer<UserDataProvider>(
+          builder: (BuildContext context, UserDataProvider user, Widget child) {
+        return SafeArea(
+          child: Column(
+            children: <Widget>[],
+          ),
+        );
+      }),
+    );
+  }
+}
