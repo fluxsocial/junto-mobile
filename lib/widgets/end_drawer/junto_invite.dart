4070f24151695169e0f3527a4d82c09b31c8f51d
Eric Yang <eric@junto.love>
2020-07-06 22:01:44 -0400

    Build unique invite cta

--- lib/widgets/end_drawer/junto_invite.dart
+++ lib/widgets/end_drawer/junto_invite.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:junto_beta_mobile/screens/welcome/widgets/junto_logo.dart';
 
 import 'junto_invite_appbar.dart';
+import 'junto_invite_cta.dart';
 
 class JuntoInvite extends StatelessWidget {
   @override
@@ -19,25 +21,26 @@ class JuntoInvite extends StatelessWidget {
           child: Container(
             padding: const EdgeInsets.symmetric(
               horizontal: 20,
-              vertical: 10,
+              vertical: 40,
             ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Expanded(
-                  child: ListView(
-                    children: <Widget>[
-                      JuntoInviteText(
-                        "Junto was created from the desire to have more meaningful experiences on social media. On Junto, you have the opportunity to build your Pack -- people closest to you that evoke the most unfiltered version of yourself.",
-                      ),
-                      JuntoInviteText(
-                        "You always have the option to share privately to your Pack. The people in your Pack will also have access to your Pack feed, which displays the public content from everyone in it. You become the common thread between all the people you invite, facilitating a more organic way for them to discover one another through their mutual connection - you. ",
-                      ),
-                      JuntoInviteText(
-                        "We envision the growth of Junto to take place through the creation of each individual's closest relationships. Thank you for participating in building this grassroots community with us.",
-                      ),
-                    ],
-                  ),
+                Column(
+                  children: <Widget>[
+                    JuntoLogo(),
+                    JuntoInviteText(
+                      "Junto was created from the desire to have more meaningful experiences on social media. On Junto, you can build your Pack - people closest to you that evoke the most unfiltered version of yourself.",
+                    ),
+                    JuntoInviteText(
+                      "We envision the growth of Junto to take place through the creation of each individual's closest relationships and invite you to bring on one new person a week. Thank you for participating in building this grassroots community with us.",
+                    ),
+                  ],
                 ),
+                JuntoInviteCTA(
+                  title: 'INVITE ONE PERSON',
+                  callToAction: () {},
+                )
               ],
             ),
           ),
