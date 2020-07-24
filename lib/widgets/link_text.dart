77be0031dc113c11544f364c21d2ac64fd37c70b
Neevash Ramdial <mail@neevash.dev>
2020-07-23 21:04:50 -0400

    Integrate link text for shortform

---
+++ lib/widgets/link_text.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+
+class LinkText extends StatelessWidget {
+  const LinkText(
+    this.text, {
+    Key key,
+    this.style,
+    this.textAlign = TextAlign.start,
+    this.maxLines = 1,
+    this.overflow,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle style;
+  final TextAlign textAlign;
+  final int maxLines;
+  final TextOverflow overflow;
+
+  Future<void> onLinkPressed(LinkableElement element) async {
+    if (await canLaunch(element.url)) {
+      await launch(element.url);
+    } else {
+      return;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Linkify(
+      onOpen: onLinkPressed,
+      text: text,
+      linkStyle: style,
+      maxLines: maxLines,
+      textAlign: textAlign,
+      overflow: overflow,
+    );
+  }
+}
