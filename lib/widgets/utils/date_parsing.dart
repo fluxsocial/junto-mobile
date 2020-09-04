a2c56e97701372ca2594f38de3d7d7963deb2033
Eric Yang <eric@junto.foundation>
2020-09-03 22:31:59 -0500

    Clean up parseDate and date_parsing.dart

--- lib/widgets/utils/date_parsing.dart
+++ lib/widgets/utils/date_parsing.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:junto_beta_mobile/backend/backend.dart';
 
 /// Top level function which handles parsing and rendering dates.
 /// Function requires the `BuildContext`, [context], of the current widget and the `DateTime`, [time].
@@ -20,9 +18,9 @@ String parseDate(BuildContext context, DateTime time) {
   // Calculate the difference in time from the give date to the current date.
   final int elapsedTimeInHours = timeDifference.inHours;
   if (elapsedTimeInHours <= 24) {
-    return 'Today';
+    return 'today';
   } else if (elapsedTimeInHours >= 25 && elapsedTimeInHours <= 48) {
-    return 'Yesterday';
+    return 'yesterday';
   } else if (elapsedTimeInHours > 48 && elapsedTimeInHours <= 168) {
     return '${timeDifference.inDays} days ago';
   } else if (elapsedTimeInHours > 168 && elapsedTimeInHours <= 192) {
@@ -30,14 +28,4 @@ String parseDate(BuildContext context, DateTime time) {
   } else {
     return MaterialLocalizations.of(context).formatFullDate(time.toLocal());
   }
-}
-
-Future<DateTime> getTimeOfLastInviteSent(BuildContext context) async {
-  final Map<String, dynamic> inviteInfo =
-      await Provider.of<UserRepo>(context, listen: false).lastInviteSent();
-  return DateTime.parse(inviteInfo['last_invite']);
-}
-
-Duration getTimeDifferenceFromNow(DateTime timeStamp) {
-  return DateTime.now().difference(timeStamp);
 }
