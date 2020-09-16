68e79046809c4cc04774985bacc2974ad05373f5
Neevash Ramdial <mail@neevash.dev>
2020-09-15 20:35:09 -0400

    remove IOClient

--- lib/backend/backend.dart
+++ lib/backend/backend.dart
@@ -1,4 +1,3 @@
-import 'package:http/io_client.dart';
 import 'package:junto_beta_mobile/app/logger/logger.dart';
 import 'package:junto_beta_mobile/app/themes_provider.dart';
 import 'package:junto_beta_mobile/backend/mock/mock_auth.dart';
@@ -55,7 +54,6 @@ class Backend {
       final imageHandler = DeviceImageHandler();
       final authService = CognitoClient();
       final client = JuntoHttp(
-        httpClient: IOClient(),
         tokenProvider: authService,
       );
       final userService = UserServiceCentralized(client);
