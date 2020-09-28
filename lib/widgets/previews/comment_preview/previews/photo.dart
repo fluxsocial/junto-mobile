56c0d720c6af8da19dc408c832a0cd19b4e0b6a8
Eric Yang <eric@junto.foundation>
2020-09-28 16:29:45 -0500

    Add caption to photo comment previews

--- lib/widgets/previews/comment_preview/previews/photo.dart
+++ lib/widgets/previews/comment_preview/previews/photo.dart
@@ -13,19 +13,37 @@ class PhotoPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      child: ImageWrapper(
-        imageUrl: comment.expressionData.thumbnail600,
-        placeholder: (BuildContext context, String _) {
-          return Container(
-            color: Theme.of(context).dividerColor,
-            height: MediaQuery.of(context).size.width / 3 * 2,
-            width: MediaQuery.of(context).size.width,
-          );
-        },
-        fit: BoxFit.cover,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: ImageWrapper(
+            imageUrl: comment.expressionData.thumbnail600,
+            placeholder: (BuildContext context, String _) {
+              return Container(
+                color: Theme.of(context).dividerColor,
+                height: MediaQuery.of(context).size.width / 3 * 2,
+                width: MediaQuery.of(context).size.width,
+              );
+            },
+            fit: BoxFit.cover,
+          ),
+        ),
+        if (comment.expressionData.caption.isNotEmpty)
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
+            ),
+            child: Text(
+              comment.expressionData.caption,
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ),
+      ],
     );
   }
 }
