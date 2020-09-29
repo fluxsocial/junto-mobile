71c6f96aa8e968ffc35588219ed35b88955e8c3e
Neevash Ramdial <mail@neevash.dev>
2020-09-29 15:54:43 -0400

    also check for addresses which contain a null user

--- lib/utils/junto_http.dart
+++ lib/utils/junto_http.dart
@@ -3,10 +3,10 @@ import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:junto_beta_mobile/app/app_config.dart';
 import 'package:flutter/foundation.dart';
-import 'package:meta/meta.dart';
 import 'package:junto_beta_mobile/api.dart';
 import 'package:junto_beta_mobile/app/logger/logger.dart';
 import 'package:junto_beta_mobile/backend/backend.dart';
+import 'package:meta/meta.dart';
 
 class JuntoHttp {
   JuntoHttp({
@@ -25,6 +25,7 @@ class JuntoHttp {
       return client;
     };
   }
+
   VoidCallback onUnAuthorized;
   final IdTokenProvider tokenProvider;
   final String _endPoint = END_POINT;
@@ -130,11 +131,13 @@ class JuntoHttp {
 
 class ErrorInterceptor extends Interceptor {
   ErrorInterceptor(this.onUnAuthorized);
+
   final VoidCallback onUnAuthorized;
 
   Future<DioError> onError(DioError err) async {
     final _hasError = err?.response?.statusCode == 401;
-    if (_hasError && onUnAuthorized != null) {
+    if (_hasError && onUnAuthorized != null ||
+        err.request.path.contains('/users/null')) {
       onUnAuthorized();
     }
     return err;
@@ -144,6 +147,7 @@ class ErrorInterceptor extends Interceptor {
 class HeaderInterceptors extends Interceptor {
   HeaderInterceptors(this.tokenProvider) : assert(tokenProvider != null);
   final IdTokenProvider tokenProvider;
+
   Future<String> _getAuthKey() => tokenProvider.getIdToken();
 
   @override
