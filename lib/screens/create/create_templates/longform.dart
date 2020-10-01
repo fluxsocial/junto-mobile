ddc325c4b4928a751f88bd90809c1648c7d0cb1d
Eric Yang <eric@junto.foundation>
2020-09-30 22:08:58 -0500

    Optimize ui layout and constraints

--- lib/screens/create/create_templates/longform.dart
+++ lib/screens/create/create_templates/longform.dart
@@ -139,202 +139,200 @@ class CreateLongformState extends State<CreateLongform> {
         expressionType: ExpressionType.dynamic,
         onNext: _onNext,
         expressionHasData: expressionHasData,
-        child: Expanded(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: TextField(
-                  focusNode: _titleFocus,
-                  buildCounter: (
-                    BuildContext context, {
-                    int currentLength,
-                    int maxLength,
-                    bool isFocused,
-                  }) =>
-                      null,
-                  controller: _titleController,
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Title',
-                    hintStyle: Theme.of(context).textTheme.headline6.copyWith(
-                          color: Theme.of(context).primaryColorLight,
+        child: Container(
+          child: BlocBuilder<SearchBloc, dynamic>(
+            builder: (BuildContext context, dynamic state) {
+              List<Map<String, String>> _users = [];
+              if (state is LoadedSearchState) {
+                final _listUsers = state?.results;
+
+                _users = _listUsers.where((element) {
+                  print(addedmentions
+                          .indexWhere((e) => element.address == e['id']) ==
+                      -1);
+                  return addedmentions
+                          .indexWhere((e) => element.address == e['id']) ==
+                      -1;
+                }).map((e) {
+                  return ({
+                    'id': e.address,
+                    'display': e.username,
+                    'full_name': e.name,
+                    'photo':
+                        e.profilePicture.length > 0 ? e.profilePicture[0] : '',
+                    'bio': e.bio,
+                    'backgroundPhoto': e.backgroundPhoto,
+                  });
+                }).toList();
+              }
+
+              final _finalList = [...addedmentions, ..._users];
+
+              print("users: $_finalList");
+
+              return Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: TextField(
+                        focusNode: _titleFocus,
+                        buildCounter: (
+                          BuildContext context, {
+                          int currentLength,
+                          int maxLength,
+                          bool isFocused,
+                        }) =>
+                            null,
+                        controller: _titleController,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Title',
+                          hintStyle:
+                              Theme.of(context).textTheme.headline6.copyWith(
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
                         ),
-                  ),
-                  cursorColor: Theme.of(context).primaryColor,
-                  cursorWidth: 2,
-                  maxLines: null,
-                  maxLength: 140,
-                  style: Theme.of(context).textTheme.headline6,
-                  keyboardAppearance: Theme.of(context).brightness,
-                  textCapitalization: TextCapitalization.sentences,
-                  keyboardType: TextInputType.text,
-                ),
-              ),
-              Container(child: BlocBuilder<SearchBloc, dynamic>(
-                builder: (BuildContext context, dynamic state) {
-                  List<Map<String, String>> _users = [];
-                  if (state is LoadedSearchState) {
-                    final _listUsers = state?.results;
-
-                    _users = _listUsers.where((element) {
-                      print(addedmentions
-                              .indexWhere((e) => element.address == e['id']) ==
-                          -1);
-                      return addedmentions
-                              .indexWhere((e) => element.address == e['id']) ==
-                          -1;
-                    }).map((e) {
-                      return ({
-                        'id': e.address,
-                        'display': e.username,
-                        'full_name': e.name,
-                        'photo': e.profilePicture.length > 0
-                            ? e.profilePicture[0]
-                            : '',
-                        'bio': e.bio,
-                        'backgroundPhoto': e.backgroundPhoto,
-                      });
-                    }).toList();
-                  }
-
-                  final _finalList = [...addedmentions, ..._users];
-
-                  print("users: $_finalList");
-                  return Column(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(
-                            maxHeight:
-                                MediaQuery.of(context).size.height * 0.7),
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: FlutterMentions(
-                          key: metionKey,
-                          suggestionPosition: SuggestionPosition.Bottom,
-                          minLines: 1,
-                          maxLines: 20,
-                          keyboardAppearance: Theme.of(context).brightness,
-                          cursorWidth: 2,
-                          focusNode: _bodyFocus,
-                          textInputAction: TextInputAction.newline,
-                          style: Theme.of(context)
-                              .textTheme
-                              .caption
-                              .copyWith(fontSize: 17),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Write here...',
-                          ),
-                          onChanged: (value) {
-                            print(value);
-                          },
-                          onSearchChanged: (String trigger, String value) {
-                            if (value.isNotEmpty) {
-                              context
-                                  .bloc<SearchBloc>()
-                                  .add(SearchingEvent(value, true));
-
-                              setState(() {
-                                _showList = true;
-                              });
-                            } else {
-                              setState(() {
-                                _showList = false;
-                              });
-                            }
-                          },
-                          hideSuggestionList: true,
-                          mentions: [
-                            Mention(
-                              trigger: '@',
-                              data: [..._finalList],
-                              style: TextStyle(
-                                color: Colors.amber,
-                              ),
-                              markupBuilder: (trigger, mention, value) {
-                                return '[$trigger$value:$mention]';
-                              },
-                            ),
-                          ],
-                        ),
+                        cursorColor: Theme.of(context).primaryColor,
+                        cursorWidth: 2,
+                        maxLines: null,
+                        maxLength: 140,
+                        style: Theme.of(context).textTheme.headline6,
+                        keyboardAppearance: Theme.of(context).brightness,
+                        textCapitalization: TextCapitalization.sentences,
+                        keyboardType: TextInputType.text,
                       ),
-                      if (_showList)
-                        ListView.builder(
-                          padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          itemCount: _finalList.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return MemberPreview(
-                              onUserTap: () {
-                                metionKey.currentState
-                                    .addMention(_finalList[index]);
+                    ),
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: FlutterMentions(
+                              key: metionKey,
+                              suggestionPosition: SuggestionPosition.Bottom,
+                              minLines: 1,
+                              maxLines: 20,
+                              keyboardAppearance: Theme.of(context).brightness,
+                              cursorWidth: 2,
+                              focusNode: _bodyFocus,
+                              textInputAction: TextInputAction.newline,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .caption
+                                  .copyWith(fontSize: 17),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Write here...',
+                              ),
+                              onChanged: (value) {
+                                print(value);
+                              },
+                              onSearchChanged: (String trigger, String value) {
+                                if (value.isNotEmpty) {
+                                  context
+                                      .bloc<SearchBloc>()
+                                      .add(SearchingEvent(value, true));
 
-                                if (addedmentions.indexWhere((element) =>
-                                        element['id'] ==
-                                        _finalList[index]['id']) ==
-                                    -1) {
-                                  addedmentions = [
-                                    ...addedmentions,
-                                    _finalList[index]
-                                  ];
+                                  setState(() {
+                                    _showList = true;
+                                  });
+                                } else {
+                                  setState(() {
+                                    _showList = false;
+                                  });
                                 }
-
-                                setState(() {
-                                  _showList = false;
-                                });
                               },
-                              profile: UserProfile(
-                                username: _finalList[index]['display'],
-                                address: _finalList[index]['id'],
-                                profilePicture: [_finalList[index]['photo']],
-                                name: _finalList[index]['full_name'],
-                                verified: true,
-                                backgroundPhoto: _finalList[index]
-                                    ['backgroundPhoto'],
-                                badges: [],
-                                gender: [],
-                                website: [],
-                                bio: _finalList[index]['bio'],
-                                location: [],
-                              ),
-                            );
-                          },
-                        )
-                    ],
-                  );
-                },
-              )),
-              !_showBottomNav
-                  ? GestureDetector(
-                      onTap: () {
-                        _titleFocus.unfocus();
-                        _bodyFocus.unfocus();
-                      },
-                      onVerticalDragEnd: (dx) {
-                        _titleFocus.unfocus();
-                        _bodyFocus.unfocus();
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 15,
-                        ),
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.centerRight,
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                          child: Icon(
-                            CustomIcons.back,
-                            size: 15,
-                            color: Theme.of(context).primaryColorLight,
+                              hideSuggestionList: true,
+                              mentions: [
+                                Mention(
+                                  trigger: '@',
+                                  data: [..._finalList],
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColorDark,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                  markupBuilder: (trigger, mention, value) {
+                                    return '[$trigger$value:$mention]';
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
+                          if (_showList)
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              left: 0,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).backgroundColor,
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: Theme.of(context).dividerColor,
+                                      width: .75,
+                                    ),
+                                  ),
+                                ),
+                                constraints: BoxConstraints(
+                                  maxHeight:
+                                      MediaQuery.of(context).size.height * .38,
+                                ),
+                                child: ListView.builder(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 10.0),
+                                  itemCount: _finalList.length,
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, index) {
+                                    return MemberPreview(
+                                      onUserTap: () {
+                                        metionKey.currentState
+                                            .addMention(_finalList[index]);
+
+                                        if (addedmentions.indexWhere(
+                                                (element) =>
+                                                    element['id'] ==
+                                                    _finalList[index]['id']) ==
+                                            -1) {
+                                          addedmentions = [
+                                            ...addedmentions,
+                                            _finalList[index]
+                                          ];
+                                        }
+
+                                        setState(() {
+                                          _showList = false;
+                                        });
+                                      },
+                                      profile: UserProfile(
+                                        username: _finalList[index]['display'],
+                                        address: _finalList[index]['id'],
+                                        profilePicture: [
+                                          _finalList[index]['photo']
+                                        ],
+                                        name: _finalList[index]['full_name'],
+                                        verified: true,
+                                        backgroundPhoto: _finalList[index]
+                                            ['backgroundPhoto'],
+                                        badges: [],
+                                        gender: [],
+                                        website: [],
+                                        bio: _finalList[index]['bio'],
+                                        location: [],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                        ],
                       ),
-                    )
-                  : const SizedBox()
-            ],
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),
