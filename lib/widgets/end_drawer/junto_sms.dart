fb5211e370feee740611349b93f4ebb6548206b7
Eric Yang <eric@junto.foundation>
2020-10-03 14:26:22 -0500

    Fix Junto SMS list of phone display

--- lib/widgets/end_drawer/junto_sms.dart
+++ lib/widgets/end_drawer/junto_sms.dart
@@ -72,101 +72,115 @@ class JuntoSmsState extends State<JuntoSms> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _contacts.length,
-              itemBuilder: (BuildContext context, int index) {
-                final Contact contact = _contacts.elementAt(index);
-                final String number = contact.phones.first.value;
-                return Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Theme.of(context).dividerColor,
-                        width: .5,
+          if (_contacts.isNotEmpty || _contacts != null)
+            Expanded(
+              child: ListView.builder(
+                itemCount: _contacts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Contact contact = _contacts.elementAt(index);
+                  String number;
+
+                  if (contact.phones.isNotEmpty && contact.phones != null) {
+                    number = contact.phones.first.value;
+                  } else {
+                    number = '';
+                  }
+
+                  if (contact.displayName != null && number.isNotEmpty) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
                       ),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          MemberAvatarPlaceholder(
-                            diameter: 45,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Theme.of(context).dividerColor,
+                            width: .5,
                           ),
-                          const SizedBox(width: 10),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
                             children: [
-                              Text(
-                                contact.displayName,
+                              MemberAvatarPlaceholder(
+                                diameter: 45,
+                              ),
+                              const SizedBox(width: 10),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    contact.displayName,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Theme.of(context).primaryColor,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Text(
+                                    number,
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      color:
+                                          Theme.of(context).primaryColorLight,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          InkWell(
+                            borderRadius: BorderRadius.circular(25),
+                            onTap: () async {
+                              String uri;
+                              if (Platform.isIOS) {
+                                uri = Uri.encodeFull(
+                                    "sms:${number}&body=Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${_userProfile.user.username}. https://testflight.apple.com/join/LT9d0ob1");
+                              } else if (Platform.isAndroid) {
+                                uri = Uri.encodeFull(
+                                    "sms:${number}?body=Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${_userProfile.user.username}. https://testflight.apple.com/join/LT9d0ob1");
+                              }
+
+                              if (await canLaunch(uri)) {
+                                await launch(uri);
+                              } else {
+                                await SingleActionDialog(
+                                  context: context,
+                                  dialogText:
+                                      'Sorry, something is up. Try again.',
+                                );
+                              }
+                              ;
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).dividerColor,
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 15, vertical: 5),
+                              child: Text(
+                                'Invite',
                                 style: TextStyle(
-                                  fontSize: 15,
+                                  fontSize: 13,
                                   color: Theme.of(context).primaryColor,
                                   fontWeight: FontWeight.w700,
                                 ),
                               ),
-                              Text(
-                                contact.phones.first.value,
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  color: Theme.of(context).primaryColorLight,
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ],
                       ),
-                      InkWell(
-                        borderRadius: BorderRadius.circular(25),
-                        onTap: () async {
-                          String uri;
-                          if (Platform.isIOS) {
-                            uri = Uri.encodeFull(
-                                "sms:${number}&body=Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${_userProfile.user.username}. https://testflight.apple.com/join/LT9d0ob1");
-                          } else if (Platform.isAndroid) {
-                            uri = Uri.encodeFull(
-                                "sms:${number}?body=Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${_userProfile.user.username}. https://testflight.apple.com/join/LT9d0ob1");
-                          }
-
-                          if (await canLaunch(uri)) {
-                            await launch(uri);
-                          } else {
-                            await SingleActionDialog(
-                              context: context,
-                              dialogText: 'Sorry, something is up. Try again.',
-                            );
-                          }
-                          ;
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).dividerColor,
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 15, vertical: 5),
-                          child: Text(
-                            'Invite',
-                            style: TextStyle(
-                              fontSize: 13,
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+                    );
+                  } else {
+                    return SizedBox();
+                  }
+                },
+              ),
             ),
-          ),
         ],
       ),
     );
