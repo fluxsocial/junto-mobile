951326844ea22983aad89362bded027b0107d08c
Eric <37339459+sunyatax@users.noreply.github.com>
2020-10-04 18:39:36 -0400

    Revert "Build widget to enable user to copy shareable invite link"
    This reverts commit 62eaa9af7d05f53963f6841209044a138f5ea71d.

--- lib/widgets/end_drawer/junto_invite_appbar.dart
+++ lib/widgets/end_drawer/junto_invite_appbar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/app/custom_icons.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:clipboard/clipboard.dart';
-import 'package:junto_beta_mobile/widgets/dialogs/user_feedback.dart';
 
 class JuntoInviteAppBar extends StatelessWidget {
   @override
@@ -41,21 +39,7 @@ class JuntoInviteAppBar extends StatelessWidget {
               'Build Your Pack',
               style: Theme.of(context).textTheme.subtitle1,
             ),
-            GestureDetector(
-              onTap: () async {
-                FlutterClipboard.copy('https://junto.typeform.com/to/k7BUVK8f');
-                await showFeedback(context,
-                    message: 'Copied shareable invite link!');
-              },
-              child: Container(
-                width: 42,
-                child: Image.asset(
-                  'assets/images/junto-mobile__external-link.png',
-                  height: 17,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-            ),
+            const SizedBox(width: 42)
           ],
         ),
       ),
