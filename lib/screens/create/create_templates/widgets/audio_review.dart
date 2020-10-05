42b7e2c207891add48e0c5f1e816b3cd62c2d91f
Fayeed <fayeed@live.com>
2020-10-05 09:56:39 +0530

    Audio expression completed

--- lib/screens/create/create_templates/widgets/audio_review.dart
+++ lib/screens/create/create_templates/widgets/audio_review.dart
@@ -1,72 +1,192 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_mentions/flutter_mentions.dart';
+import 'package:junto_beta_mobile/backend/repositories/search_repo.dart';
 import 'package:junto_beta_mobile/generated/l10n.dart';
+import 'package:junto_beta_mobile/models/user_model.dart';
 import 'package:junto_beta_mobile/screens/create/create_templates/audio_service.dart';
+import 'package:junto_beta_mobile/screens/global_search/search_bloc/search_bloc.dart';
+import 'package:junto_beta_mobile/screens/global_search/search_bloc/search_event.dart';
+import 'package:junto_beta_mobile/screens/global_search/search_bloc/search_state.dart';
+import 'package:junto_beta_mobile/utils/utils.dart';
 import 'package:junto_beta_mobile/widgets/audio/audio_preview.dart';
+import 'package:junto_beta_mobile/widgets/previews/member_preview/member_preview.dart';
 import 'package:junto_beta_mobile/widgets/utils/hex_color.dart';
 import 'package:provider/provider.dart';
 
-class AudioReview extends StatelessWidget {
-  const AudioReview({
+class AudioReview extends StatefulWidget {
+  AudioReview({
     this.audioPhotoBackground,
     this.audioGradientValues,
     this.titleController,
     this.captionController,
     this.captionFocus,
+    this.mentionKey,
   });
   final File audioPhotoBackground;
   final List<String> audioGradientValues;
   final TextEditingController titleController;
   final TextEditingController captionController;
   final FocusNode captionFocus;
+  final GlobalKey<FlutterMentionsState> mentionKey;
+
+  @override
+  _AudioReviewState createState() => _AudioReviewState();
+}
+
+class _AudioReviewState extends State<AudioReview>
+    with CreateExpressionHelpers {
+  bool _showList = false;
+
+  List<Map<String, dynamic>> addedmentions = [];
 
   @override
   Widget build(BuildContext context) {
     return Consumer<AudioService>(builder: (context, audio, child) {
-      return Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView(
-              children: <Widget>[
-                _showAudioReviewTemplate(),
-              ],
-            ),
-          ),
-          if (captionFocus.hasFocus)
-            AudioUnfocus(
-              captionFocus: captionFocus,
-            ),
-        ],
+      return BlocProvider(
+        create: (BuildContext context) {
+          return SearchBloc(Provider.of<SearchRepo>(context, listen: false));
+        },
+        child: BlocBuilder<SearchBloc, SearchState>(
+          builder: (context, state) {
+            final _users = getUserList(state, addedmentions);
+
+            final _finalList = [...addedmentions, ..._users];
+
+            return Container(
+              child: Stack(
+                children: [
+                  Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: ListView(
+                          children: <Widget>[
+                            _showAudioReviewTemplate(),
+                          ],
+                        ),
+                      ),
+                      if (widget.captionFocus.hasFocus)
+                        AudioUnfocus(
+                          captionFocus: widget.captionFocus,
+                        ),
+                    ],
+                  ),
+                  if (_showList) buildUserMention(context, _finalList),
+                ],
+              ),
+            );
+          },
+        ),
       );
     });
   }
 
+  void toggleSearch(bool value) {
+    setState(() {
+      _showList = value;
+    });
+  }
+
+  Positioned buildUserMention(
+      BuildContext context, List<Map<String, dynamic>> _finalList) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+          border: Border(
+            top: BorderSide(
+              color: Theme.of(context).dividerColor,
+              width: .75,
+            ),
+          ),
+        ),
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height * .38,
+        ),
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          itemCount: _finalList.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return MemberPreview(
+              onUserTap: () {
+                widget.mentionKey.currentState.addMention(_finalList[index]);
+
+                if (addedmentions.indexWhere((element) =>
+                        element['id'] == _finalList[index]['id']) ==
+                    -1) {
+                  addedmentions = [...addedmentions, _finalList[index]];
+                }
+
+                setState(() {
+                  _showList = false;
+                });
+              },
+              profile: UserProfile(
+                username: _finalList[index]['display'],
+                address: _finalList[index]['id'],
+                profilePicture: [_finalList[index]['photo']],
+                name: _finalList[index]['full_name'],
+                verified: true,
+                backgroundPhoto: _finalList[index]['backgroundPhoto'],
+                badges: [],
+                gender: [],
+                website: [],
+                bio: _finalList[index]['bio'],
+                location: [],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
   Widget _showAudioReviewTemplate() {
-    if (audioPhotoBackground == null && audioGradientValues.isEmpty) {
+    if (widget.audioPhotoBackground == null &&
+        widget.audioGradientValues.isEmpty) {
       return AudioReviewDefault(
-        titleController: titleController,
-        captionController: captionController,
-        captionFocus: captionFocus,
+        titleController: widget.titleController,
+        captionController: widget.captionController,
+        captionFocus: widget.captionFocus,
+        toggleSearch: toggleSearch,
+        addedmentions: addedmentions,
+        mentionKey: widget.mentionKey,
       );
-    } else if (audioPhotoBackground != null && audioGradientValues.isEmpty) {
+    } else if (widget.audioPhotoBackground != null &&
+        widget.audioGradientValues.isEmpty) {
       return AudioReviewWithPhoto(
-        titleController: titleController,
-        captionController: captionController,
-        audioPhotoBackground: audioPhotoBackground,
-        captionFocus: captionFocus,
+        titleController: widget.titleController,
+        captionController: widget.captionController,
+        audioPhotoBackground: widget.audioPhotoBackground,
+        captionFocus: widget.captionFocus,
+        toggleSearch: toggleSearch,
+        addedmentions: addedmentions,
+        mentionKey: widget.mentionKey,
       );
-    } else if (audioPhotoBackground == null && audioGradientValues.isNotEmpty) {
+    } else if (widget.audioPhotoBackground == null &&
+        widget.audioGradientValues.isNotEmpty) {
       return AudioReviewWithGradient(
-        titleController: titleController,
-        captionController: captionController,
-        audioGradientValues: audioGradientValues,
-        captionFocus: captionFocus,
+        titleController: widget.titleController,
+        captionController: widget.captionController,
+        audioGradientValues: widget.audioGradientValues,
+        captionFocus: widget.captionFocus,
+        toggleSearch: toggleSearch,
+        addedmentions: addedmentions,
+        mentionKey: widget.mentionKey,
       );
     } else {
       return AudioReviewDefault(
-        captionController: captionController,
-        titleController: titleController,
-        captionFocus: captionFocus,
+        captionController: widget.captionController,
+        titleController: widget.titleController,
+        captionFocus: widget.captionFocus,
+        toggleSearch: toggleSearch,
+        addedmentions: addedmentions,
+        mentionKey: widget.mentionKey,
       );
     }
   }
@@ -77,11 +197,18 @@ class AudioReviewDefault extends StatelessWidget {
     this.titleController,
     this.captionController,
     this.captionFocus,
+    this.toggleSearch,
+    this.addedmentions,
+    this.mentionKey,
   });
 
   final TextEditingController titleController;
   final TextEditingController captionController;
   final FocusNode captionFocus;
+  final Function(bool) toggleSearch;
+  final List<Map<String, dynamic>> addedmentions;
+  final GlobalKey<FlutterMentionsState> mentionKey;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -93,6 +220,9 @@ class AudioReviewDefault extends StatelessWidget {
         AudioCaption(
           captionController: captionController,
           captionFocus: captionFocus,
+          toggleSearch: toggleSearch,
+          addedmentions: addedmentions,
+          mentionKey: mentionKey,
         ),
         // if (captionFocus.hasFocus)
         //   AudioUnfocus(
@@ -109,12 +239,18 @@ class AudioReviewWithGradient extends StatelessWidget {
     this.captionController,
     this.captionFocus,
     this.audioGradientValues,
+    this.toggleSearch,
+    this.addedmentions,
+    this.mentionKey,
   });
 
   final TextEditingController titleController;
   final TextEditingController captionController;
   final FocusNode captionFocus;
   final List<String> audioGradientValues;
+  final Function(bool) toggleSearch;
+  final List<Map<String, dynamic>> addedmentions;
+  final GlobalKey<FlutterMentionsState> mentionKey;
 
   @override
   Widget build(BuildContext context) {
@@ -140,6 +276,9 @@ class AudioReviewWithGradient extends StatelessWidget {
         AudioCaption(
           captionController: captionController,
           captionFocus: captionFocus,
+          toggleSearch: toggleSearch,
+          addedmentions: addedmentions,
+          mentionKey: mentionKey,
         ),
         // if (captionFocus.hasFocus)
         //   AudioUnfocus(
@@ -156,12 +295,18 @@ class AudioReviewWithPhoto extends StatelessWidget {
     this.captionController,
     this.captionFocus,
     this.audioPhotoBackground,
+    this.toggleSearch,
+    this.addedmentions,
+    this.mentionKey,
   });
 
   final titleController;
   final TextEditingController captionController;
   final FocusNode captionFocus;
   final File audioPhotoBackground;
+  final Function(bool) toggleSearch;
+  final List<Map<String, dynamic>> addedmentions;
+  final GlobalKey<FlutterMentionsState> mentionKey;
 
   @override
   Widget build(BuildContext context) {
@@ -202,6 +347,9 @@ class AudioReviewWithPhoto extends StatelessWidget {
         AudioCaption(
           captionController: captionController,
           captionFocus: captionFocus,
+          toggleSearch: toggleSearch,
+          addedmentions: addedmentions,
+          mentionKey: mentionKey,
         ),
         // if (captionFocus.hasFocus)
         //   AudioUnfocus(
@@ -295,47 +443,84 @@ class AudioTitle extends StatelessWidget {
   }
 }
 
-class AudioCaption extends StatelessWidget {
-  const AudioCaption({
+class AudioCaption extends StatelessWidget with CreateExpressionHelpers {
+  AudioCaption({
     Key key,
     @required this.captionController,
     @required this.captionFocus,
+    this.toggleSearch,
+    this.addedmentions,
+    this.mentionKey,
   }) : super(key: key);
 
   final TextEditingController captionController;
   final FocusNode captionFocus;
+  final Function(bool) toggleSearch;
+  final List<Map<String, dynamic>> addedmentions;
+  final GlobalKey<FlutterMentionsState> mentionKey;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: TextField(
-        controller: captionController,
-        focusNode: captionFocus,
-        autofocus: false,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.all(0),
-          hintStyle: TextStyle(
-            color: Theme.of(context).primaryColorLight,
-            fontSize: 17,
-            fontWeight: FontWeight.w500,
+    return BlocBuilder<SearchBloc, SearchState>(
+      builder: (context, state) {
+        final _users = getUserList(state, addedmentions);
+
+        final _finalList = [...addedmentions, ..._users];
+
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: FlutterMentions(
+            key: mentionKey,
+            focusNode: captionFocus,
+            autofocus: false,
+            onSearchChanged: (String trigger, String value) {
+              if (value.isNotEmpty) {
+                context.bloc<SearchBloc>().add(SearchingEvent(value, true));
+
+                toggleSearch(true);
+              } else {
+                toggleSearch(false);
+              }
+            },
+            mentions: [
+              Mention(
+                trigger: '@',
+                data: [..._finalList],
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                  fontWeight: FontWeight.w700,
+                ),
+                markupBuilder: (trigger, mention, value) {
+                  return '[$trigger$value:$mention]';
+                },
+              ),
+            ],
+            hideSuggestionList: true,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(0),
+              hintStyle: TextStyle(
+                color: Theme.of(context).primaryColorLight,
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+              border: InputBorder.none,
+              hintText: 'Write a caption...',
+              counter: SizedBox(),
+            ),
+            cursorColor: Theme.of(context).primaryColor,
+            cursorWidth: 2,
+            maxLines: null,
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+            textInputAction: TextInputAction.newline,
+            keyboardAppearance: Theme.of(context).brightness,
+            textCapitalization: TextCapitalization.sentences,
           ),
-          border: InputBorder.none,
-          hintText: 'Write a caption...',
-          counter: SizedBox(),
-        ),
-        cursorColor: Theme.of(context).primaryColor,
-        cursorWidth: 2,
-        maxLines: null,
-        style: TextStyle(
-          color: Theme.of(context).primaryColor,
-          fontSize: 17,
-          fontWeight: FontWeight.w500,
-        ),
-        textInputAction: TextInputAction.newline,
-        keyboardAppearance: Theme.of(context).brightness,
-        textCapitalization: TextCapitalization.sentences,
-      ),
+        );
+      },
     );
   }
 }
