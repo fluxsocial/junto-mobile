2305d2dea7c7dcc3432e57cd21c75e9591a8a247
Eric Yang <eric@junto.foundation>
2020-10-08 16:18:03 -0400

    Update cc addresses

--- lib/app/community_center_addresses.dart
+++ lib/app/community_center_addresses.dart
@@ -2,9 +2,9 @@ import 'package:junto_beta_mobile/app/app_config.dart';
 
 final String kCommunityCenterAddress = appConfig.flavor == Flavor.prod
     ? '0ab99620-8835-d63b-3836-f091992ca2b4'
-    : '80ba6100-cf6e-cc3c-cb8c-c945c82e8446';
+    : '9aba84ed-fb00-02bc-1c20-d353fb6a505d';
 
 // updates address
 String kUpdatesAddress = appConfig.flavor == Flavor.prod
     ? '98b99620-ca1f-fda2-060d-d1a22f1de6d2'
-    : '3eba6101-0240-f2a7-ecfe-f15440d7f02e';
+    : '98ba84ee-23e1-955a-e7cc-ac57344750a2';
