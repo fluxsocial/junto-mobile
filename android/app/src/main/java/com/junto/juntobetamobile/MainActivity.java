edae3dfc5a3c6202e660e7d65624e84d7c0ffb8b
Eric <37339459+sunyatax@users.noreply.github.com>
2020-11-21 17:47:49 -0600

    Revert "Moved android project to embeding v2"
    This reverts commit a4ca5126f33d30c1499edbcc0af9c69861461692.

--- android/app/src/main/java/com/junto/juntobetamobile/MainActivity.java
+++ android/app/src/main/java/com/junto/juntobetamobile/MainActivity.java
@@ -1,5 +1,13 @@
 package com.junto.juntobetamobile;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {}
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
