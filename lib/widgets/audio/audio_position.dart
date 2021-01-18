62f4a7c444d3e06607f891e224685075061c802e
Fayeed <fayeed@live.com>
2021-01-18 11:19:35 +0530

    fix(audio_player) - Moved to different audio player library flutter_sound

--- lib/widgets/audio/audio_position.dart
+++ lib/widgets/audio/audio_position.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:junto_beta_mobile/screens/create/create_templates/audio_service.dart';
 import 'package:provider/provider.dart';
+import 'package:junto_beta_mobile/utils/extensions/duration_ext.dart';
 
 class AudioPosition extends StatelessWidget {
   const AudioPosition({
@@ -27,17 +28,25 @@ class AudioPosition extends StatelessWidget {
   }
 
   String getCurrentPosition(AudioService audio) {
-    if (audio.currentPosition != null) {
-      return formatDuration(audio.currentPosition);
+    Duration max;
+    if (audio.recordingAvailable) {
+      max = audio.recordingDuration;
+    } else {
+      max = audio.maxDuration;
     }
+
+    if (audio.currentPosition != null) {
+      return formatDuration(audio.currentPosition.secondCeilRounder(max));
+    }
+
     return '0:00';
   }
 
   String getMaxDuration(AudioService audio) {
     if (audio.recordingAvailable) {
-      return formatDuration(audio.recordingDuration);
+      return formatDuration(audio.recordingDuration.secondFloorRounder());
     } else {
-      return formatDuration(audio.maxDuration);
+      return formatDuration(audio.maxDuration.secondFloorRounder());
     }
   }
 
