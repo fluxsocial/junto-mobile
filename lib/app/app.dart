c7f5ec49b705605e63c1dbef0dfdb4e0e7c7fddf
Fayeed <fayeed@live.com>
2021-02-04 21:51:35 +0530

    feat(acai) - ACAI setup

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_liquidcore/liquidcore.dart';
 import 'package:flutter_mentions/flutter_mentions.dart';
 import 'package:junto_beta_mobile/app/material_app_with_theme.dart';
 import 'package:junto_beta_mobile/app/providers/bloc_providers.dart';
@@ -11,17 +12,54 @@ import 'package:junto_beta_mobile/backend/services.dart';
 import 'package:junto_beta_mobile/screens/notifications/notifications_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:http/http.dart' as http;
 
 class JuntoApp extends StatelessWidget {
-  const JuntoApp({
+  JuntoApp({
     Key key,
     @required this.backend,
   }) : super(key: key);
 
   final Backend backend;
+  MicroService _microService;
+
+  void initMicroService() async {
+    String uri;
+
+    try {
+      uri = "@flutter_assets/acai/main.js";
+
+      final _microService = MicroService(uri);
+
+      // Start the service.
+      await _microService.start();
+
+      final id = await _microService.getMicroServiceId();
+
+      print('test: 1 ${id} | ${_microService.isStarted}');
+
+      if (_microService.isStarted) {
+        print('test: Microservice started');
+        await _microService.emit('init');
+        try {
+          final result = await http.get('http://10.0.2.2:4000');
+          print('test: ${result.body.replaceAll('\n', '')}');
+        } catch (e) {
+          print('test: 1 $e');
+        }
+      }
+
+      print('test: 1 ${id} | ${_microService.hasExit}');
+    } catch (e) {
+      print('error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_microService == null) {
+      initMicroService();
+    }
     return MultiProvider(
       providers: <SingleChildWidget>[
         ChangeNotifierProvider<UserDataProvider>.value(
