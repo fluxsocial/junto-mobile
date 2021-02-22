c2752734f2b6155584777b8ac22ed7b4d6abe2f1
Fayeed Pawaskar <fayeed@live.com>
2021-02-22 14:37:00 +0530

    feat(relations) - Added bloc to my pack relations tab

--- lib/widgets/end_drawer/end_drawer_relationships/pack_members.dart
+++ lib/widgets/end_drawer/end_drawer_relationships/pack_members.dart
@@ -2,9 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:junto_beta_mobile/backend/repositories.dart';
 import 'package:junto_beta_mobile/models/expression_query_params.dart';
 import 'package:junto_beta_mobile/models/models.dart';
+import 'package:junto_beta_mobile/screens/packs/packs_bloc/pack_bloc.dart';
 import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer_relationships/error_widget.dart';
 import 'package:junto_beta_mobile/widgets/previews/member_preview/member_preview.dart';
 import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
@@ -24,59 +26,82 @@ class PackMembers extends StatefulWidget {
 }
 
 class PackMembersState extends State<PackMembers> {
-  //TODO(Nash): Replace with bloc
-  Future<List<Users>> getPackMembers() async {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> getPackMembers(BuildContext _context) async {
     final userData = await Provider.of<UserRepo>(context, listen: false)
         .getUser(widget.userAddress);
-    final query = await Provider.of<GroupRepo>(context, listen: false)
-        .getGroupMembers(userData.pack.address,
-            ExpressionQueryParams(paginationPosition: '0'));
-    return query.results;
+    _context.bloc<PackBloc>().add(FetchPacks(group: userData.pack.address));
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Users>>(
-      future: getPackMembers(),
-      builder: (BuildContext context, AsyncSnapshot<List<Users>> snapshot) {
-        if (snapshot.hasData) {
-          final List<Users> packMembers = snapshot.data;
+    return BlocProvider(
+      create: (context) => PackBloc(
+        Provider.of<ExpressionRepo>(context, listen: false),
+        Provider.of<GroupRepo>(context, listen: false),
+      ),
+      child: BlocBuilder<PackBloc, PackState>(
+        builder: (context, state) {
+          if (state is PackInitial) {
+            getPackMembers(context);
+          } else if (state is PacksLoaded) {
+            final List<Users> packMembers = state.groupMemebers;
 
-          if (packMembers.length > 0) {
-            return Container(
-              child: Column(
-                children: [
-                  SearchBar(
-                    hintText: 'Search Packs',
-                  ),
-                  Expanded(
-                    child: ListView(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      children: packMembers
-                          .map(
-                            (dynamic packMember) =>
-                                MemberPreview(profile: packMember.user),
-                          )
-                          .toList(),
+            if (packMembers.length > 0) {
+              return Container(
+                child: Column(
+                  children: [
+                    SearchBar(
+                      hintText: 'Search Packs',
                     ),
-                  ),
-                ],
-              ),
-            );
-          } else {
-            return FeedPlaceholder(
-              placeholderText: 'No pack members yet!',
-              image: 'assets/images/junto-mobile__bench.png',
-            );
+                    Expanded(
+                      child: NotificationListener(
+                        onNotification: (ScrollNotification notification) {
+                          final metrics = notification.metrics;
+                          double scrollPercent =
+                              (metrics.pixels / metrics.maxScrollExtent) * 100;
+                          if (scrollPercent.roundToDouble() == 60.0) {
+                            context
+                                .bloc<PackBloc>()
+                                .add(FetchMorePacksMembers());
+                            return true;
+                          }
+                          return false;
+                        },
+                        child: ListView(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          children: packMembers
+                              .map(
+                                (dynamic packMember) =>
+                                    MemberPreview(profile: packMember.user),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return FeedPlaceholder(
+                placeholderText: 'No pack members yet!',
+                image: 'assets/images/junto-mobile__bench.png',
+              );
+            }
+          } else if (state is PacksError) {
+            return JuntoErrorWidget(errorMessage: 'Hmm, something went wrong');
           }
-        } else if (snapshot.hasError) {
-          print(snapshot.error);
-          return JuntoErrorWidget(errorMessage: 'Hmm, something went wrong');
-        }
-        return Center(
-          child: JuntoProgressIndicator(),
-        );
-      },
+          return Center(
+            child: JuntoProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
+
+class BlockBuilder {}
