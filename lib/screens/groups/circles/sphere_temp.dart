ddec514ab0a361bac7c6c3cccc2da300e3c574ae
Fayeed Pawaskar <fayeed@live.com>
2021-02-24 10:42:52 +0530

    fix(group) - Disabled groups

---
+++ lib/screens/groups/circles/sphere_temp.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:junto_beta_mobile/screens/groups/circles/sphere_temp_app.dart';
+
+// This screen displays the temporary page we'll display until groups are released
+class SpheresTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          MediaQuery.of(context).size.height * .1 + 50,
+        ),
+        child: SpheresTempAppBar(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Transform.translate(
+            offset: Offset(0.0, -60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/junto-mobile__groups--placeholder.png',
+                  height: 170,
+                  color: Theme.of(context).primaryColor,
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  'We will open this layer soon.',
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
