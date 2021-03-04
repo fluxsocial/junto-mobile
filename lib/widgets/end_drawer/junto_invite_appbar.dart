4650455aae2b844d2938250d821c762f27b6f46b
Eric Yang <eric@junto.foundation>
2021-03-04 15:57:33 -0600

    Update invite link to indiegogo

--- lib/widgets/end_drawer/junto_invite_appbar.dart
+++ lib/widgets/end_drawer/junto_invite_appbar.dart
@@ -42,15 +42,22 @@ class JuntoInviteAppBar extends StatelessWidget {
               ),
             ),
             Text(
-              'Build Your Pack',
+              'Invite',
               style: Theme.of(context).textTheme.subtitle1,
             ),
             GestureDetector(
               onTap: () async {
-                Share.share(
-                  "Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${userProfile.user.username}. https://junto.typeform.com/to/k7BUVK8f",
-                  subject: 'Check Junto out!',
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => JuntoBetaInviteDialog(
+                    context: context,
+                    userProfile: userProfile,
+                  ),
                 );
+                // Share.share(
+                //   "Hey! I started using this more authentic and nonprofit social media platform called Junto. Here's an invite to their closed alpha - you can connect with me @${userProfile.user.username}. https://junto.typeform.com/to/k7BUVK8f",
+                //   subject: 'Check Junto out!',
+                // );
               },
               child: Container(
                 width: 42,
@@ -76,6 +83,113 @@ class JuntoInviteAppBar extends StatelessWidget {
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class JuntoBetaInviteDialog extends StatelessWidget {
+  const JuntoBetaInviteDialog({
+    this.context,
+    this.userProfile,
+  });
+
+  final BuildContext context;
+  final UserData userProfile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.only(
+          top: 25,
+          left: 25,
+          right: 25,
+        ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Feel free to share our Indiegogo campaign with your friends! Those who contribute will get instant access to our private beta. ',
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.fade,
+              style: TextStyle(
+                fontSize: 17,
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Share.share(
+                  "https://www.indiegogo.com/projects/junto-a-new-breed-of-social-media--2#/",
+                  subject: 'Check out the Junto beta!',
+                );
+              },
+              child: Container(
+                color: Colors.transparent,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/junto-mobile__external-link.png',
+                      height: 17,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Copy Link',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding:
+                        const EdgeInsets.only(right: 25, left: 25, bottom: 25),
+                    color: Colors.transparent,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'CLOSE',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
