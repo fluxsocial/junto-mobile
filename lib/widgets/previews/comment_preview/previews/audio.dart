23fbcab53667a6f93a8e3f6058c14fa61be92bac
Fayeed Pawaskar <fayeed@live.com>
2021-03-11 10:58:41 +0530

    fix(dep) - Image chaching fixed

--- lib/widgets/previews/comment_preview/previews/audio.dart
+++ lib/widgets/previews/comment_preview/previews/audio.dart
@@ -199,7 +199,7 @@ class AudioPreviewWithPhoto extends StatelessWidget {
           ),
           image: CachedNetworkImageProvider(
             photo,
-            cacheManager: CustomCacheManager(),
+            cacheManager: CustomCacheManager.instance,
           ),
           fit: BoxFit.cover,
         ),
