e0bacfec89a6525a35d36451c16356d852eb028a
Fayeed Pawaskar <fayeed@live.com>
2021-04-08 08:47:11 +0530

    feat(group) - Search working with groups

--- lib/screens/groups/circles/public_circles.dart
+++ lib/screens/groups/circles/public_circles.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:junto_beta_mobile/models/models.dart';
 import 'package:junto_beta_mobile/utils/utils.dart';
 import 'package:junto_beta_mobile/widgets/custom_refresh/custom_refresh.dart';
+import 'package:junto_beta_mobile/widgets/end_drawer/end_drawer_relationships/search_bar.dart';
 import 'package:junto_beta_mobile/widgets/previews/circle_preview/circle_preview.dart';
 import 'package:junto_beta_mobile/widgets/progress_indicator.dart';
 import 'package:junto_beta_mobile/filters/bloc/channel_filtering_bloc.dart';
@@ -28,91 +29,107 @@ class _PublicCirclesState extends State<PublicCircles> {
 
   double sensitivityFactor = 20.0;
 
+  TextEditingController _subController;
+
   @override
   Widget build(BuildContext context) {
     return CustomRefresh(
       refresh: () {
         context.bloc<CircleBloc>().add(FetchPublicCircle());
       },
-      child: Column(
-        children: <Widget>[
-          if (widget.userProfile != null)
-            BlocBuilder<CircleBloc, CircleState>(
-              builder: (context, state) {
-                if (state is CircleError) {
-                  return Expanded(
-                    child: Center(
-                      child: Transform.translate(
-                        offset: const Offset(0.0, -50),
-                        child: const Text(
-                          'Hmm, something is up...',
-                        ),
-                      ),
-                    ),
-                  );
-                }
-                if (state is CircleLoaded) {
-                  final list = state.publicGroups;
-                  return Expanded(
-                    child: NotificationListener<ScrollNotification>(
-                      onNotification: (ScrollNotification notification) {
-                        final metrics = notification.metrics;
-                        double scrollPercent =
-                            (metrics.pixels / metrics.maxScrollExtent) * 100;
+      child: BlocBuilder<CircleBloc, CircleState>(
+        builder: (context, state) {
+          if (state is CircleError) {
+            return Expanded(
+              child: Center(
+                child: Transform.translate(
+                  offset: const Offset(0.0, -50),
+                  child: const Text(
+                    'Hmm, something is up...',
+                  ),
+                ),
+              ),
+            );
+          }
+          if (state is CircleLoaded) {
+            final list = state.publicGroups;
+            return Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                    top: 10,
+                  ),
+                  child: SearchBar(
+                    hintText: 'Search',
+                    textEditingController: _subController,
+                    onTextChange: (val) {
+                      context
+                          .bloc<CircleBloc>()
+                          .add(FetchPublicCircle(query: val));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: NotificationListener<ScrollNotification>(
+                    onNotification: (ScrollNotification notification) {
+                      final metrics = notification.metrics;
+                      double scrollPercent =
+                          (metrics.pixels / metrics.maxScrollExtent) * 100;
 
-                        if (notification.metrics.pixels - position >=
-                            sensitivityFactor) {
-                          position = notification.metrics.pixels;
-                          print('test: $scrollPercent');
-                          if (scrollPercent.toInt() >= 80) {
+                      if (notification.metrics.pixels - position >=
+                          sensitivityFactor) {
+                        position = notification.metrics.pixels;
+                        if (scrollPercent.toInt() >= 80) {
+                          context
+                              .bloc<CircleBloc>()
+                              .add(FetchMorePublicCircle());
+
+                          return true;
+                        }
+                      }
+
+                      if (position - notification.metrics.pixels >=
+                          sensitivityFactor) {
+                        position = notification.metrics.pixels;
+                      }
+
+                      return false;
+                    },
+                    child: ListView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      itemCount: list.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final group = list[index];
+                        return GestureDetector(
+                          onTap: () {
+                            widget.onGroupSelected(group);
+
                             context
-                                .bloc<CircleBloc>()
-                                .add(FetchMorePublicCircle());
-
-                            return true;
-                          }
-                        }
-
-                        if (position - notification.metrics.pixels >=
-                            sensitivityFactor) {
-                          position = notification.metrics.pixels;
-                        }
-
-                        return false;
+                                .bloc<ChannelFilteringBloc>()
+                                .add(FilterClear());
+                          },
+                          child: CirclePreview(
+                            group: group,
+                          ),
+                        );
                       },
-                      child: ListView.builder(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        itemCount: list.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final group = list[index];
-                          return GestureDetector(
-                            onTap: () {
-                              widget.onGroupSelected(group);
-
-                              context
-                                  .bloc<ChannelFilteringBloc>()
-                                  .add(FilterClear());
-                            },
-                            child: CirclePreview(
-                              group: group,
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  );
-                }
-                return Expanded(
-                  child: Center(
-                    child: Transform.translate(
-                      offset: const Offset(0.0, -50),
-                      child: JuntoProgressIndicator(),
                     ),
                   ),
-                );
-              },
+                ),
+              ],
+            );
+          }
+          return Expanded(
+            child: Center(
+              child: Transform.translate(
+                offset: const Offset(0.0, -50),
+                child: JuntoProgressIndicator(),
+              ),
             ),
-        ],
+          );
+        },
       ),
     );
   }
